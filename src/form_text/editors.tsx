9a12b338a61c68e787a130fc18697de2c9bb3120
CMJ <chemingjun@kuaishou.com>
2022-08-08 17:17:47 +0800

    refactor forms

--- src/form_text/editors.tsx
+++ src/form_text/editors.tsx
@@ -1,13 +1,13 @@
 export default {
-  '@parentUpdated'({id, data, parent}, {schema}) {
-    // if (schema === 'mybricks.normal-pc.form-container/form-item') {//in form container
-    //   data.type = 'formItem'
-    //
-    //   parent['@_setFormItem']({id, name: data.name, schema: {type: 'string'}})//use parents API
-    // } else {
-    //   data.type = 'normal'
-    // }
-  },
+  // '@parentUpdated'({id, data, parent}, {schema}) {
+  //   // if (schema === 'mybricks.normal-pc.form-container/form-item') {//in form container
+  //   //   data.type = 'formItem'
+  //   //
+  //   //   parent['@_setFormItem']({id, name: data.name, schema: {type: 'string'}})//use parents API
+  //   // } else {
+  //   //   data.type = 'normal'
+  //   // }
+  // },
   ':root'({data}: EditorResult<{ type }>, ...catalog) {
     catalog[0].title = '常规';
 
