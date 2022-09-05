fe1a7fa81bca850404ad3b75c2f8f48c9204e12c
zzfzfz <zzf.zhenfeng.zhang@gmail.com>
2022-09-05 17:20:16 +0800

    feat: pref text

--- src/text/constants.ts
+++ src/text/constants.ts
@@ -1,15 +1,52 @@
-export type AlignType = "left" | "center" | "right";
-
-type TextType = 'secondary' | 'danger' | 'warning' | undefined;
+export enum AlignTypeEnum {
+  Left = 'left',
+  Center = 'center',
+  Right = 'right'
+}
+export enum CursorTypeEnum {
+  Pointer = 'pointer',
+  Default = 'default'
+}
+export enum WhiteSpaceEnum {
+  PreWrap = 'pre-wrap',
+  NoWrap = 'nowrap'
+}
 
 export interface Data {
   content: string;
-  align?: AlignType;
-  style?: Record<string, any>;
-  stylePadding?: number[];
-  textType?: TextType;
-  click?: boolean;
+  align?: AlignTypeEnum;
+  style: Record<string, any>;
+  useClick?: boolean;
   outputContent?: string;
   isEllipsis: boolean;
-  ellipsis?: Record<string, any>;
+  ellipsis: Record<string, any>;
+
+  useDynamicStyle?: boolean;
 }
+
+export const InputIds = {
+  SetContent: 'content',
+  SetStyle: 'setStyle'
+};
+export const OutputIds = {
+  Click: 'click'
+};
+export const Schemas = {
+  String: {
+    type: 'string'
+  },
+  Style: {
+    type: 'object',
+    properties: {
+      color: {
+        type: 'string'
+      },
+      fontSize: {
+        type: 'string'
+      },
+      fontWeight: {
+        type: 'number'
+      }
+    }
+  }
+};
