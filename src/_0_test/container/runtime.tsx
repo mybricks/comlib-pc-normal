5e6a405f1efa383c7fb7ba6d696bd7a78a499ac8
CMJ <chemingjun@kuaishou.com>
2022-09-13 14:25:34 +0800

    pub.mybricks.json

--- src/_0_test/container/runtime.tsx
+++ src/_0_test/container/runtime.tsx
@@ -1,6 +1,27 @@
 import { useCallback, useState } from 'react';
 
 export default function ({ env, data, slots }) {
+  const [other, setOther] = useState(Math.random);
+
+  return (
+    <div style={{ border: '1px solid red', padding: 10 }}>
+      <div>
+        <button
+          onClick={(e) => {
+            setOther(Math.random);
+          }}
+        >
+          Test
+        </button>
+      </div>
+      <Test slots={slots} other={other} />
+    </div>
+  );
+}
+
+function Test({ slots, other }) {
+  console.log('other', other);
+
   const [ipts, setInpus] = useState([]);
 
   const test = useCallback(() => {
@@ -12,22 +33,17 @@ export default function ({ env, data, slots }) {
     });
   }, []);
 
-  return (
-    <div style={{ border: '1px solid red', padding: 10 }}>
-      <div>
-        <button onClick={test}>Test</button>
-      </div>
-      {env.runtime
-        ? slots['content'].render({
-            wrap(comAray: { id; jsx; def; inputs; outputs }[]) {
-              const item0 = comAray[0];
-              ipts.push(item0.inputs);
+  return slots['content'].render({
+    wrap(comAray: { id; jsx; def; inputs; outputs }[]) {
+      const item0 = comAray[0];
+      ipts.push(item0.inputs);
 
-              return comAray[0].jsx;
-            },
-            key: '0'
-          })
-        : slots['content'].render()}
-    </div>
-  );
+      console.log(comAray.length);
+
+      return comAray.map(({ id, jsx }, idx) => {
+        return <div key={id}>{jsx}</div>;
+      });
+    },
+    key: Math.random()
+  });
 }
