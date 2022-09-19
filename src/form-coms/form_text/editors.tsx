9737f5ee42b9b84f09c31fbe09403e72dcb908a9
huangqiuyun03 <huangqiuyun03@kuaishou.com>
2022-09-19 15:55:11 +0800

    feat:问题修复

--- src/form-coms/form_text/editors.tsx
+++ src/form-coms/form_text/editors.tsx
@@ -105,10 +105,10 @@ export default {
         options: [{ min: -1 }],
         value: {
           get({ data }) {
-            return data.config.maxLength || [-1];
+            return [data.config.maxLength];
           },
           set({ data }, value: number) {
-            data.config['maxLength'] = value;
+            data.config['maxLength'] = value[0];
           }
         }
       },
