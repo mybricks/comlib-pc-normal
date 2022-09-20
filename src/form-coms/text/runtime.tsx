9de7d7184f8044d7702fd9ff843a468c486e565b
GodStream <zpqwangyiyouxiang@163.com>
2022-09-20 17:08:54 +0800

    style: update code style

--- src/form-coms/text/runtime.tsx
+++ src/form-coms/text/runtime.tsx
@@ -21,15 +21,19 @@ interface Data {
 export default function ({ env, data, _inputs, inputs, _outputs, outputs }: RuntimeParams<Data>) {
   const { edit } = env;
 
+  const [value, setValue] = useState();
+
   useLayoutEffect(() => {
     inputs['setValue']((val) => {
       data.value = val;
-      outputs['onChange'](data.value);
+      setValue(val);
+      outputs['onChange'](value);
     });
 
     inputs['validate']((val, outputRels) => {
       validateFormItem({
-        value: data.value,
+        // value: data.value,
+        value,
         env,
         rules: data.rules
       })
@@ -40,13 +44,16 @@ export default function ({ env, data, _inputs, inputs, _outputs, outputs }: Runt
           outputRels['returnValidate'](e);
         });
     });
-
+    console.log('----text1111', `data.value ${data.value} ---`, value);
     inputs['getValue']((val, outputRels) => {
-      outputRels['returnValue'](data.value);
+      console.log('----text2222', `data.value ${data.value} ---`, value);
+      outputRels['returnValue'](value);
+      // outputRels['returnValue'](data.value);
     });
 
     inputs['resetValue'](() => {
       data.value = void 0;
+      setValue(void 0);
     });
 
     inputs['setVisible']((val: boolean) => {
@@ -56,12 +63,13 @@ export default function ({ env, data, _inputs, inputs, _outputs, outputs }: Runt
     inputs['setDisabled']((val: boolean) => {
       data.config.disabled = val;
     });
-  }, []);
+  }, [value]);
 
   const changeValue = useCallback((e) => {
-    const value = e.target.value;
-    data.value = value;
-    outputs['onChange'](data.value);
+    // const value = e.target.value;
+    data.value = e.target.value;
+    setValue(e.target.value);
+    outputs['onChange'](e.target.value);
   }, []);
 
   const onBlur = useCallback((e) => {
@@ -74,7 +82,7 @@ export default function ({ env, data, _inputs, inputs, _outputs, outputs }: Runt
     <Input
       type="text"
       {...data.config}
-      value={data.value}
+      value={value}
       readOnly={!!edit}
       onChange={changeValue}
       onBlur={onBlur}
