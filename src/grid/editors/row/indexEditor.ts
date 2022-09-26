9964f1d50321f191e73128b600b3dce31bee3452
tangxiaoxin <tangxiaoxin@kuaishou.com>
2022-09-26 15:36:28 +0800

    grid

--- src/grid/editors/row/indexEditor.ts
+++ src/grid/editors/row/indexEditor.ts
@@ -3,89 +3,107 @@ import { addColumn, copyRow, divideColumn, getRowIndex, getRowItem, updateCol } 
 
 const IndexEditor = [
   {
-    title: '添加列',
-    type: 'Button',
-    value: {
-      set(props: EditorResult<Data>) {
-        addColumn(props);
-        //最后一列“自动填充”需求
-        const { data, focusArea, slot } = props
-        if (!focusArea) return;
-        const row = getRowItem(data, focusArea);
-        updateCol(row, slot);
+    title: '行操作',
+    items: [
+      {
+        title: '前面添加行',
+        type: 'Button',
+        value: {
+          set(props: EditorResult<Data>, val: string) {
+            copyRow(props, 'before')
+          }
+        }
+      },
+      {
+        title: '后面添加行',
+        type: 'Button',
+        value: {
+          set(props: EditorResult<Data>, val: string) {
+            copyRow(props, 'after')
+          }
+        }
+      },
+      {
+        title: '上移',
+        type: 'Button',
+        ifVisible({ focusArea }: EditorResult<Data>) {
+          if (!focusArea) return;
+          const index = getRowIndex(focusArea);
+          return index !== undefined && index !== 0;
+        },
+        value: {
+          set({ data, focusArea }: EditorResult<Data>) {
+            if (!focusArea) return;
+            const index = getRowIndex(focusArea);
+            if (index < 1) return;
+            const oldRow = data.rows[index - 1];
+            data.rows[index - 1] = data.rows[index];
+            data.rows[index] = oldRow;
+          }
+        }
+      },
+      {
+        title: '下移',
+        type: 'Button',
+        ifVisible({ data, focusArea }: EditorResult<Data>) {
+          if (!focusArea) return;
+          const index = getRowIndex(focusArea);
+          return index !== undefined && index + 1 !== data.rows.length;
+        },
+        value: {
+          set({ data, focusArea }: EditorResult<Data>) {
+            if (!focusArea) return;
+            const index = getRowIndex(focusArea);
+            if (index === data.rows.length - 1) return;
+            const oldRow = data.rows[index + 1];
+            data.rows[index + 1] = data.rows[index];
+            data.rows[index] = oldRow;
+          }
+        }
+      },
+      {
+        title: '删除',
+        type: 'Button',
+        value: {
+          set({ data, slot, focusArea }: EditorResult<Data>) {
+            if (!focusArea) return;
+            const item = getRowItem(data, focusArea);
+            const index = getRowIndex(focusArea);
+            item.columns.forEach((columnItem) => {
+              slot.remove(columnItem.slot);
+            });
+            data.rows.splice(index, 1);
+          }
+        }
       }
-    }
+    ]
   },
   {
-    title: '列等分',
-    type: 'Button',
-    value: {
-      set(props: EditorResult<Data>) {
-        divideColumn(props);
-      }
-    }
+    title: '列操作',
+    items: [
+      {
+        title: '添加列',
+        type: 'Button',
+        value: {
+          set(props: EditorResult<Data>) {
+            addColumn(props);
+            //最后一列“自动填充”需求
+            const { data, focusArea, slot } = props
+            if (!focusArea) return;
+            const row = getRowItem(data, focusArea);
+            updateCol(row, slot);
+          }
+        }
+      },
+      {
+        title: '列等分',
+        type: 'Button',
+        value: {
+          set(props: EditorResult<Data>) {
+            divideColumn(props);
+          }
+        }
+      }]
   },
-  {
-    title: '复制当前行',
-    type: 'Button',
-    value: {
-      set(props: EditorResult<Data>) {
-        copyRow(props);
-      }
-    }
-  },
-  {
-    title: '上移',
-    type: 'Button',
-    ifVisible({ focusArea }: EditorResult<Data>) {
-      if (!focusArea) return;
-      const index = getRowIndex(focusArea);
-      return index !== undefined && index !== 0;
-    },
-    value: {
-      set({ data, focusArea }: EditorResult<Data>) {
-        if (!focusArea) return;
-        const index = getRowIndex(focusArea);
-        if (index < 1) return;
-        const oldRow = data.rows[index - 1];
-        data.rows[index - 1] = data.rows[index];
-        data.rows[index] = oldRow;
-      }
-    }
-  },
-  {
-    title: '下移',
-    type: 'Button',
-    ifVisible({ data, focusArea }: EditorResult<Data>) {
-      if (!focusArea) return;
-      const index = getRowIndex(focusArea);
-      return index !== undefined && index + 1 !== data.rows.length;
-    },
-    value: {
-      set({ data, focusArea }: EditorResult<Data>) {
-        if (!focusArea) return;
-        const index = getRowIndex(focusArea);
-        if (index === data.rows.length - 1) return;
-        const oldRow = data.rows[index + 1];
-        data.rows[index + 1] = data.rows[index];
-        data.rows[index] = oldRow;
-      }
-    }
-  },
-  {
-    title: '删除',
-    type: 'Button',
-    value: {
-      set({ data, slot, focusArea }: EditorResult<Data>) {
-        if (!focusArea) return;
-        const item = getRowItem(data, focusArea);
-        const index = getRowIndex(focusArea);
-        item.columns.forEach((columnItem) => {
-          slot.remove(columnItem.slot);
-        });
-        data.rows.splice(index, 1);
-      }
-    }
-  }
 ];
 export default IndexEditor;
