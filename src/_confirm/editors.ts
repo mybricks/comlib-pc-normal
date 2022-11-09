6ea9dfb69ce138bb7f2d1128100be46163cc1345
Bigbig-Huang <hqy09092020@163.com>
2022-11-09 16:04:20 +0800

    feat:确认提示-画布延迟加载，output.add之后，output.remove不起作用

--- src/_confirm/editors.ts
+++ src/_confirm/editors.ts
@@ -21,18 +21,25 @@ const TypeOptions = [
 export default {
   '@inputConnected'({ output }, fromPin, toPin) {
     if (toPin.id === InputIds.Open) {
+      //连接时拿到传递过来的inputValue的schema，给到确认和取消
+      //可以包含确认和其余类型
       const outputSchema =
-        fromPin.schema?.type === 'object' && fromPin.schema?.properties?.outputValue
+        ((fromPin.schema?.type === 'object') && (fromPin.schema?.properties?.outputValue !== undefined))
           ? fromPin.schema?.properties?.outputValue
           : fromPin.schema;
       output.get(OutputIds.Ok).setSchema(outputSchema);
-      output.get(OutputIds.Cancel).setSchema(outputSchema);
+
+      if(output.get(OutputIds.Cancel)){
+        output.get(OutputIds.Cancel).setSchema(outputSchema);
+      }
     }
   },
   '@inputDisConnected'({ output }, fromPin, toPin) {
     if (toPin.id === InputIds.Open) {
       output.get(OutputIds.Ok).setSchema(Schemas.Any);
-      output.get(OutputIds.Cancel).setSchema(Schemas.Any);
+      if(output.get(OutputIds.Cancel)){
+        output.get(OutputIds.Cancel).setSchema(Schemas.Any);
+      }
     }
   },
   '@init'({ data, setDesc }: EditorResult<Data>) {
@@ -42,9 +49,27 @@ export default {
     Editor<Data>('类型', EditorType.Select, 'type', {
       options: TypeOptions,
       value: {
-        set({ data, setDesc }: EditorResult<Data>, val: TypeEnum) {
+        set({ data, output, setDesc }: EditorResult<Data>, val: TypeEnum) {
           data.type = val;
           setDescByData({ data, setDesc });
+
+          const cancelSchema = output.get(OutputIds.Ok).schema;
+          console.log('output.get(OutputIds.Ok).schema',output.get(OutputIds.Ok).schema);
+
+          if(data.type === 'confirm'){
+            console.log('确认选项');
+            console.log('是否有OutputIds.Cancel', output.get(OutputIds.Cancel));
+            output.add(OutputIds.Cancel, '取消', cancelSchema);
+
+            console.log('所有的output1', output.get(OutputIds.Cancel), output.get(OutputIds.Ok));
+          }else{
+            console.log('其余选项');
+            console.log('确定有cancel的output嘛', output.get(OutputIds.Cancel));
+            console.log('output.remove(OutputIds.Cancel)');
+            output.remove(OutputIds.Cancel);
+
+            console.log('所有的output2', output.get(OutputIds.Cancel), output.get(OutputIds.Ok));
+          }
         }
       }
     }),
