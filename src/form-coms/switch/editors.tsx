ef6ceaa55c6833cd804aac88a973cf38f9ac260e
GodStream <zpqwangyiyouxiang@163.com>
2022-11-11 11:46:19 +0800

    feat: add switch

---
+++ src/form-coms/switch/editors.tsx
@@ -0,0 +1,131 @@
+import { RuleKeys, defaultValidatorExample, defaultRules } from '../utils/validator';
+
+export default {
+  // '@resize': {
+  //   // options: ['width']
+  // },
+  '@parentUpdated'({ id, data, parent }, { schema }) {
+    if (schema === 'mybricks.normal-pc.form-container/form-item') {
+      parent['@_setFormItem']({ id, schema: { type: 'boolean' } });
+    }
+    // if (schema === 'mybricks.normal-pc.form-container/form-item') {//in form container
+    //   data.type = 'formItem'
+    //
+    //   parent['@_setFormItem']({id, name: data.name, schema: {type: 'string'}})//use parents API
+    // } else {
+    //   data.type = 'normal'
+    // }
+  },
+  ':root'({ data }: EditorResult<{ type }>, ...catalog) {
+    catalog[0].title = '常规';
+
+    catalog[0].items = [
+      {
+        title: '禁用状态',
+        type: 'switch',
+        description: '是否禁用状态',
+        value: {
+          get({ data }) {
+            return data.config.disabled;
+          },
+          set({ data }, value: boolean) {
+            data.config.disabled = value;
+          }
+        }
+      },
+      {
+        title: '校验规则',
+        description: '提供快捷校验配置',
+        type: 'ArrayCheckbox',
+        options: {
+          checkField: 'status',
+          visibleField: 'visible',
+          getTitle,
+          items: [
+            // {
+            //   title: '提示文字',
+            //   description: '提示文字的表达式（{}, =, <, >, ||, &&）, 例：${label}不能为空',
+            //   type: 'EXPRESSION',
+            //   options: {
+            //     autoSize: true,
+            //     placeholder: '例：${label}不能为空',
+            //     // suggestions: getSuggestions(true),
+            //   },
+            //   value: 'message'
+            // },
+            {
+              title: '提示文字',
+              type: 'Text',
+              value: 'message',
+              ifVisible(item: any, index: number) {
+                return item.key === RuleKeys.REQUIRED;
+              }
+            },
+            {
+              title: '编辑校验规则',
+              type: 'code',
+              options: {
+                language: 'javascript',
+                enableFullscreen: false,
+                title: '编辑校验规则',
+                width: 600,
+                minimap: {
+                  enabled: false
+                },
+                babel: true,
+                eslint: {
+                  parserOptions: {
+                    ecmaVersion: '2020',
+                    sourceType: 'module'
+                  }
+                }
+              },
+              ifVisible(item: any, index: number) {
+                return item.key === RuleKeys.CODE_VALIDATOR;
+              },
+              value: 'validateCode'
+            }
+          ]
+        },
+        value: {
+          get({ data }) {
+            return data.rules.length > 0 ? data.rules : defaultRules;
+          },
+          set({ data }, value: any) {
+            data.rules = value;
+          }
+        }
+      },
+      {
+        title: '事件',
+        items: [
+          {
+            title: '值发生改变',
+            type: '_event',
+            options: {
+              outputId: 'onChange'
+            }
+          }
+          // {
+          //   title: '失去焦点',
+          //   type: '_event',
+          //   options: {
+          //     outputId: 'onBlur'
+          //   }
+          // }
+        ]
+      }
+    ];
+  }
+};
+
+const getTitle = (item: any, index: number) => {
+  const { key, title, numericalLimit, regExr } = item;
+  // let detail;
+  // if (key === RuleKeys.REG_EXP) {
+  //   detail = regExpressions.find(({ value }) => value === regExr)?.label;
+  // } else if ([RuleKeys.MIN, RuleKeys.MAX, RuleKeys.MIN_LENGTH, RuleKeys.MAX_LENGTH].includes(key)) {
+  //   detail = Array.isArray(numericalLimit) ? numericalLimit[0] || '0' : '0';
+  // }
+  return title;
+};
