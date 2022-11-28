2c6d27d1a7cda5232f5924eed06eae9e82f20c07
tangxiaoxin <tangxiaoxin@kuaishou.com>
2022-11-28 21:59:35 +0800

    slot schema

--- src/timeline/editors/index.ts
+++ src/timeline/editors/index.ts
@@ -2,14 +2,15 @@ import { Data, OutputIds } from '../constants';
 import ItemEditor from './item';
 import BaseEditor, { DefaultSourceSchema } from './baseEditor';
 import StyleEditor from './styleEditor';
-import { updateSourceSchema } from './utils';
+import { updateSourceSchema, updateSlotSchema } from './utils';
 
 export default {
   '@resize': {
     options: ['width']
   },
-  '@inputConnected'({ data, input }, fromPin) {
+  '@inputConnected'({ data, input, slots }, fromPin) {
     updateSourceSchema(input, fromPin.schema);
+    updateSlotSchema(slots, fromPin.schema)
   },
   '@inputDisConnected'({ data, input }) {
     updateSourceSchema(input, DefaultSourceSchema);
