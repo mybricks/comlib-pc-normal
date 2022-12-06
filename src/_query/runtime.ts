75c50697f64bbada3fcdc0e2eb50e6df309baaad
GodStream <zpqwangyiyouxiang@163.com>
2022-12-06 21:04:20 +0800

    ref: update query error  message

--- src/_query/runtime.ts
+++ src/_query/runtime.ts
@@ -21,7 +21,8 @@ export default function ({ env, data, inputs, outputs, logger, onError, id }: Ru
         }
       })
     } else {
-      console.warn(`【路由参数】组件： 环境变量 vars.getQuery 方法未实现`);
+      onError?.('环境变量 vars.getQuery 方法未实现');
+      console.error(`【路由参数】组件： 环境变量 vars.getQuery 方法未实现`);
     }
 
   } catch (ex) {
