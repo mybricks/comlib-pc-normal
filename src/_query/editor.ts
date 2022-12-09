2867080da3dca3627c6e40d1b55284f524b9d541
tangxiaoxin <tangxiaoxin@kuaishou.com>
2022-12-09 18:10:30 +0800

    fix:_query

--- src/_query/editor.ts
+++ src/_query/editor.ts
@@ -7,8 +7,5 @@ export default {
       data.runImmediate = true;
     }
   },
-  ':root'({ data }: EditorResult<Data>, ...cate) {
-    cate[0].title = '配置';
-    cate[0].items = [];
-  }
+  ':root': []
 };
