885df2c7c1d9d094c8084b314e383b49d68028f1
z35635 <linzhujlu@foxmail.com>
2022-12-09 17:14:31 +0800

    feat: rate upgradeIO

--- src/form-coms/rate/runtime.tsx
+++ src/form-coms/rate/runtime.tsx
@@ -3,7 +3,9 @@ import { Rate } from 'antd';
 import { validateFormItem } from '../utils/validator';
 import * as Icons from '@ant-design/icons';
 import css from './runtime.less';
-interface Data {
+import useFormItemInputs from '../form-container/models/FormItem';
+
+export interface Data {
   options: any[];
   isChoose: boolean;
   choose: string;
@@ -22,43 +24,42 @@ export default function Runtime(props: RuntimeParams<Data>) {
   const { data, inputs, outputs, env } = props;
   const [value, setValue] = useState<number>(data.config.defaultValue);
 
-  useLayoutEffect(() => {
-    //1、设置值
-    inputs['setValue']((val) => {
-      setValue(val);
-      onChange(val);
-    });
-    //2、设置校验
-    inputs['validate']((val, outputRels) => {
-      validateFormItem({
-        value: value,
-        env,
-        rules: data.rules
-      })
-        .then((r) => {
-          outputRels['returnValidate'](r);
+  useFormItemInputs({
+    inputs,
+    outputs,
+    configs: {
+      setValue(val) {
+        setValue(val);
+      },
+      setInitialValue(val) {
+        setValue(val);
+      },
+      returnValue(output) {
+        output(value);
+      },
+      resetValue() {
+        setValue(0);
+      },
+      setDisabled() {
+        data.config.disabled = true;
+      },
+      setEnabled() {
+        data.config.disabled = false;
+      },
+      validate(output) {
+        validateFormItem({
+          value: value,
+          env,
+          rules: data.rules
         })
-        .catch((e) => {
-          outputRels['returnValidate'](e);
-        });
-    });
-    //3、获取值
-    inputs['getValue']((val, outputRels) => {
-      outputRels['returnValue'](value);
-    });
-  }, [value]);
-
-  //4、重置值
-  inputs['resetValue'](() => {
-    setValue(0);
-  });
-  //5、设置只读状态
-  inputs['setDisabled'](() => {
-    data.config.disabled = true;
-  });
-  //6、设置启用状态
-  inputs['setEnabled'](() => {
-    data.config.disabled = false;
+          .then((r) => {
+            output(r);
+          })
+          .catch((e) => {
+            output(e);
+          });
+      }
+    }
   });
 
   //1、值变化
