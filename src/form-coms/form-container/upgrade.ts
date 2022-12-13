be9fdc3e3a483c20096d5855a9384c0fef3a5207
z35635 <linzhujlu@foxmail.com>
2022-12-13 14:34:02 +0800

    style: form-container upgrade.ts

--- src/form-coms/form-container/upgrade.ts
+++ src/form-coms/form-container/upgrade.ts
@@ -2,29 +2,29 @@ import { Data } from './types';
 import { inputIds } from './constants'
 
 export default function ({ data, input, output }: UpgradeParams<Data>): boolean {
-    if (!input.get(inputIds.SET_INITIAL_VALUES)) {
-        const schema = {
-            "type": "object",
-            "properties": {}
-        };
+  if (!input.get(inputIds.SET_INITIAL_VALUES)) {
+    const schema = {
+      "type": "object",
+      "properties": {}
+    };
 
-        input.add(inputIds.SET_INITIAL_VALUES, '设置表单数据', schema);
+    input.add(inputIds.SET_INITIAL_VALUES, '设置表单数据', schema);
+  }
+
+  if (input.get(inputIds.SET_FIELDS_VALUE)) {
+    input.get(inputIds.SET_FIELDS_VALUE).setTitle('设置表单数据（触发值变化）')
+  }
+
+  //1.1.0 ->1.1.1
+  if (!data.actions.widthOption) {
+    data.actions.widthOption = 'span';
+  }
+  data.items.forEach(item => {
+    if (!item.widthOption) {
+      item.widthOption = 'span'
+      item.span = 24 / data.formItemColumn;
     }
+  })
 
-    if (input.get(inputIds.SET_FIELDS_VALUE)) {
-        input.get(inputIds.SET_FIELDS_VALUE).setTitle('设置表单数据（触发值变化）')
-    }
-
-    //1.1.0 ->1.1.1
-    if (!data.actions.widthOption) {
-        data.actions.widthOption = 'span';
-    }
-    data.items.forEach(item => {
-        if (!item.widthOption) {
-            item.widthOption = 'span'
-            item.span = 24 / data.formItemColumn;
-        }
-    })
-
-    return true;
+  return true;
 }
