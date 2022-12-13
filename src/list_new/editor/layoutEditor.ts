3963ae724c4198a91abfcfc5c8bac2f6cf58b2f2
Bigbig-Huang <hqy09092020@163.com>
2022-12-13 15:20:37 +0800

    feat:新列表容器

---
+++ src/list_new/editor/layoutEditor.ts
@@ -0,0 +1,97 @@
+import {
+  Data
+} from '../constants';
+
+export const LayoutEditor = [
+  {
+    title: '布局',
+    items: [
+      {
+        title: '换行',
+        type: 'switch',
+        description: '默认自动换行，开启后不换行, 接着可配置是否需要横向滚动',
+        value: {
+          get({ data }: EditorResult<Data>){
+            return data.isAuto;
+          },
+          set({ data }: EditorResult<Data>, val: boolean){
+            data.isAuto = val;
+          }
+        }
+      },
+      {
+        title: '横向滚动',
+        type: 'switch',
+        description: '开启后传递的内容超过页面宽度后, 可选择是否需要横向滚动, 开启后横向滚动, 否则隐藏内容',
+        ifVisible({ data }: EditorResult<Data>) {
+          return !data.isAuto;
+        },
+        value:{
+          get({ data }: EditorResult<Data>){
+            return data.isScroll || false;
+          },
+          set({ data }: EditorResult<Data>, val: boolean){
+            data.isScroll = val;
+          }
+        }
+      },
+      {
+        title: '列数自定义',
+        type: 'switch',
+        description: '开启后传递的内容超过页面宽度后, 可选择是否需要横向滚动, 开启后横向滚动, 否则隐藏内容',
+        ifVisible({ data }: EditorResult<Data>) {
+          return !!data.isAuto;
+        },
+        value:{
+          get({ data }: EditorResult<Data>){
+            return data.isCustom || false;
+          },
+          set({ data }: EditorResult<Data>, val: boolean){
+            data.isCustom = val;
+          }
+        }
+      },
+      {
+        title: '列数',
+        type: 'InputNumber',
+        ifVisible({ data }: EditorResult<Data>) {
+            return !!data.isAuto && !!data.isCustom;
+          },
+        options: [{ min: 1, max: 1000, width: 100 }],
+        value: {
+          get({ data }: EditorResult<Data>) {
+            return [data.grid.column];
+          },
+          set({ data }: EditorResult<Data>, val) {
+            data.grid.column = val[0];
+          }
+        }
+      },
+      {
+        title: '间隔',
+        type: 'InputNumber',
+        options: [
+          { min: 0, max: 1000, width: 100, title: '左右' },
+          { min: 0, max: 1000, width: 100, title: '上下' }
+        ],
+        value: {
+          get({ data }: EditorResult<Data>) {
+            if (Array.isArray(data.grid.gutter)) {
+              return data.grid.gutter;
+            }
+            return [data.grid.gutter, 16];
+          },
+          set({ data }: EditorResult<Data>, val) {
+            if (!data.grid) {
+              data.grid = {};
+            }
+            data.grid = {
+              ...data.grid,
+              gutter: val
+            };
+          }
+        }
+      },
+    ]
+  }
+];
---
+++ src/list_new/runtime.tsx
@@ -0,0 +1,182 @@
+import React, { useEffect, useState } from 'react';
+import { List, Spin } from 'antd';
+import classnames from 'classnames';
+import { Data, InputIds } from './constants';
+import { uuid } from '../utils';
+import css from './style.less';
+
+const rowKey = '_itemKey';
+export default ({ data, inputs, slots, env, outputs }: RuntimeParams<Data>) => {
+  const { grid, useLoading, useGetDataSource } = data;
+  const [dataSource, setDataSource] = useState<any[]>([...(data.dataSource || [])]);
+  const [loading, setLoading] = useState(false);
+  const gutter: any = Array.isArray(grid.gutter) ? grid.gutter : [grid.gutter, 16];
+
+  //设置数据源输入及loading状态设置
+  useEffect(() => {
+    if (env.edit) {
+      setDataSource([{ id: 1 }]);
+    }
+    if (env.runtime) {
+      inputs[InputIds.DATA_SOURCE]((v) => {
+        if (Array.isArray(v)) {
+          const ds = v.map((item, index) => ({ item, [rowKey]: uuid(), index: index }));
+          data.dataSource = ds;
+          setDataSource(ds);
+        }
+        setLoading(false);
+      });
+      useLoading &&
+        inputs[InputIds.LOADING] &&
+        inputs[InputIds.LOADING]((v) => {
+          setLoading(v !== false);
+        });
+    }
+  }, []);
+  //获取数据源
+  useEffect(() => {
+    if (env.runtime && useGetDataSource && inputs[InputIds.GetDataSource]) {
+      inputs[InputIds.GetDataSource]((val, relOutputs) => {
+        const outputFn = relOutputs?.[InputIds.GetDataSource] || outputs[InputIds.GetDataSource];
+        outputFn(dataSource.map(({ [rowKey]: key, ...res }) => ({ ...res })));
+      });
+    }
+  }, [dataSource]);
+
+  //1. 自动换行，列数不自定义的情况
+  const AutoRender = () => {
+    return (
+      <div
+        className={css.flexContainer}
+        style={{ margin: `calc(0px - ${gutter[1]}px) 0 0 calc(0px - ${gutter[0]}px)` }}
+      >
+        {dataSource.map(({ [rowKey]: key, index: index, item: item }) => (
+          <div key={key} style={{ margin: `${gutter[1]}px 0 0 ${gutter[0]}px` }}>
+            {slots['item'].render({
+              inputValues: {
+                itemData: item,
+                index: index
+              },
+              key: key
+            })}
+          </div>
+        ))}
+      </div>
+    );
+  };
+
+  //2. 换行，列数自定义
+  const ListItemRender = ({ [rowKey]: key, index: index, item: item }) => {
+    return (
+      <List.Item key={key}>
+        {/* 当前项数据和索引 */}
+        {slots['item'].render({
+          inputValues: {
+            itemData: item,
+            index: index
+          },
+          key: key
+        })}
+      </List.Item>
+    );
+  };
+
+  //3. 不换行，且不滚动
+  const NoAutoRender = () => {
+    return (
+      <div
+        className={css.noflexContainer}
+        style={{ margin: `calc(0px - ${gutter[1]}px) 0 0 calc(0px - ${gutter[0]}px)` }}
+      >
+        {dataSource.map(({ [rowKey]: key, index: index, item: item }) => (
+          <div key={key} style={{ margin: `${gutter[1]}px 0 0 ${gutter[0]}px` }}>
+            {slots['item'].render({
+              inputValues: {
+                itemData: item,
+                index: index
+              },
+              key: key
+            })}
+          </div>
+        ))}
+      </div>
+    );
+  };
+
+  //4. 不换行，但是滚动
+  const NoAutoScrollRender = () => {
+    return (
+      <div className={css.scrollContainer}>
+        {dataSource.map(({ [rowKey]: key, index: index, item: item }) => (
+          <div
+            key={key}
+            className={css.scrollBox}
+            style={{ margin: `0 ${gutter[0]}px ${gutter[1]}px 0` }}
+          >
+            {slots['item'].render({
+              inputValues: {
+                itemData: item,
+                index: index
+              },
+              key: key
+            })}
+          </div>
+        ))}
+      </div>
+    );
+  };
+
+  //0) 无内容
+  if (slots['item'].size === 0) {
+    return slots['item'].render();
+  }
+  //1) 自动换行
+  if (data.isAuto === true && data.isCustom === false) {
+    return loading ? (
+      <Spin spinning={loading} tip={data.loadingTip} wrapperClassName={css.loading}>
+        <AutoRender />
+      </Spin>
+    ) : (
+      <AutoRender />
+    );
+  }
+  //2) 换行，列数自定义
+  else if (data.isAuto === true && data.isCustom === true && data.grid.column !== undefined) {
+    return (
+      <List
+        loading={loading}
+        grid={{
+          ...grid,
+          gutter
+        }}
+        dataSource={dataSource}
+        renderItem={ListItemRender}
+        rowKey={rowKey}
+        className={classnames(
+          css.listWrap,
+          dataSource.length === 0 && env.runtime && !loading && css.hideEmpty
+        )}
+      />
+    );
+  }
+  //3）不自动换行，不滚动
+  else if (data.isAuto === false && data.isScroll === false) {
+    return loading ? (
+      <Spin spinning={loading} tip={data.loadingTip} wrapperClassName={css.loading}>
+        <NoAutoRender />
+      </Spin>
+    ) : (
+      <NoAutoRender />
+    );
+  }
+  //4）不换行，滚动
+  else if (data.isAuto === false && data.isScroll === true) {
+    return loading ? (
+      <Spin spinning={loading} tip={data.loadingTip} wrapperClassName={css.loading}>
+        <NoAutoScrollRender />
+      </Spin>
+    ) : (
+      <NoAutoScrollRender />
+    );
+  }
+};
