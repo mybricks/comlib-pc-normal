f8bc4146a8a112a6960eee735d4b8108fa7e8317
dawujiang <yqdawujiang@163.com>
2022-12-15 21:41:56 +0800

    fix: form detail schema

--- src/form-detail/editors.tsx
+++ src/form-detail/editors.tsx
@@ -4,6 +4,17 @@ import { ItemsEditors } from './editors/item';
 import { Schemas } from './editors/utils';
 
 export default {
+  '@inputConnected'({ data, input }, fromPin, toPin) {
+    if (toPin.id === InputIds.SetDataSource) {
+      if (fromPin.schema.type === 'object') {
+        data.inputSchema = fromPin.schema;
+        input.get(InputIds.SetDataSource).setSchema(fromPin.schema);
+      }
+    }
+  },
+  '@inputDisConnected'({ data }) {
+    data.inputSchema = void 0;
+  },
   '@init'({ data, input }: EditorResult<Data>) {
     input.add(InputIds.SetDataSource, '设置数据源', Schemas.SetDataSource(data));
     input.add(InputIds.SetTitle, '设置标题', { type: 'string' });
