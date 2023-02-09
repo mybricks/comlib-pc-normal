67674808aaa72fdf339adce9ca96235a60912b2a
liuzhigang06 <liuzhigang06@kuaishou.com>
2023-02-09 17:59:09 +0800

    feat(code_segment): support multiple input

--- src/_code-segment/constants.ts
+++ src/_code-segment/constants.ts
@@ -1,4 +1,4 @@
-export const CODE_TEMPLATE = `({ inputValue, outputs }) => {
+export const CODE_TEMPLATE = `({ inputValue, outputs, inputs }) => {
   const { output0 } = outputs;
   output0(inputValue);
 }`;
@@ -11,18 +11,22 @@ export const IMMEDIATE_CODE_TEMPLATE = `({ outputs }) => {
 export const COMMENTS = `/**
 * @param inputValue: any 输入项的值
 * @parma outputs: any 输出项
+* @parma inputs: any 输入项
 *
 * 例子
-* ({ inputValue, outputs }) => {
+* ({ inputValue, outputs, inputs }) => {
+*   const { inputValue0, inputValue1, inputValue2 } = inputs;
 *   const { output0, output1, output2 } = outputs;
 *   const res = '该值输出给下一个组件使用' + inputValue
 *   
 *   // 向输出项（output0）输出结果
 *   output0(res); 
-*   
+
 *   // 多输出的情况
-*   // output1(res); 
-*   // output2(res); 
+*   // 向输出项（output1）输出输入项0的值
+*   // output1(inputValue0); 
+*   // 向输出项（output2）输出输入项1的值
+*   // output2(inputValue1); 
 * }
 */`;
 
