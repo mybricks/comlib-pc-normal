be1af0f12daa28a548ebc8ebb6db6d5f7deb3e55
Bigbig-Huang <hqy09092020@163.com>
2023-02-10 16:01:32 +0800

    feat(card&list-new):change style of list-new and card tocode

--- src/card/toReact.ts
+++ src/card/toReact.ts
@@ -18,14 +18,14 @@ export default function ({ data, slots, style }) {
         height: '100%',
         cursor: data.cursor ? 'pointer' : 'unset'
       })}}
-      extra={${data.useExtra ? slotExtra : undefined}}
+      ${data.useExtra && slotExtra.length!==0 ? `extra={${slotExtra}}` : ''}
       hoverable={${data.hoverable}}
       >
         ${slotBody}
       </Card>
     </div>`;
   };
-  
+
   let str = renderCard(data,slots,style);
 
   return {
--- src/list-new/runtime.tsx
+++ src/list-new/runtime.tsx
@@ -5,6 +5,7 @@ import { Data, InputIds } from './constants';
 import { uuid } from '../utils';
 import css from './style.less';
 import { SortableList, SortableItem } from './sort';
+import { AutoRender, NoAutoRender, NoAutoScrollRender } from './render';
 
 const arrayMove = <T,>(array: Array<T>, form: number, to: number): Array<T> => {
   const _array = array.slice();
@@ -51,29 +52,7 @@ export default ({ data, inputs, slots, env, outputs }: RuntimeParams<Data>) => {
     }
   }, [dataSource]);
 
-  //1. 自动换行，列数不自定义的情况
-  const AutoRender = () => {
-    return (
-      <div
-        className={css.flexContainer}
-        style={{ margin: `calc(0px - ${gutter[1]}px) 0 0 calc(0px - ${gutter[0]}px)` }}
-      >
-        {dataSource.map(({ [rowKey]: key, index: index, item: item }) => (
-          <div key={key} style={{ margin: `${gutter[1]}px 0 0 ${gutter[0]}px` }}>
-            {slots['item'].render({
-              inputValues: {
-                itemData: item,
-                index: index
-              },
-              key: key
-            })}
-          </div>
-        ))}
-      </div>
-    );
-  };
-
-  //2. 换行，列数自定义
+  //换行，列数自定义
   const ListItemRender = ({ [rowKey]: key, index: index, item: item }) => {
     return (
       <List.Item key={key}>
@@ -89,138 +68,92 @@ export default ({ data, inputs, slots, env, outputs }: RuntimeParams<Data>) => {
     );
   };
 
-  //3. 不换行，且不滚动
-  const NoAutoRender = () => {
-    return (
-      <div
-        className={css.noflexContainer}
-        style={{ margin: `calc(0px - ${gutter[1]}px) 0 0 calc(0px - ${gutter[0]}px)` }}
-      >
-        {dataSource.map(({ [rowKey]: key, index: index, item: item }) => (
-          <div key={key} style={{ margin: `${gutter[1]}px 0 0 ${gutter[0]}px` }}>
-            {slots['item'].render({
-              inputValues: {
-                itemData: item,
-                index: index
-              },
-              key: key
-            })}
-          </div>
-        ))}
-      </div>
-    );
-  };
-
-  //4. 不换行，但是滚动
-  const NoAutoScrollRender = () => {
-    return (
-      <div className={css.scrollContainer}>
-        {dataSource.map(({ [rowKey]: key, index: index, item: item }) => (
-          <div key={key} className={css.scrollBox} style={{ margin: `0 ${gutter[0]}px 0 0` }}>
-            {slots['item'].render({
-              inputValues: {
-                itemData: item,
-                index: index
-              },
-              key: key
-            })}
-          </div>
-        ))}
-      </div>
-    );
-  };
-
-  //0) 无内容
+  //0、 无内容
   if (slots['item'].size === 0) {
     return slots['item'].render();
   }
-
-  if (data.canSort) {
-    return env.edit ? (
-      <List
-        loading={loading}
-        grid={{
-          ...grid,
-          gutter
-        }}
-        dataSource={dataSource}
-        renderItem={ListItemRender}
-        rowKey={rowKey}
-        className={classnames(
-          css.listWrap,
-          dataSource.length === 0 && env.runtime && !loading && css.hideEmpty
-        )}
-      />
-    ) : (
-      <SortableList
-        list={dataSource}
-        data={data}
-        lockAxis="y"
-        renderItem={({ key, item, index }) => (
-          <SortableItem key={key} index={index}>
-            {slots['item'].render({
-              inputValues: {
-                itemData: item,
-                index
-              },
-              key
-            })}
-          </SortableItem>
-        )}
-        onSortEnd={({ oldIndex, newIndex }) => {
-          setDataSource(arrayMove(dataSource, oldIndex, newIndex));
-        }}
-      />
-    );
-  }
-
-  //1) 自动换行
+  //1、 自动换行
   if (data.isAuto === true && data.isCustom === false) {
     return loading ? (
       <Spin spinning={loading} tip={data.loadingTip} wrapperClassName={css.loading}>
-        <AutoRender />
+        {AutoRender(dataSource, data, slots)}
       </Spin>
     ) : (
-      <AutoRender />
+      AutoRender(dataSource, data, slots)
     );
   }
-  //2) 换行，列数自定义
+  //2、 换行，列数自定义
   else if (data.isAuto === true && data.isCustom === true && data.grid.column !== undefined) {
-    return (
-      <List
-        loading={loading}
-        grid={{
-          ...grid,
-          gutter
-        }}
-        dataSource={dataSource}
-        renderItem={ListItemRender}
-        rowKey={rowKey}
-        className={classnames(
-          css.listWrap,
-          dataSource.length === 0 && env.runtime && !loading && css.hideEmpty
-        )}
-      />
-    );
+    //2.1、可拖拽（换行、列数自定义、列数为1时，可拖拽排序）
+    if (data.canSort) {
+      return env.edit ? (
+        <List
+          loading={loading}
+          grid={{
+            ...grid,
+            gutter
+          }}
+          dataSource={dataSource}
+          renderItem={ListItemRender}
+          rowKey={rowKey}
+          className={classnames(
+            css.listWrap,
+            dataSource.length === 0 && env.runtime && !loading && css.hideEmpty
+          )}
+        />
+      ) : (
+        <SortableList
+          list={dataSource}
+          data={data}
+          lockAxis="y"
+          renderItem={({ key, item, index }) => (
+            <SortableItem key={key} index={index}>
+              {slots['item'].render({
+                inputValues: {
+                  itemData: item,
+                  index
+                },
+                key
+              })}
+            </SortableItem>
+          )}
+          onSortEnd={({ oldIndex, newIndex }) => {
+            setDataSource(arrayMove(dataSource, oldIndex, newIndex));
+          }}
+        />
+      );
+    } else {
+      //2.2、普通情况
+      return (
+        <List
+          loading={loading}
+          grid={{
+            ...grid,
+            gutter
+          }}
+          dataSource={dataSource}
+          renderItem={ListItemRender}
+          rowKey={rowKey}
+          className={classnames(
+            css.listWrap,
+            dataSource.length === 0 && env.runtime && !loading && css.hideEmpty
+          )}
+        />
+      );
+    }
   }
-  //3）不自动换行，不滚动
-  else if (data.isAuto === false && data.isScroll === false) {
+  //3、不自动换行，不滚动
+  //4、不自动换行，滚动
+  else if (data.isAuto === false) {
     return loading ? (
       <Spin spinning={loading} tip={data.loadingTip} wrapperClassName={css.loading}>
-        <NoAutoRender />
+        {data.isScroll} ? {NoAutoScrollRender(dataSource, data, slots)} :{' '}
+        {NoAutoRender(dataSource, data, slots)}
       </Spin>
+    ) : data.isScroll ? (
+      NoAutoScrollRender(dataSource, data, slots)
     ) : (
-      <NoAutoRender />
-    );
-  }
-  //4）不换行，滚动
-  else if (data.isAuto === false && data.isScroll === true) {
-    return loading ? (
-      <Spin spinning={loading} tip={data.loadingTip} wrapperClassName={css.loading}>
-        <NoAutoScrollRender />
-      </Spin>
-    ) : (
-      <NoAutoScrollRender />
+      NoAutoRender(dataSource, data, slots)
     );
   }
 };
