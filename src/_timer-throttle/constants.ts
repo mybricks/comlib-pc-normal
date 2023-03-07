bd02543b37b86af6c39d32108bbc3fe67d386c8d
Bigbig-Huang <hqy09092020@163.com>
2023-03-07 16:51:29 +0800

    feat(debounce&throttle):add timers

---
+++ src/_timer-throttle/constants.ts
@@ -0,0 +1,21 @@
+export const InputIds = {
+  Trigger: 'trigger',
+};
+
+export const OutputIds = {
+  Trigger: 'trigger'
+};
+
+export const Schemas = {
+  Any: {
+    type: 'any'
+  }
+};
+
+/**
+ * 数据源
+ * @param delay 延迟时间
+ */
+export interface Data {
+  delay: number;
+}
