35ddae2c7bbbfb5c679117cada9797581f686d27
Bigbig-Huang <hqy09092020@163.com>
2023-03-24 10:31:15 +0800

    test(00_schema-test):schema error

---
+++ src/00_schema-test/runtime.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Button } from 'antd';
+
+export default function (props) {
+  const { env, inputs, data, logger, slots, style } = props;
+  return (
+    <div>
+      <Button>123</Button>
+    </div>
+  );
+}
