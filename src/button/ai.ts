25baf3560ea7aa500e9d28ec8b81ab8e183b6bbd
GodStream <zpqwangyiyouxiang@163.com>
2023-03-31 11:47:37 +0800

    feat(button): update ai

--- src/button/ai.ts
+++ src/button/ai.ts
@@ -1,20 +1,33 @@
 export default {
-  '@focus'({data}) {
+  '@focus'({ data }) {
     return {
       data,
       prompts: `
       以下是一些例子：
       Q：修改当前文案为ABC;
-      A:{text: 'ABC'}
+      A：{text: 'ABC'}
+      Q：输出 1
+      A：{dataType: 'number', outVal: 1}
+      Q：输出 mybricks
+      A：{dataType: 'string', outVal: 'mybricks'}
       `
     }
   },
-  '@update'({data, newData}) {
+  '@update'({ data, newData }) {
     if (typeof newData.text === 'string') {
       data.text = newData.text
     }
-    if(newData.style){
-      data.style = Object.assign({},data.style,newData.style)
+
+    if (newData.style) {
+      data.style = Object.assign({}, data.style, newData.style)
+    }
+
+    if (typeof newData.dataType === 'string') {
+      data.dataType = newData.dataType
+    }
+
+    if (newData.outVal) {
+      data.outVal = newData.outVal
     }
   }
 }
