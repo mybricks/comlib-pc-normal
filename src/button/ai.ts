a04b69f451079b3be98cd7abb3c192c7b4437158
GodStream <zpqwangyiyouxiang@163.com>
2023-04-07 11:35:32 +0800

    ref(button): update ai

--- src/button/ai.ts
+++ src/button/ai.ts
@@ -3,23 +3,33 @@ export default {
     return {
       data,
       prompts: `
-      以下是一些例子：
-      Q：修改当前文案为ABC;
-      A：{text: 'ABC'}
-      Q：输出 1
-      A：{dataType: 'number', outVal: 1}
-      Q：输出 mybricks
-      A：{dataType: 'string', outVal: 'mybricks'}
+      以下是一些例子（最终回答不需要带上“答：”字）：
+      问：修改当前文案为ABC；
+      答：{text: 'ABC'}
+      问：输出 1；
+      答：{dataType: 'number', outVal: 1}
+      问：输出 mybricks；
+      答：{dataType: 'string', outVal: 'mybricks'}
+      问：颜色为红色；
+      答：{style: {color: 'red'}}
       `
     }
   },
-  '@update'({ data, newData }) {
+  '@update'({ data, newData, style }) {
     if (typeof newData.text === 'string') {
       data.text = newData.text
     }
-
+    
     if (newData.style) {
-      data.style = Object.assign({}, data.style, newData.style)
+      const { width, height, ...otherStyles } = newData.style
+      data.style = Object.assign({}, data.style, otherStyles)
+      if (width) {
+        style.width = width
+      }
+      if (height) {
+        style.height = height
+      }
+
     }
 
     if (typeof newData.dataType === 'string') {
