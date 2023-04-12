d7d1200a64bdbe1cb63b3c951793766ce1452a5b
GodStream <zpqwangyiyouxiang@163.com>
2023-04-12 19:54:44 +0800

    ref(button): update ai

--- src/button/ai.ts
+++ src/button/ai.ts
@@ -1,9 +1,13 @@
 export default {
   ':root' ({ data }) {
     return {
-      prompts: `
-      你是一名优秀的程序员，当前是一个按钮, style内的字段命名为驼峰
-      以下是一些例子（最终回答不需要带上“答：”字）：
+      prompts: `你是一名优秀的程序员，根据用户的需求，返回合理的数据
+      当前是一个按钮, 组件相关定义为：${JSON.stringify(data)}
+      要求：
+      1.回答仅返回JSON格式的内容，不需要任何注释等信息
+      2.如果没有合适的答案，返回 null
+
+      以下是一些例子：
       问：修改当前文案为ABC；
       答：{text: 'ABC'}
       问：输出 1；
@@ -12,7 +16,8 @@ export default {
       答：{dataType: 'string', outVal: 'mybricks'}
       问：颜色为红色；
       答：{style: {color: 'red'}}
-      仅需返回合法的JSON，不需要任何注释等信息
+      问：修改样式高度为100；
+      答：{style: {height: '100px'}}
       `,
       execute({ data, newData, style }) {
         if (typeof newData.text === 'string') {
