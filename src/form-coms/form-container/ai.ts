3477e51a54d4b401720463b23ac6aedb858d9718
GodStream <zpqwangyiyouxiang@163.com>
2023-04-12 16:52:35 +0800

    ref(form-container): update ai

--- src/form-coms/form-container/ai.ts
+++ src/form-coms/form-container/ai.ts
@@ -1,7 +1,98 @@
 export default {
+
+  ':root'({ data }) {
+    return {
+      prompts: `
+      你是一名优秀的程序员，当前是一个表单容器,
+      添加组件的 namespace 必须由以下定义中选出进行组合：['form-text', 'select', 'radio', 'password', 'auto-complete', 'cascader', 'checkbox', 'date-picker', 'color', 'input-number', 'form-email', 'input-textarea', 'form-phone-number', 'range-picker', 'rate', 'search', 'slider', 'time-picker', 'time-range-picker', 'transfer', 'upload']
+      返回的数据类型定义为{type: 'addItem'|'updateForm', namespace: string, config: {label: stirng, name: string}}, 如果需要添加多个组件则返回数组结构
+      以下是一些例子：
+      问：添加输入框
+      答：{ type: 'addItem', namespace: 'form-text', config: { label: '输入框', name: 'name0' } }
+      问：输入框、下拉框
+      答：[{ type: 'addItem', namespace: 'form-text', config: { label: '输入框', name: 'name0' } },{ type: 'addItem', namespace: 'select', config: { label: '下拉框', name: 'name1'  } }]
+      问：文本与上传
+      答：[{ type: 'addItem', namespace: 'form-text', config: { label: '输入框', name: 'name0' } },{ type: 'addItem', namespace: 'upload', config: { label: '上传', name: 'name1'  } }]
+      问：简单的登录表单
+      答：[{ type: 'addItem', namespace: 'form-text', config: { label: '用户名', name: 'username' } },{ type: 'addItem', namespace: 'password', config: { label: '密码', name: 'password'  } }]
+      问：内联布局
+      答：{type: 'updateForm', data: { config: { layout: 'inline' } }}
+      问：每行4列
+      答：{type: 'updateForm', data: { formItemColumn: 4 }}
+      仅需返回合法的JSON，不需要任何注释等信息
+      `,
+      execute({ data, newData, slots }) {
+        // console.log(newData)
+        try {
+          const slot = slots.get('content')
+          const newItems: any[] = []
+    
+          if (Array.isArray(newData)) {
+            newData.forEach(item => {
+              if (item.type === 'addItem') {
+                const id = slot.addCom(`mybricks.normal-pc.${item.namespace}`)
+                newItems.push({ id, ...item.config })
+              }
+            })
+          } else {
+            if (newData.type === 'addItem') {
+              const id = slot.addCom(`mybricks.normal-pc.${newData.namespace}`)
+              newItems.push({ id, ...newData.config })
+            }
+    
+            if (newData.type === 'updateForm') {
+              if (typeof newData.data?.formItemColumn === 'number') {
+                data.formItemColumn = newData.data?.formItemColumn
+                data.actions.span = (24 / data.formItemColumn)
+                data.items.forEach(item => {
+                  item.span = (24 / data.formItemColumn);
+                })
+              }
+    
+              if (typeof newData.data?.config?.layout === 'string') {
+                data.config.layout = newData.data?.config?.layout
+              }
+            }
+          }
+    
+          setTimeout(() => { // hack...
+            newItems.forEach(newItem => {
+              const item = data.items.find(item => item.id === newItem.id)
+              console.log(item, newItem)
+              if (item) {
+                newItem.name && (item.name = newItem.name + '_' + newItem.id)
+                newItem.label && (item.label = newItem.label)
+              }
+            })
+            // console.log(data.items)
+          }, 100)
+    
+        } catch (ex) {
+          console.error(ex)
+        }
+      }
+    }
+  },
+//   '[data-menugroup]'({ data, focusArea }) {
+//     const menuIndex = focusArea.dataset['menugroup']
+
+//     const menuGroup = data.modules.navMenu.menus[parseInt(menuIndex)]
+
+//     return {
+//       prompts: `
+// 您是一位卓越的产品架构师，请根据用户的需求，设计一个合理的后台管理系统的模块.
+// 要求返回的答案是json格式.
+// 当前模块定义为:${JSON.stringify(menuGroup)}
+//       `,
+//       execute({ data, newData, slots }) {
+//         const menuGroup = data.modules.navMenu.menus[parseInt(menuIndex)]
+//         menuGroup.subMenus = newData.subMenus
+//       }
+//     }
+//   },
   def: {
     layout: "horizontal",
-    buttons: [{ type:'primary', title:'提交' }],
+    buttons: [{ type: 'primary', title: '提交' }],
     formItemColumn: 1
   },
   prompts: `
@@ -30,7 +121,7 @@ export default {
           }
       }
   `,
-  '@create' (props) {
+  '@create'(props) {
     const { def, data } = props
     // console.log('def:', def)
     if (def.buttons && def.buttons.length > 0) {
@@ -54,12 +145,12 @@ export default {
     setTimeout(() => {
       if (def.slots && def.slots.content && def.slots.content.length > 0) {
         def.slots.content.forEach((item, index) => {
-  
+
           if (data.items[index]) { // Todo...
             item.label && (data.items[index].label = item.label)
             item.name && (data.items[index].name = item.name)
           }
-          
+
         })
       }
     }, 100)
@@ -69,82 +160,82 @@ export default {
     }
     // console.log('data:', data)
   },
-  '@focus'({ data }) {
-    const { items, config, ...otherData } = data
+  // '@focus'({ data }) {
+  //   const { items, config, ...otherData } = data
 
-    return {
-      data: {
-        config
-      },
-      prompts: `
-      当前是一个表单容器,
-      添加组件的 namespace 必须由以下定义中选出进行组合：['form-text', 'select', 'radio', 'password', 'auto-complete', 'cascader', 'checkbox', 'date-picker', 'color', 'input-number', 'form-email', 'input-textarea', 'form-phone-number', 'range-picker', 'rate', 'search', 'slider', 'time-picker', 'time-range-picker', 'transfer', 'upload']
-      返回的数据类型定义为{type: 'addItem'|'updateForm', namespace: string, config: {label: stirng, name: string}}, 如果需要添加多个组件则返回数组结构
-      以下是一些例子：
-      问：添加输入框
-      答：{ type: 'addItem', namespace: 'form-text', config: { label: '输入框', name: 'name0' } }
-      问：输入框、下拉框
-      答：[{ type: 'addItem', namespace: 'form-text', config: { label: '输入框', name: 'name0' } },{ type: 'addItem', namespace: 'select', config: { label: '下拉框', name: 'name1'  } }]
-      问：文本与上传
-      答：[{ type: 'addItem', namespace: 'form-text', config: { label: '输入框', name: 'name0' } },{ type: 'addItem', namespace: 'upload', config: { label: '上传', name: 'name1'  } }]
-      问：简单的登录表单
-      答：[{ type: 'addItem', namespace: 'form-text', config: { label: '用户名', name: 'username' } },{ type: 'addItem', namespace: 'password', config: { label: '密码', name: 'password'  } }]
-      问：内联布局
-      答：{type: 'updateForm', data: { config: { layout: 'inline' } }}
-      问：每行4列
-      答：{type: 'updateForm', data: { formItemColumn: 4 }}
-      `
-    }
-  },
-  '@update'({ data, newData, slots }) { // 1. 动态添加组件，2. 样式问题  { namespace: mybricks.normal-pc.form-container }
-    // console.log(data, newData)
-    try {
-      const slot = slots.get('content')
-      const newItems: any[] = []
+  //   return {
+  //     data: {
+  //       config
+  //     },
+  //     prompts: `
+  //     当前是一个表单容器,
+  //     添加组件的 namespace 必须由以下定义中选出进行组合：['form-text', 'select', 'radio', 'password', 'auto-complete', 'cascader', 'checkbox', 'date-picker', 'color', 'input-number', 'form-email', 'input-textarea', 'form-phone-number', 'range-picker', 'rate', 'search', 'slider', 'time-picker', 'time-range-picker', 'transfer', 'upload']
+  //     返回的数据类型定义为{type: 'addItem'|'updateForm', namespace: string, config: {label: stirng, name: string}}, 如果需要添加多个组件则返回数组结构
+  //     以下是一些例子：
+  //     问：添加输入框
+  //     答：{ type: 'addItem', namespace: 'form-text', config: { label: '输入框', name: 'name0' } }
+  //     问：输入框、下拉框
+  //     答：[{ type: 'addItem', namespace: 'form-text', config: { label: '输入框', name: 'name0' } },{ type: 'addItem', namespace: 'select', config: { label: '下拉框', name: 'name1'  } }]
+  //     问：文本与上传
+  //     答：[{ type: 'addItem', namespace: 'form-text', config: { label: '输入框', name: 'name0' } },{ type: 'addItem', namespace: 'upload', config: { label: '上传', name: 'name1'  } }]
+  //     问：简单的登录表单
+  //     答：[{ type: 'addItem', namespace: 'form-text', config: { label: '用户名', name: 'username' } },{ type: 'addItem', namespace: 'password', config: { label: '密码', name: 'password'  } }]
+  //     问：内联布局
+  //     答：{type: 'updateForm', data: { config: { layout: 'inline' } }}
+  //     问：每行4列
+  //     答：{type: 'updateForm', data: { formItemColumn: 4 }}
+  //     `
+  //   }
+  // },
+  // '@update'({ data, newData, slots }) { // 1. 动态添加组件，2. 样式问题  { namespace: mybricks.normal-pc.form-container }
+  //   // console.log(data, newData)
+  //   try {
+  //     const slot = slots.get('content')
+  //     const newItems: any[] = []
 
-      if (Array.isArray(newData)) {
-        newData.forEach(item => {
-          if (item.type === 'addItem') {
-            const id = slot.addCom(`mybricks.normal-pc.${item.namespace}`)
-            newItems.push({ id, ...item.config })
-          }
-        })
-      } else {
-        if (newData.type === 'addItem') {
-          const id = slot.addCom(`mybricks.normal-pc.${newData.namespace}`)
-          newItems.push({ id, ...newData.config })
-        }
+  //     if (Array.isArray(newData)) {
+  //       newData.forEach(item => {
+  //         if (item.type === 'addItem') {
+  //           const id = slot.addCom(`mybricks.normal-pc.${item.namespace}`)
+  //           newItems.push({ id, ...item.config })
+  //         }
+  //       })
+  //     } else {
+  //       if (newData.type === 'addItem') {
+  //         const id = slot.addCom(`mybricks.normal-pc.${newData.namespace}`)
+  //         newItems.push({ id, ...newData.config })
+  //       }
 
-        if (newData.type === 'updateForm') {
-          if (typeof newData.data?.formItemColumn === 'number') {
-            data.formItemColumn = newData.data?.formItemColumn
-            data.actions.span = (24 / data.formItemColumn)
-            data.items.forEach(item => {
-              item.span = (24 / data.formItemColumn);
-            })
-          }
+  //       if (newData.type === 'updateForm') {
+  //         if (typeof newData.data?.formItemColumn === 'number') {
+  //           data.formItemColumn = newData.data?.formItemColumn
+  //           data.actions.span = (24 / data.formItemColumn)
+  //           data.items.forEach(item => {
+  //             item.span = (24 / data.formItemColumn);
+  //           })
+  //         }
 
-          if (typeof newData.data?.config?.layout === 'string') {
-            data.config.layout = newData.data?.config?.layout
-          }
-        }
-      }
+  //         if (typeof newData.data?.config?.layout === 'string') {
+  //           data.config.layout = newData.data?.config?.layout
+  //         }
+  //       }
+  //     }
 
-      setTimeout(() => { // hack...
-        newItems.forEach(newItem => {
-          const item = data.items.find(item => item.id === newItem.id)
-          console.log(item, newItem)
-          if (item) {
-            newItem.name && (item.name = newItem.name + '_' + newItem.id)
-            newItem.label && (item.label = newItem.label)
-          }
-        })
-        // console.log(data.items)
-      }, 100)
-      
-    } catch (ex) {
-      console.error(ex)
-    }
-    
-  }
+  //     setTimeout(() => { // hack...
+  //       newItems.forEach(newItem => {
+  //         const item = data.items.find(item => item.id === newItem.id)
+  //         console.log(item, newItem)
+  //         if (item) {
+  //           newItem.name && (item.name = newItem.name + '_' + newItem.id)
+  //           newItem.label && (item.label = newItem.label)
+  //         }
+  //       })
+  //       // console.log(data.items)
+  //     }, 100)
+
+  //   } catch (ex) {
+  //     console.error(ex)
+  //   }
+
+  // }
 }
