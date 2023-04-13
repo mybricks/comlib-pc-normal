20676e921eff51196f37a827c131a30cc221901a
Bigbig-Huang <hqy09092020@163.com>
2023-04-13 11:52:00 +0800

    feat(_post-message & _receive-message & iframe):add postmessage

---
+++ src/_post-message/runtime.tsx
@@ -0,0 +1,32 @@
+export interface Data {
+  iframeId: string;
+  goal: 'subPage' | 'parentPage';
+}
+
+export interface Item {
+  url: string;
+  id: string;
+}
+
+export default function (props: RuntimeParams<Data>) {
+  const { env, inputs, data } = props;
+  const { runtime } = env;
+
+  if (runtime) {
+    inputs['post']((val) => {
+      //父页面向子页面传递数据
+      if (data.goal === 'subPage' && data.iframeId !== '') {
+        let iFramexDom = document.getElementById(data.iframeId);
+        let iFramex = document.getElementById(data.iframeId).contentWindow;
+        iFramexDom.onload = function () {
+          iFramex.postMessage(val, '*');
+        };
+      }
+
+      //子页面向父页面传递数据
+      if (data.goal === 'parentPage') {
+        parent.postMessage(val, '*');
+      }
+    });
+  }
+}
