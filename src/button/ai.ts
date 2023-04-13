ca1411055a3645b7c123d11d7a52a72a38847319
GodStream <zpqwangyiyouxiang@163.com>
2023-04-13 17:43:48 +0800

    ref(button): update ai

--- src/button/ai.ts
+++ src/button/ai.ts
@@ -8,16 +8,16 @@ export default {
       2.如果没有合适的答案，返回 null
 
       以下是一些例子：
-      问：修改当前文案为ABC；
-      答：{text: 'ABC'}
-      问：输出 1；
-      答：{dataType: 'number', outVal: 1}
-      问：输出 mybricks；
-      答：{dataType: 'string', outVal: 'mybricks'}
-      问：颜色为红色；
-      答：{style: {color: 'red'}}
-      问：修改样式高度为100；
-      答：{style: {height: '100px'}}
+      请回答：修改当前文案为ABC；
+      {text: 'ABC'}
+      请回答：输出 1；
+      {dataType: 'number', outVal: 1}
+      请回答：输出 mybricks；
+      {dataType: 'string', outVal: 'mybricks'}
+      请回答：颜色为红色；
+      {style: {color: 'red'}}
+      请回答：修改样式高度为100；
+      {style: {height: '100px'}}
       `,
       execute({ data, newData, style }) {
         if (typeof newData.text === 'string') {
