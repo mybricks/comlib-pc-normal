84332d8737ded3f14c7df96fe44654efc4523779
tangxiaoxin <tangxiaoxin@kuaishou.com>
2023-04-13 21:54:44 +0800

    row col

--- src/grid/ai/row/actions.ts
+++ src/grid/ai/row/actions.ts
@@ -7,7 +7,7 @@ enum ACTION_TYPE {
   STYLE = 'style'
 }
 
-const addRow = (props) => {
+const insertRow = (props) => {
   const { data, slots } = props;
   const preRow = data.rows.slice().pop();
   const rowKey = uuid();
@@ -24,32 +24,36 @@ const addRow = (props) => {
   data.rows.push(newRow);
 };
 
-const addCom = (props) => {
+const insertCom = (props) => {
   const { slots, newData } = props;
-  if (newData.data.namespace) {
-    const slot = slots.get(newData.data.slot);
-    if (slot) {
-      slot.addCom(newData.data.namespace);
+  (newData[ACTION_TYPE.ADD_COM] || []).forEach((col) => {
+    const slot = slots.get(col.slot);
+    if (slot && col.coms) {
+      col.coms.forEach((namespace) => {
+        slot.addCom(namespace);
+      });
     }
-  }
+  });
 };
+
 const handleStyle = (props) => {
   const { data, newData, focusArea } = props;
   if (!focusArea) return;
   const row = getRowItem(data, focusArea);
-  row.backgroundColor = newData.data.backgroundColor;
+  row.backgroundColor = newData[ACTION_TYPE.STYLE].backgroundColor;
 };
 
 const handleLayout = (props) => {
   const { data, newData, focusArea } = props;
   if (!focusArea) return;
   const row = getRowItem(data, focusArea);
-  row.justify = newData.data.justify || row.justify;
-  row.align = newData.data.align || row.align;
+  row.justify = newData[ACTION_TYPE.LAYOUT].justify || row.justify;
+  row.align = newData[ACTION_TYPE.LAYOUT].align || row.align;
 };
+
 const actions = {
-  [ACTION_TYPE.ADD_COM]: addCom,
-  [ACTION_TYPE.ADD_ROW]: addRow,
+  [ACTION_TYPE.ADD_COM]: insertCom,
+  [ACTION_TYPE.ADD_ROW]: insertRow,
   [ACTION_TYPE.LAYOUT]: handleLayout,
   [ACTION_TYPE.STYLE]: handleStyle
 };
--- src/grid/ai/row/index.ts
+++ src/grid/ai/row/index.ts
@@ -1,30 +1,37 @@
 import { actions, ACTION_TYPE } from './actions';
+import { isObject } from '../../../utils/types';
 export default {
   '[data-type-row]'() {
     return {
       prompts: `
             以下是一些例子（最终回答不需要带上“答：”字）：
             问：添加一行
-            答：{type: ${JSON.stringify(ACTION_TYPE.ADD_ROW)}, data: [
+            答：{${JSON.stringify(ACTION_TYPE.ADD_ROW)}: [
                 {key: "column_0", widthOption: 'px', span: 300, slot: "slot_0"},
                 {key: "column_1", widthOption: 'span', span: 8, slot: "slot_1"}, 
                 {key: "column_2", widthOption: 'auto', flex: 1, slot: "slot_2"}
             ]}
             问：左对齐
-            答：{type: ${JSON.stringify(ACTION_TYPE.LAYOUT)}, data: {justify: "start"}
+            答：{${JSON.stringify(ACTION_TYPE.LAYOUT)}: {justify: "start"}}
             问：垂直居中
-            答：{type: ${JSON.stringify(ACTION_TYPE.LAYOUT)}, data: {align: "middle"}
+            答：{${JSON.stringify(ACTION_TYPE.LAYOUT)}: {align: "middle"}}
             问：底部对齐
-            答：{type: ${JSON.stringify(ACTION_TYPE.LAYOUT)}, data: {align: "bottom"}
+            答：{${JSON.stringify(ACTION_TYPE.LAYOUT)}: {align: "bottom"}}
             问：背景色
-            答：{type: ${JSON.stringify(ACTION_TYPE.STYLE)}, data: {backgroundColor: "red"}
-            问：第一列添加文本
-            答：{type: "addCom", data: { key: "column_0", slot: "slot_0", namespace: "mybricks.normal-pc.text"}}
+            答：{${JSON.stringify(ACTION_TYPE.STYLE)}: {backgroundColor: "red"}}
+            问：第一列添加文本和按钮
+            答：{${JSON.stringify(
+              ACTION_TYPE.ADD_COM
+            )}: [{ key: "column_0", slot: "slot_0", coms: ["mybricks.normal-pc.text", "mybricks.normal-pc.button"]}]}
             `,
       execute(props) {
         const { newData } = props;
-        if (newData.type && newData.type in actions) {
-          actions[newData.type](props);
+        if (isObject(newData)) {
+          Object.keys(newData).forEach((key) => {
+            if (key in actions) {
+              actions[key](props);
+            }
+          });
         }
       }
     };
