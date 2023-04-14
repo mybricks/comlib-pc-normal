95c045b4a08fbb97570e822943bbf932d1aa787f
tangxiaoxin <tangxiaoxin@kuaishou.com>
2023-04-14 14:49:07 +0800

    release: grid

--- src/grid/ai/index.ts
+++ src/grid/ai/index.ts
@@ -1,104 +1,87 @@
 import RowAi from './row';
 import ColAi from './col';
 import { createColBySpan } from '../editors/utils';
-import { isArray, isObject } from '../../utils/types';
+import { isObject } from 'lodash';
+
+const prompts = `
+  你是一名低代码搭建工程师，当前是一个栅格布局组件，添加栅格时请按照以下格式返回
+  {columns: [
+    {key: "column_1", widthOption: 'span', span: 8, slot: "slot_1", content: []}, 
+    {key: "column_2", widthOption: 'auto', flex: 1, slot: "slot_2"}
+  ]}
+  以下是一些搭建例子：
+  问：添加一个两列布局
+  答：{columns: [
+          {key: "column_0", widthOption: 'span', span: 8, slot: "slot_0"}, content: ["mybricks.normal-pc.text"]
+          {key: "column_1", widthOption: 'auto', flex: 1, slot: "slot_1"}
+      ]
+  }
+  问：添加一个2列栅格，第一列包含文本组件，第二列包含表格组件
+  答：{columns: [
+    {key: "column_0", slot: "slot_0", content: ["mybricks.normal-pc.text"]},
+    {key: "column_1", slot: "slot_1", content: ["mybricks.normal-pc.table"]}
+  ]}
+  `;
+
 export default {
   ':root'({ data }) {
     return {
-      prompts: `
-            你是一名低代码搭建工程师，当前是一个栅格布局组件，添加栅格时请按照以下格式返回
-            {cols: [
-              {key: "column_1", widthOption: 'span', span: 8, slot: "slot_1"}, 
-              {key: "column_2", widthOption: 'auto', flex: 1, slot: "slot_2"}
-            ]}
-            一下是一些搭建例子：
-            问：2列布局
-            答：{type: 'mybricks.normal-pc.grid', columns: [
-                    {key: "column_1", widthOption: 'span', span: 8, slot: "slot_1"}, 
-                    {key: "column_2", widthOption: 'auto', flex: 1, slot: "slot_2"}
-                ]
-            }
-            问：栅格第一列包含文本组件
-            答：{type: 'mybricks.normal-pc.grid', columns: [{widthOption: 'px', width: 280, slots: {content: []}}]}
-        `,
+      prompts,
       execute({ data, newData, slots }) {
         console.log(newData);
       }
     };
   },
+  prompts,
   '@create'(props) {
-    const { data, def, slots, slot } = props;
-    console.log(def);
-    const dataColCount = data.rows[0].columns.length;
-    const defColCount = analyzeColCount({ def });
-    if (defColCount < dataColCount) {
-      data.rows[0].columns.splice(defColCount, dataColCount - defColCount);
-    } else if (defColCount > dataColCount) {
-      data.rows[0].columns = data.rows[0].columns.concat(
-        createCols({ slots, count: defColCount - dataColCount })
-      );
-    }
-    if ('slots' in def) {
-      const slotContentMap = analyzeSlotContent({ def, slots });
-      for (const [id, coms] of Object.entries(slotContentMap || {})) {
-        const slot = slots.get(id);
-        coms.forEach((com) => {
-          slot.addCom(com);
-        });
+    try {
+      const { data, def, slots } = props;
+      console.log(def);
+      if ('columns' in def) {
+        const legacyColumns = data.rows[0].columns;
+        const dataColCount = legacyColumns.length;
+        const defColCount = def.columns.length;
+        if (defColCount <= dataColCount) {
+          legacyColumns.splice(defColCount, dataColCount - defColCount);
+          insertCom({ columns: legacyColumns, slots });
+        } else {
+          const newCols = createCols({ slots, columns: def.columns.slice(dataColCount) });
+          legacyColumns.splice(dataColCount, 0, ...newCols);
+          const list = legacyColumns.map((column, index) => {
+            return { ...column, content: def.columns[index].content };
+          });
+          insertCom({
+            columns: list,
+            slots
+          });
+        }
       }
+    } catch (error) {
+      throw Error(`【chatGPT回答数据解析失败】：${error}`);
     }
   },
   ...RowAi,
   ...ColAi
 };
 
-const analyzeColCount = ({ def }) => {
-  let defColCount = 0;
-  if ('columns' in def) {
-    defColCount = typeof def.columns === 'number' ? def.columns : def.columns.length;
-  }
-  if ('cols' in def) {
-    defColCount = typeof def.cols === 'number' ? def.cols : def.cols.length;
-  }
-  if ('props' in def && 'columns' in def.props) {
-    defColCount =
-      typeof def.props.columns === 'number' ? def.props.columns : def.props.columns.length;
-  }
-  if ('props' in def && 'cols' in def.props) {
-    defColCount = typeof def.props.cols === 'number' ? def.props.cols : def.props.cols.length;
-  }
-  return defColCount;
-};
-
-const createCols = ({ slots, count }) => {
-  let i = count;
-  const cols: any = [];
-  while (i-- > 0) {
-    const col = createColBySpan();
-    cols.push(col);
-    slots.add(col.slot, `col-${col.span}`);
-  }
-  return cols;
-};
-
-const analyzeSlotContent = ({ def, slots }) => {
-  const slotContent = {};
-  if (isArray(def.slots)) {
-  }
-  if (isObject(def.slots)) {
-    let index = 0;
-    for (const [key, val] of Object.entries(def.slots)) {
-      let coms: Array<string> = [],
-        slotId = key;
-      if (isArray(val)) {
-        coms = val.map((com) => com.type);
-      }
-      if (!slots.get(key)) {
-        slotId = `slot_${index}`;
-        index++;
-      }
-      slotContent[slotId] = coms;
+const insertCom = ({ columns, slots }) => {
+  (columns || []).forEach((column) => {
+    const { slot, content } = column;
+    const slotInstance = slots.get(slot);
+    if (!!content && Array.isArray(content) && slotInstance) {
+      content.forEach((item) => {
+        let namespace = item;
+        if (isObject(item)) namespace = item.type;
+        slotInstance.addCom(namespace);
+      });
     }
-  }
-  return slotContent;
+  });
+};
+
+const createCols = ({ slots, columns }) => {
+  return (columns || []).map((column) => {
+    const newColumn = createColBySpan(column.span);
+    slots.add(newColumn.slot, `col-${newColumn.span}`);
+    return newColumn;
+  });
 };
