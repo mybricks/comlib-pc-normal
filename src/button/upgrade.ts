1a3984039fa0df6904fa3f0c1778c5704b048de6
Bigbig-Huang <hqy09092020@163.com>
2023-04-19 17:25:32 +0800

    feat(button):fix button outputVal

--- src/button/upgrade.ts
+++ src/button/upgrade.ts
@@ -1,6 +1,6 @@
-import { Data } from './constants';
+import { Data, OutputIds } from './constants';
 
-export default function ({ input, output }: UpgradeParams<Data>): boolean {
+export default function ({ input, output, data }: UpgradeParams<Data>): boolean {
   /**
    * @description v1.0.3 , 新增按钮动态标题
   */
@@ -9,6 +9,20 @@ export default function ({ input, output }: UpgradeParams<Data>): boolean {
   }
   if (!input.get('dynamicTitle')) {
     input.add('dynamicTitle', '设置标题', valueSchema);
+  }
+
+  /**
+   * @description v1.0.5 , fix setSchema问题
+  */
+  const click = output.get(OutputIds.Click);
+  const dbClick = output.get(OutputIds.DbClick);
+  if (data.dataType === 'number') {
+    click.setSchema({
+      type: 'number'
+    });
+    dbClick.setSchema({
+      type: 'number'
+    });
   }
   return true;
 }
