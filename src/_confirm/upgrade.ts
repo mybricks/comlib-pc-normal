2e51496a72644425c5c5093800f5efc98c4bb7cc
z35635 <linzhujlu@foxmail.com>
2023-04-24 17:58:33 +0800

    chore(_confirm): upgrade 1.0.2

---
+++ src/_confirm/upgrade.ts
@@ -0,0 +1,17 @@
+import { Data, OutputIds } from './constants';
+
+export default function ({
+  data,
+  output
+}: UpgradeParams<Data>): boolean {
+
+  /**
+     * @description v1.0.2 
+     */
+  const cancelSchema = output.get(OutputIds.Ok).schema;
+  if (data.type === 'confirm' && !output.get(OutputIds.Cancel)) {
+    output.add(OutputIds.Cancel, '取消', cancelSchema);
+  }
+
+  return true;
+}
