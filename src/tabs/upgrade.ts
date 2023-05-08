22dc453dec76444003b0f7b2581abdbcb2338dd3
tangxiaoxin <tangxiaoxin@kuaishou.com>
2023-05-08 20:53:19 +0800

    upgrade: tabs

---
+++ src/tabs/upgrade.ts
@@ -0,0 +1,18 @@
+import { Data } from './constants';
+
+export default function ({ input, output, data }: UpgradeParams<Data>): boolean {
+  data.tabList.forEach(({ key, name }) => {
+    if (!output.get(`${key}_into`)) {
+      output.add(`${key}_into`, `${name}显示`, { type: 'any' });
+    }
+    if (!output.get(`${key}_leave`)) {
+      output.add(`${key}_leave`, `${name}隐藏`, { type: 'any' });
+    }
+    if (!input.get(key)) {
+      input.add(key, `${name}的通知数`, {
+        type: 'string'
+      });
+    }
+  });
+  return true;
+}
