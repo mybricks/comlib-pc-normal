e26b3db4b314a2ac97c93d11d6aec5d265ec2d82
z35635 <linzhujlu@foxmail.com>
2023-05-10 15:27:34 +0800

    ref(form-detail): ItemsEditors

--- src/form-detail/editors/item/index.ts
+++ src/form-detail/editors/item/index.ts
@@ -5,13 +5,15 @@ import { IndexEditor } from './indexEditor';
 
 const itemKey = '.ant-descriptions-item';
 export const ItemsEditors = {
-  [itemKey]: ({}: EditorResult<Data>, cate1, cate2, cate3) => {
-    cate1.title = '常规';
-    cate1.items = [...BaseEditor, ...IndexEditor];
+  [itemKey]: {
+    title: '描述项',
+    items: ({ }: EditorResult<Data>, cate1, cate2, cate3) => {
+      cate1.title = '常规';
+      cate1.items = [...BaseEditor, ...IndexEditor];
 
-    cate2.title = '样式';
-    cate2.items = [...StyleEditor];
+      cate2.title = '样式';
+      cate2.items = [...StyleEditor];
 
-    return { title: '描述项' };
+    }
   }
 };
