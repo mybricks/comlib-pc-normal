b4a22bbd859436b18263c69b0589e910f5594c27
z35635 <linzhujlu@foxmail.com>
2023-05-17 21:47:41 +0800

    feat(json-view): BackgroundColor

--- src/json-view/runtime.tsx
+++ src/json-view/runtime.tsx
@@ -3,7 +3,7 @@ import { Alert, Tooltip, Tree, message } from 'antd';
 import React, { useEffect, useMemo, useState } from 'react';
 import copy from 'copy-to-clipboard';
 import { typeCheck, uuid } from '../utils';
-import { Data, dataSourceTypeMap, InputIds, OutputIds } from './constant';
+import { Data, dataSourceTypeMap, InputIds, OutputIds, TypeEnum } from './constant';
 import css from './runtime.less';
 
 export default function ({ env, data, inputs, outputs, title }: RuntimeParams<Data>) {
@@ -173,6 +173,10 @@ export default function ({ env, data, inputs, outputs, title }: RuntimeParams<Da
   return (
     <Tree
       treeData={treeData}
+      rootStyle={{
+        backgroundColor: data.colors[TypeEnum.BackgroundColor]
+      }}
+      className={css.root}
       showLine={{ showLeafIcon: false }}
       switcherIcon={<DownOutlined />}
       defaultExpandedKeys={expandedKeys}
