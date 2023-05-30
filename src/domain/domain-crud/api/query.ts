ffe485aa7b05366ec4584abf05c1770e275df3b2
GodStream <zpqwangyiyouxiang@163.com>
2023-05-30 17:53:42 +0800

    ref: update

--- src/domain/domain-crud/api/query.ts
+++ src/domain/domain-crud/api/query.ts
@@ -2,7 +2,6 @@ import { ajax } from '../util'
 
 export default function queryData(domainConfig, { query, pageParams, ordersParams }) {
   console.log('domainConfig', domainConfig)
-  const pagination = true
   const { fields = [] } = domainConfig
   let subEntitis = new Set()
   // 额外添加子实体的名称，如"角色.名称"中的”角色“
@@ -22,14 +21,9 @@ export default function queryData(domainConfig, { query, pageParams, ordersParam
     ajax({
       params: {
         query,
-        fields: fields,
+        fields,
         orders: ordersParams,
-        // orders: [
-        //   ...orderFields,
-        //   hasPrimaryFieldOrder ? undefined : { fieldId: primaryField.id, order: 'DESC' }
-        // ].filter(Boolean),
         page: {
-          pagination,
           pageNum: pageParams.pageNum,
           pageSize: pageParams.pageSize
         },
@@ -40,17 +34,15 @@ export default function queryData(domainConfig, { query, pageParams, ordersParam
       // projectId: projectId || undefined
     }).then(r => {
 
-      if (pagination) {
-        resolve({
-          dataSource: r.dataSource,
-          pageNum: r.pageNum,
-          pageSize: r.pageSize,
-          total: r.total
-        })
-      } else {
-        resolve(r)
-      }
+      resolve({
+        dataSource: r.dataSource,
+        pageNum: r.pageNum,
+        pageSize: r.pageSize,
+        total: r.total
+      })
 
+    }).catch(e => {
+      reject(e)
     })
   })
 
