2c87e56e47a8cdd515050aac3247544ce8072417
z35635 <linzhujlu@foxmail.com>
2023-05-30 17:51:31 +0800

    ref(button): data.asMapArea

--- src/button/editors.tsx
+++ src/button/editors.tsx
@@ -11,23 +11,7 @@ export default {
     options: ['width', 'height']
   },
   ':root': {
-    style: {
-      title: '按钮',
-      options: ['border', 'font', 'bgcolor', 'bgimage']
-      // initValue: {
-      //   color: '#FFF',
-      //   fontWeight: 400,
-      //   fontStyle: 'normal',
-      //   fontSize: '14px',
-      //   lineHeight: 1.5715,
-      //   background: '#1890ff',
-      //   borderColor: '#1890ff',
-      //   borderWidth: '1px',
-      //   borderStyle: 'solid'
-      // },
-      // target: '.button'
-    },
-    items: [
+    style: [
       {
         title: '作为热区使用',
         type: 'switch',
@@ -40,6 +24,16 @@ export default {
           }
         }
       },
+      {
+        title: '按钮',
+        ifVisible({ data }: EditorResult<Data>) {
+          return !data.asMapArea;
+        },
+        options: ['border', 'font', 'bgcolor', 'bgimage'],
+        target: '.button'
+      }
+    ],
+    items: [
       {
         ifVisible({ data }: EditorResult<Data>) {
           return !data.asMapArea;
