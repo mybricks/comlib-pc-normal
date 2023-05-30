84f5d0de407180a625281e6ec5c6634721d2dc4d
Bigbig-Huang <hqy09092020@163.com>
2023-05-30 21:39:11 +0800

    feat(alert):add style

---
+++ src/alert/upgrade.ts
@@ -0,0 +1,17 @@
+import { Data } from './constants';
+
+export default function ({ data, setDeclaredStyle }: UpgradeParams<Data>): boolean {
+  /**
+   * @description v1.0.0 -> v1.0.1, 新增标题和介绍文案字体样式大小
+  */
+  if (typeof data.size === "undefined") {
+    data.size = "16px"
+  };
+
+  //兼容之前颜色自定义情况
+  if(data.isColor){
+    setDeclaredStyle(`.ant-alert-description`, data.textColor);
+  }
+  
+  return true;
+}
