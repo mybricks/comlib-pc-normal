899804c498eb2bc4559706f8e2f20c3804a55d2f
z35635 <linzhujlu@foxmail.com>
2023-06-07 19:50:39 +0800

    chore(time-picker): remove style

--- src/form-coms/time-picker/runtime.tsx
+++ src/form-coms/time-picker/runtime.tsx
@@ -108,7 +108,7 @@ export default function ({
   }, [format, customFormat]);
 
   return (
-    <div className={styles.wrap} style={style}>
+    <div className={styles.wrap}>
       <TimePicker
         placeholder={placeholder}
         value={value}
