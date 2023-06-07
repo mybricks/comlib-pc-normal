9f8556a1783ff0294741097fe261e8aa3443a656
z35635 <linzhujlu@foxmail.com>
2023-06-07 16:19:02 +0800

    feat(form-text): style

--- src/form-coms/text/editors.tsx
+++ src/form-coms/text/editors.tsx
@@ -7,199 +7,205 @@ export default {
   '@init': ({ style }) => {
     style.width = '100%';
   },
-  ':root'({ data }: EditorResult<{ type }>, ...catalog) {
-    catalog[0].title = '常规';
+  ':root': {
+    style: {
+      options: ['border'],
+      target: '.ant-input-affix-wrapper'
+    },
+    items: ({ data }: EditorResult<{ type }>, ...catalog) => {
+      catalog[0].title = '常规';
 
-    catalog[0].items = [
-      {
-        title: '提示内容',
-        type: 'Text',
-        description: '该提示内容会在值为空时显示',
-        value: {
-          get({ data }) {
-            return data.config.placeholder;
-          },
-          set({ data }, value: string) {
-            data.config.placeholder = value;
+      catalog[0].items = [
+        {
+          title: '提示内容',
+          type: 'Text',
+          description: '该提示内容会在值为空时显示',
+          value: {
+            get({ data }) {
+              return data.config.placeholder;
+            },
+            set({ data }, value: string) {
+              data.config.placeholder = value;
+            }
           }
-        }
-      },
-      {
-        title: '显示清除图标',
-        type: 'switch',
-        description: '可以点击清除图标删除内容',
-        value: {
-          get({ data }) {
-            return data.config.allowClear;
-          },
-          set({ data }, value: boolean) {
-            data.config.allowClear = value;
+        },
+        {
+          title: '显示清除图标',
+          type: 'switch',
+          description: '可以点击清除图标删除内容',
+          value: {
+            get({ data }) {
+              return data.config.allowClear;
+            },
+            set({ data }, value: boolean) {
+              data.config.allowClear = value;
+            }
           }
-        }
-      },
-      {
-        title: '前置标签',
-        type: 'text',
-        description: '带标签的 input，设置前置标签',
-        value: {
-          get({ data }) {
-            return data.config.addonBefore;
-          },
-          set({ data }, value: string) {
-            data.config.addonBefore = value;
+        },
+        {
+          title: '前置标签',
+          type: 'text',
+          description: '带标签的 input，设置前置标签',
+          value: {
+            get({ data }) {
+              return data.config.addonBefore;
+            },
+            set({ data }, value: string) {
+              data.config.addonBefore = value;
+            }
           }
-        }
-      },
-      {
-        title: '后置标签',
-        type: 'text',
-        description: '带标签的 input，设置后置标签',
-        value: {
-          get({ data }) {
-            return data.config.addonAfter;
-          },
-          set({ data }, value: string) {
-            data.config.addonAfter = value;
+        },
+        {
+          title: '后置标签',
+          type: 'text',
+          description: '带标签的 input，设置后置标签',
+          value: {
+            get({ data }) {
+              return data.config.addonAfter;
+            },
+            set({ data }, value: string) {
+              data.config.addonAfter = value;
+            }
           }
-        }
-      },
-      {
-        title: '禁用状态',
-        type: 'switch',
-        description: '是否禁用状态',
-        value: {
-          get({ data }) {
-            return data.config.disabled;
-          },
-          set({ data }, value: boolean) {
-            data.config.disabled = value;
+        },
+        {
+          title: '禁用状态',
+          type: 'switch',
+          description: '是否禁用状态',
+          value: {
+            get({ data }) {
+              return data.config.disabled;
+            },
+            set({ data }, value: boolean) {
+              data.config.disabled = value;
+            }
           }
-        }
-      },
-      {
-        title: '显示字数',
-        type: 'switch',
-        description: '是否展示字数',
-        value: {
-          get({ data }) {
-            return data.config.showCount;
-          },
-          set({ data }, value: boolean) {
-            data.config.showCount = value;
+        },
+        {
+          title: '显示字数',
+          type: 'switch',
+          description: '是否展示字数',
+          value: {
+            get({ data }) {
+              return data.config.showCount;
+            },
+            set({ data }, value: boolean) {
+              data.config.showCount = value;
+            }
           }
-        }
-      },
-      {
-        title: '最大长度',
-        type: 'InputNumber',
-        description: '可输入的内容最大长度, -1 为不限制',
-        options: [{ min: -1 }],
-        value: {
-          get({ data }) {
-            return [data.config.maxLength];
-          },
-          set({ data }, value: number) {
-            data.config['maxLength'] = value[0];
+        },
+        {
+          title: '最大长度',
+          type: 'InputNumber',
+          description: '可输入的内容最大长度, -1 为不限制',
+          options: [{ min: -1 }],
+          value: {
+            get({ data }) {
+              return [data.config.maxLength];
+            },
+            set({ data }, value: number) {
+              data.config['maxLength'] = value[0];
+            }
           }
-        }
-      },
-      {
-        title: '校验规则',
-        description: '提供快捷校验配置',
-        type: 'ArrayCheckbox',
-        options: {
-          checkField: 'status',
-          visibleField: 'visible',
-          getTitle,
+        },
+        {
+          title: '校验规则',
+          description: '提供快捷校验配置',
+          type: 'ArrayCheckbox',
+          options: {
+            checkField: 'status',
+            visibleField: 'visible',
+            getTitle,
+            items: [
+              // {
+              //   title: '提示文字',
+              //   description: '提示文字的表达式（{}, =, <, >, ||, &&）, 例：${label}不能为空',
+              //   type: 'EXPRESSION',
+              //   options: {
+              //     autoSize: true,
+              //     placeholder: '例：${label}不能为空',
+              //     // suggestions: getSuggestions(true),
+              //   },
+              //   value: 'message'
+              // },
+              {
+                title: '提示文字',
+                type: 'Text',
+                value: 'message',
+                ifVisible(item: any, index: number) {
+                  return item.key === RuleKeys.REQUIRED;
+                }
+              },
+              {
+                title: '编辑校验规则',
+                type: 'code',
+                options: {
+                  language: 'javascript',
+                  enableFullscreen: false,
+                  title: '编辑校验规则',
+                  width: 600,
+                  minimap: {
+                    enabled: false
+                  },
+                  babel: true,
+                  eslint: {
+                    parserOptions: {
+                      ecmaVersion: '2020',
+                      sourceType: 'module'
+                    }
+                  }
+                },
+                ifVisible(item: any, index: number) {
+                  return item.key === RuleKeys.CODE_VALIDATOR;
+                },
+                value: 'validateCode'
+              }
+            ]
+          },
+          value: {
+            get({ data }) {
+              return data.rules.length > 0 ? data.rules : defaultRules;
+            },
+            set({ data }, value: any) {
+              data.rules = value;
+            }
+          }
+        },
+        {
+          title: '事件',
           items: [
-            // {
-            //   title: '提示文字',
-            //   description: '提示文字的表达式（{}, =, <, >, ||, &&）, 例：${label}不能为空',
-            //   type: 'EXPRESSION',
-            //   options: {
-            //     autoSize: true,
-            //     placeholder: '例：${label}不能为空',
-            //     // suggestions: getSuggestions(true),
-            //   },
-            //   value: 'message'
-            // },
             {
-              title: '提示文字',
-              type: 'Text',
-              value: 'message',
-              ifVisible(item: any, index: number) {
-                return item.key === RuleKeys.REQUIRED;
+              title: '值初始化',
+              type: '_event',
+              options: {
+                outputId: 'onInitial'
               }
             },
             {
-              title: '编辑校验规则',
-              type: 'code',
+              title: '值更新',
+              type: '_event',
               options: {
-                language: 'javascript',
-                enableFullscreen: false,
-                title: '编辑校验规则',
-                width: 600,
-                minimap: {
-                  enabled: false
-                },
-                babel: true,
-                eslint: {
-                  parserOptions: {
-                    ecmaVersion: '2020',
-                    sourceType: 'module'
-                  }
-                }
-              },
-              ifVisible(item: any, index: number) {
-                return item.key === RuleKeys.CODE_VALIDATOR;
-              },
-              value: 'validateCode'
+                outputId: 'onChange'
+              }
+            },
+            {
+              title: '失去焦点',
+              type: '_event',
+              options: {
+                outputId: 'onBlur'
+              }
+            },
+            {
+              title: '按下回车',
+              type: '_event',
+              options: {
+                outputId: 'onPressEnter'
+              }
             }
           ]
-        },
-        value: {
-          get({ data }) {
-            return data.rules.length > 0 ? data.rules : defaultRules;
-          },
-          set({ data }, value: any) {
-            data.rules = value;
-          }
         }
-      },
-      {
-        title: '事件',
-        items: [
-          {
-            title: '值初始化',
-            type: '_event',
-            options: {
-              outputId: 'onInitial'
-            }
-          },
-          {
-            title: '值更新',
-            type: '_event',
-            options: {
-              outputId: 'onChange'
-            }
-          },
-          {
-            title: '失去焦点',
-            type: '_event',
-            options: {
-              outputId: 'onBlur'
-            }
-          },
-          {
-            title: '按下回车',
-            type: '_event',
-            options: {
-              outputId: 'onPressEnter'
-            }
-          }
-        ]
-      }
-    ];
+      ];
+    }
   }
 };
 
