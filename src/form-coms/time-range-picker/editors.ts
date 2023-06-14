8018b3d4516b03df64fa8d38ece7574f20b796dd
z35635 <linzhujlu@foxmail.com>
2023-06-14 16:03:35 +0800

    feat(time-range-picker): hover style

--- src/form-coms/time-range-picker/editors.ts
+++ src/form-coms/time-range-picker/editors.ts
@@ -8,10 +8,18 @@ export default {
     style.width = '100%';
   },
   ':root': {
-    style: {
-      options: ['border'],
-      target: '.ant-picker'
-    },
+    style: [
+      {
+        title: '默认样式',
+        options: ['border'],
+        target: '.ant-picker'
+      },
+      {
+        title: '激活样式',
+        options: ['border'],
+        target: '.ant-picker:hover'
+      },
+    ],
     items: ({ data }: EditorResult<Data>, ...cate) => {
       cate[0].title = '配置';
       cate[0].items = [
