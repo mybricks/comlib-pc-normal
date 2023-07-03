ee5b0f0961acf7dd37aff51ab9496f1ea89681f5
tangxiaoxin <1302947749@qq.com>
2023-07-03 11:01:38 +0800

    fix: enum-map desc

--- src/_enum-map/editor.ts
+++ src/_enum-map/editor.ts
@@ -16,7 +16,7 @@ export default {
     {
       title: '值取键',
       type: 'switch',
-      description: '默认根据输入键名获取键值',
+      description: '默认根据输入键值获取键名',
       value: {
         get({ data }: EditorResult<Data>) {
           return !!data.valueToKey;
