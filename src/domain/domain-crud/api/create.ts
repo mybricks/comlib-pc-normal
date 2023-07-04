50e9269b958b3361188e51a9c2548156d8e63ab8
GodStream <zpqwangyiyouxiang@163.com>
2023-07-04 16:08:39 +0800

    ref(domain-crud): add env.callDomainModel check

--- src/domain/domain-crud/api/create.ts
+++ src/domain/domain-crud/api/create.ts
@@ -2,19 +2,23 @@
 
 export default function createData(callDomainModel, domainModel, params) {
   return new Promise((resolve, reject) => {
-
-    callDomainModel(domainModel, 'INSERT', {
-      query: params
-    }).then(r => {
-      if (r.code === 1) {
-        resolve(r.data)
-      } else {
-        reject(r.msg || '请求错误，请稍候再试~')
-      }
-    }).catch(e => {
-      console.error(e)
-      reject('请求错误，请稍候再试~')
-    })
+    if (callDomainModel) {
+      callDomainModel(domainModel, 'INSERT', {
+        query: params
+      }).then(r => {
+        if (r.code === 1) {
+          resolve(r.data)
+        } else {
+          reject(r.msg || '请求错误，请稍候再试~')
+        }
+      }).catch(e => {
+        console.error(e)
+        reject('请求错误，请稍候再试~')
+      })
+    } else {
+      reject('env.callDomainModel 未实现')
+    }
+    
     // ajax({
     //   params: {
     //     query: { ...params },
--- src/domain/domain-crud/api/update.ts
+++ src/domain/domain-crud/api/update.ts
@@ -2,20 +2,25 @@
 
 export default function updateData(callDomainModel, domainModel, params) {
   return new Promise((resolve, reject) => {
-    callDomainModel(domainModel, 'UPDATE', {
-      query: {
-        ...params
-      }
-    }).then(r => {
-      if (r.code === 1) {
-        resolve(r.data)
-      } else {
-        reject(r.msg || '请求错误，请稍候再试~')
-      }
-    }).catch(e => {
-      console.error(e)
-      reject('请求错误，请稍候再试~')
-    })
+    if (callDomainModel) {
+      callDomainModel(domainModel, 'UPDATE', {
+        query: {
+          ...params
+        }
+      }).then(r => {
+        if (r.code === 1) {
+          resolve(r.data)
+        } else {
+          reject(r.msg || '请求错误，请稍候再试~')
+        }
+      }).catch(e => {
+        console.error(e)
+        reject('请求错误，请稍候再试~')
+      })
+    } else {
+      reject('env.callDomainModel 未实现')
+    }
+    
     // ajax({
     //   params: {
     //     query: { ...params },
