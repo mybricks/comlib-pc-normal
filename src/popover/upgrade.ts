f6d7c3f06974f13a4db03bf5432d359be33317a5
tangxiaoxin <1302947749@qq.com>
2023-07-05 11:38:54 +0800

    fix: upgrade style

--- src/popover/upgrade.ts
+++ src/popover/upgrade.ts
@@ -1,6 +1,10 @@
 import { Data } from './constants';
+import { isEmptyObject } from '../utils'
 
 export default function ({ input, output, data, setDeclaredStyle }: UpgradeParams<Data>): boolean {
-  setDeclaredStyle('.ant-popover-arrow-content, .ant-popover-inner .ant-popover-inner-content', { ...data.style });
+  if(!isEmptyObject(data.style)){
+    setDeclaredStyle('.ant-popover-arrow-content, .ant-popover-inner .ant-popover-inner-content', { ...data.style });
+    data.style = {};
+  }
   return true;
 }
--- src/timeline/upgrade.ts
+++ src/timeline/upgrade.ts
@@ -1,9 +1,13 @@
 import { Data } from './constants';
 
 export default function ({ input, output, data, setDeclaredStyle }: UpgradeParams<Data>): boolean {
-  (data.timelines || []).forEach(({ color, id }) => {
+  (data.timelines || []).forEach((timeline) => {
+    const { color, id } = timeline;
     const selector = `ul.ant-timeline > li[data-timeline-id="${id}"] > div.ant-timeline-item-head`;
-    setDeclaredStyle(selector, { borderColor: color });
+    if(!!color){
+      setDeclaredStyle(selector, { borderColor: color });
+      timeline.color = ''
+    }
   });
   return true;
 }
