2d134188b6b294b7211a95b36e269bac4aaec8d6
tangxiaoxin <1302947749@qq.com>
2023-07-10 16:53:54 +0800

    fix: pick upgrade

--- src/_pick/upgrade.ts
+++ src/_pick/upgrade.ts
@@ -1,6 +1,12 @@
 import { Data } from './types';
 
 export default function ({ data }: UpgradeParams<Data>): boolean {
-  data.picks = data.picks.map((pick) => ({ ...pick, expression: `{${pick.expression}}` }));
+  data.picks = data.picks.map((pick) => {
+    const expression =
+      pick.expression.startsWith('{') && pick.expression.endsWith('}')
+        ? pick.expression
+        : `{${pick.expression}}`;
+    return { ...pick, expression };
+  });
   return true;
 }
