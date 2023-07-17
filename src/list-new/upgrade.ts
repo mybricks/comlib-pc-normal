bfa83035ef14d2fe7047951e58092350621a3d04
jiakeshen <416883813@qq.com>
2023-07-17 15:16:04 +0800

    feat(list): add mobile style

--- src/list-new/upgrade.ts
+++ src/list-new/upgrade.ts
@@ -8,6 +8,16 @@ export default function ({ data, input, output }: UpgradeParams<Data>): boolean 
   if (typeof data.rowKey === "undefined") {
     data.rowKey = "";
   };
+  /**
+    * @description v1.0.6 增加移动端列数控制
+  */
+
+  if (typeof data?.grid?.mobileColumn === "undefined") {
+    if (typeof data?.grid === 'undefined') {
+      data.grid = {} as any
+    }
+    data.grid.mobileColumn = 1;
+  };
 
   /**
     * @description v1.0.7->1.0.8 增加换行情况下，排列方式，默认纵向
