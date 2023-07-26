22d427733cd1ae55dd9cf70e7ab191d6245a14a9
tangxiaoxin <1302947749@qq.com>
2023-07-26 16:00:28 +0800

    fix: taglist upgrade

--- src/tagList/upgrade.ts
+++ src/tagList/upgrade.ts
@@ -1,4 +1,5 @@
 import { Data } from './types';
+import { isEmptyObject } from '../utils'
 
 export default function ({ input, output, data, setDeclaredStyle }: UpgradeParams<Data>): boolean {
   const { tagStyle } = data;
@@ -8,14 +9,17 @@ export default function ({ input, output, data, setDeclaredStyle }: UpgradeParam
       icon: 'PlusOutlined'
     };
   }
-  data.tags.forEach(({ borderColor, color, textColor }, index) => {
-    const selector = `div[data-root] span[data-index="${index}"]`;
-    const style = {
-      background: color || tagStyle?.color,
-      color: textColor || tagStyle?.textColor,
-      borderColor: borderColor || tagStyle?.borderColor
-    };
-    setDeclaredStyle(selector, style);
-  });
+  if(tagStyle && !isEmptyObject(tagStyle)){
+    data.tags.forEach(({ borderColor, color, textColor }, index) => {
+      const selector = `div[data-root] span[data-index="${index}"]`;
+      const style = {
+        background: color || tagStyle?.color,
+        color: textColor || tagStyle?.textColor,
+        borderColor: borderColor || tagStyle?.borderColor
+      };
+      setDeclaredStyle(selector, style);
+    });
+    data.tagStyle = {};
+  }
   return true;
 }
