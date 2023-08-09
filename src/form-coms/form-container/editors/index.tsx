2aa8185071859d003c6c90f5cf7650e0e79c058e
Bigbig-Huang <hqy09092020@163.com>
2023-08-09 17:45:00 +0800

    feat(form-container):add action buttons style

--- src/form-coms/form-container/editors/index.tsx
+++ src/form-coms/form-container/editors/index.tsx
@@ -1154,53 +1154,6 @@ export default {
         }
       },
       {
-        title: '风格',
-        type: 'Select',
-        options() {
-          return [
-            { value: 'primary', label: '主按钮' },
-            { value: 'default', label: '次按钮' },
-            { value: 'dashed', label: '虚线按钮' },
-            { value: 'link', label: '链接按钮' },
-            { value: 'text', label: '文字按钮' }
-          ];
-        },
-        value: {
-          get({ data, focusArea }: EditorResult<Data>) {
-            const comId = focusArea.dataset.formActionsItem as string;
-
-            return data.actions.items.find((item) => item.key === comId)?.type || 'default';
-          },
-          set({ data, focusArea }: EditorResult<Data>, value: ButtonType) {
-            const comId = focusArea.dataset['formActionsItem'];
-            const item = data.actions.items.find((item) => item.key === comId);
-
-            if (item) {
-              item.type = value;
-            }
-          }
-        }
-      },
-      {
-        title: '危险按钮',
-        type: 'Switch',
-        value: {
-          get({ data, focusArea }: EditorResult<Data>) {
-            const comId = focusArea.dataset.formActionsItem as string;
-
-            return data.actions.items.find((item) => item.key === comId)?.danger;
-          },
-          set({ data, focusArea }: EditorResult<Data>, value: boolean) {
-            const comId = focusArea.dataset['formActionsItem'];
-            const item = data.actions.items.find((item) => item.key === comId);
-
-            if (item) {
-              item.danger = value;
-            }
-          }
-        }
-      },
-      {
         title: '事件',
         items: [
           {
