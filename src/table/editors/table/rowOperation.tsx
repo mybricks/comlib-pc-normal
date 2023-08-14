f6fdbad2e57b3faf10269ab5afbddfc71de06eea
z35635 <linzhujlu@foxmail.com>
2023-08-14 15:42:30 +0800

    feat(table): support rowDoubleClick event

--- src/table/editors/table/rowOperation.tsx
+++ src/table/editors/table/rowOperation.tsx
@@ -1,6 +1,6 @@
 import { Data } from '../../types';
 import { OutputIds } from '../../constants';
-import { Schemas } from '../../schema';
+import { Schemas, setDataSchema } from '../../schema';
 export default {
   title: '行操作',
   items: [
@@ -11,10 +11,11 @@ export default {
         get({ data }: EditorResult<Data>) {
           return data.enableRowClick;
         },
-        set({ data, output }: EditorResult<Data>, value: boolean) {
+        set({ data, output, ...res }: EditorResult<Data>, value: boolean) {
           data.enableRowClick = value;
           if (value) {
             output.add(OutputIds.ROW_CLICK, '行点击事件', Schemas.ROW_CLICK);
+            setDataSchema({ data, output, ...res });
           } else {
             output.remove(OutputIds.ROW_CLICK);
           }
@@ -30,6 +31,36 @@ export default {
       options: () => {
         return {
           outputId: OutputIds.ROW_CLICK
+        };
+      }
+    },
+    {
+      title: '行双击',
+      type: 'switch',
+      value: {
+        get({ data }: EditorResult<Data>) {
+          return data.enableRowDoubleClick;
+        },
+        set({ data, output, ...res }: EditorResult<Data>, value: boolean) {
+          data.enableRowDoubleClick = value;
+          if (value) {
+            output.add(OutputIds.ROW_DOUBLE_CLICK, '行双击事件', Schemas.ROW_CLICK);
+            setDataSchema({ data, output, ...res });
+          } else {
+            output.remove(OutputIds.ROW_DOUBLE_CLICK);
+          }
+        }
+      }
+    },
+    {
+      title: '行双击事件',
+      type: '_Event',
+      ifVisible({ data }: EditorResult<Data>) {
+        return data.enableRowDoubleClick;
+      },
+      options: () => {
+        return {
+          outputId: OutputIds.ROW_DOUBLE_CLICK
         };
       }
     },
