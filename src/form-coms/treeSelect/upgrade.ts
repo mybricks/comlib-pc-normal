f8f10c1599cda84233bb6c925bde5466a3e67499
z35635 <linzhujlu@foxmail.com>
2023-08-17 16:56:43 +0800

    feat(treeSelect): upgrade to 1.1.2

--- src/form-coms/treeSelect/upgrade.ts
+++ src/form-coms/treeSelect/upgrade.ts
@@ -11,11 +11,11 @@ export default function ({ data, input, output }: UpgradeParams<Data>): boolean 
     items: {
       type: 'object',
       properties: {
-        label: {
+        [data.labelFieldName || 'label']: {
           title: '标签',
           type: 'string'
         },
-        value: {
+        [data.valueFieldName || 'value']: {
           title: '值',
           type: 'string'
         },
@@ -23,7 +23,7 @@ export default function ({ data, input, output }: UpgradeParams<Data>): boolean 
           title: '是否叶子节点',
           type: 'boolean'
         },
-        children: {
+        [data.childrenFieldName || 'children']: {
           title: '子项',
           type: 'array',
           items: {
@@ -60,6 +60,20 @@ export default function ({ data, input, output }: UpgradeParams<Data>): boolean 
   }
 
   //=========== v1.1.0 end ===============
+
+  /**
+   * @description v1.1.2 , 新增 更新节点数据输入项、节点展开输出项
+  */
+
+  if (!input.get('setNodeData')) {
+    input.add('setNodeData', '更新节点数据', treeDataSchema.items);
+  }
+
+  if (!output.get('onExpand')) {
+    output.add('onExpand', '节点展开', treeDataSchema.items);
+  }
+
+  //=========== v1.1.2 end ===============
 
   return true;
 }
