50b3a9168c697ef29d34fa50ba440003c88849b7
zhuba-Ahhh <3477826311@qq.com>
2023-09-06 11:48:59 +0800

    feat(auto-complete): set input font color

--- src/form-coms/auto-complete/upgrade.ts
+++ src/form-coms/auto-complete/upgrade.ts
@@ -1,6 +1,10 @@
 import { InputIds } from '../types';
 import { Data } from './runtime';
 
+const valueSchema = {
+  type: 'string'
+};
+
 export default function ({ data, input, output }: UpgradeParams<Data>): boolean {
   /**
     * @description v1.0.3 统一“设置数据源”、“设置值”、“设置初始值”、“值初始化”的schema
@@ -35,5 +39,13 @@ export default function ({ data, input, output }: UpgradeParams<Data>): boolean 
   if (!output.get('onSelect')) {
     output.add('onSelect', '值选择', { type: "any"});
   }
+
+  /**
+   * v1.0.8 -> v1.0.9 增加「设置字体颜色」能力
+   */
+  if (!input.get('setColor')) {
+    input.add('setColor', '设置字体颜色', valueSchema);
+  }
+
   return true;
 }
