9ef461fde5a29c4762589ae47c5dab3fa20d877c
z35635 <linzhujlu@foxmail.com>
2023-09-12 20:37:52 +0800

    feat(form-item-container): setDisabled setEnabled

--- src/form-coms/form-item-container/runtime.tsx
+++ src/form-coms/form-item-container/runtime.tsx
@@ -1,6 +1,6 @@
-import React, { useCallback, useLayoutEffect } from 'react';
+import React, { useCallback, useEffect, useLayoutEffect } from 'react';
 import { validateTrigger } from '../form-container/models/validate';
-import { OutputIds } from '../types';
+import { InputIds, OutputIds } from '../types';
 import { validateFormItem } from '../utils/validator';
 import { SlotIds } from './constants';
 import { onChange as onChangeForFc } from '../form-container/models/onChange';
@@ -8,15 +8,20 @@ import { onChange as onChangeForFc } from '../form-container/models/onChange';
 interface Data {
   value: string | undefined;
   rules: any[];
+  disabled: boolean;
+  childrenInputs: Record<string, any>;
   config: {};
 }
 
 export default function (props: RuntimeParams<Data>) {
   const { env, data, _inputs, inputs, _outputs, outputs, slots, parentSlot, id, name, style } =
     props;
-  const { edit } = env;
 
   useLayoutEffect(() => {
+    if (!data.childrenInputs) {
+      data.childrenInputs = {};
+    }
+
     inputs['setValue']((val) => {
       data.value = val;
       slots[SlotIds.FormItem].inputs['curValue'](data.value);
@@ -52,11 +57,55 @@ export default function (props: RuntimeParams<Data>) {
       data.value = void 0;
       slots[SlotIds.FormItem].inputs['curValue'](data.value);
     });
+
+    //设置禁用
+    inputs['setDisabled']((val) => {
+      data.disabled = true;
+      slots[SlotIds.FormItem].inputs['onDisabled'](val);
+      setValuesForInput({ data, actionId: InputIds.SetDisabled, val });
+    });
+
+    //设置启用
+    inputs['setEnabled']((val) => {
+      data.disabled = false;
+      slots[SlotIds.FormItem].inputs['onEnabled'](val);
+      setValuesForInput({ data, actionId: InputIds.SetEnabled, val });
+    }, []);
   }, []);
 
   const onValidateTrigger = () => {
     validateTrigger(parentSlot, { id, name });
   };
 
-  return <div>{slots[SlotIds.FormItem] && slots[SlotIds.FormItem].render()}</div>;
+  return (
+    <div>
+      {slots[SlotIds.FormItem] &&
+        slots[SlotIds.FormItem].render({
+          wrap(comAray: { id; jsx; name; def; inputs; outputs; style }[]) {
+            comAray.forEach((com) => {
+              // 编辑态：收集表单项组件
+              if (env.edit && com.outputs?.[OutputIds.ReturnValue]) {
+                data.childrenInputs[com.name] = {};
+              }
+              // 运行时：收集表单项组件的输入项
+              if (env.runtime && data.childrenInputs[com.name]) {
+                data.childrenInputs[com.name] = com.inputs;
+              }
+            });
+
+            return <>{comAray.map((com) => com.jsx)}</>;
+          }
+        })}
+    </div>
+  );
 }
+
+/**
+ *
+ * @param data 组件数据
+ * @param actionId 触发操作
+ * @param val 参数
+ */
+const setValuesForInput = ({ data, actionId, val }) => {
+  Object.values(data.childrenInputs).forEach((inputs) => inputs?.[actionId]?.(val));
+};
