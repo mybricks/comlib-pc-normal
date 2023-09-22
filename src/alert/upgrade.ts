73d60d97c7746270f1c65ddcba333765220b072e
Bigbig-Huang <hqy09092020@163.com>
2023-09-22 10:52:08 +0800

    fix(alert):fix style

--- src/alert/upgrade.ts
+++ src/alert/upgrade.ts
@@ -1,12 +1,12 @@
 import { Data } from './constants';
 
 export default function ({ data, setDeclaredStyle }: UpgradeParams<Data>): boolean {
-  /**
-   * @description v1.0.0 -> v1.0.1, 新增标题和介绍文案字体样式大小
-  */
-  if (typeof data.size === "undefined") {
-    data.size = "16px"
-  };
+  // /**
+  //  * @description v1.0.0 -> v1.0.1, 新增标题和介绍文案字体样式大小
+  // */
+  // if (typeof data.size === "undefined") {
+  //   data.size = "16px"
+  // };
 
   //兼容之前颜色自定义情况
   if(data.isColor && data.textColor !== ''){
@@ -16,11 +16,11 @@ export default function ({ data, setDeclaredStyle }: UpgradeParams<Data>): boole
     data.textColor = '';
   }
 
-  /**
-   * @description v1.0.3 -> v1.0.4, 兼容图标大小
-  */
+  // /**
+  //  * @description v1.0.3 -> v1.0.4, 兼容图标大小
+  // */
   if(data.size && data.size !== ''){
-    setDeclaredStyle(`.ant-alert-action`, { fontSize: data.size});
+    setDeclaredStyle(`.ant-alert-icon`, { fontSize: data.size});
     data.size = '';
   }
   
