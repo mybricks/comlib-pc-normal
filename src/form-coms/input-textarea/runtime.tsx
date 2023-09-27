a55be635fc1851b5b778d77c416ef8dea4223e11
z35635 <linzhujlu@foxmail.com>
2023-09-27 14:19:10 +0800

    ref(input-textarea): input.validate

--- src/form-coms/input-textarea/runtime.tsx
+++ src/form-coms/input-textarea/runtime.tsx
@@ -59,17 +59,18 @@ export default function ({
       setEnabled() {
         data.config.disabled = false;
       },
-      validate(output) {
+      validate(model, outputRels) {
         validateFormItem({
           value: data.value,
           env,
+          model,
           rules: data.rules
         })
           .then((r) => {
-            output(r);
+            outputRels(r);
           })
           .catch((e) => {
-            output(e);
+            outputRels(e);
           });
       }
     }
