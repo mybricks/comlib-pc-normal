da8d86a79c3c1e183a47127e8b305f774375da74
zhuba-Ahhh <3477826311@qq.com>
2023-09-28 10:57:57 +0800

    feat(table): rowSelect checkbox hover

--- src/table/editors/rowSelect/index.ts
+++ src/table/editors/rowSelect/index.ts
@@ -42,6 +42,55 @@ const rowSelectEditor = {
         domTarget: `.ant-checkbox .ant-checkbox-inner`
       },
       {
+        title: '勾选框选中',
+        catelog: 'Hover',
+        ifVisible({ data }: EditorResult<Data>) {
+          return data.useRowSelection;
+        },
+        options: ['border', { type: 'background', config: { disableBackgroundImage: true } }],
+        target: `.ant-checkbox-checked:hover .ant-checkbox-inner`,
+        domTarget: `.ant-checkbox-checked .ant-checkbox-inner`
+      },
+      {
+        title: '全选框选中',
+        catelog: 'Hover',
+        ifVisible({ data }: EditorResult<Data>) {
+          return data.useRowSelection;
+        },
+        options: ['border', { type: 'background', config: { disableBackgroundImage: true } }],
+        target: `.ant-checkbox-indeterminate:hover .ant-checkbox-inner`,
+        domTarget: `.ant-checkbox-indeterminate .ant-checkbox-inner`
+      },
+      {
+        title: '普通勾选符号',
+        catelog: 'Hover',
+        ifVisible({ data }: EditorResult<Data>) {
+          return data.useRowSelection;
+        },
+        options: [
+          {
+            type: 'border',
+            config: {
+              disableBorderWidth: true,
+              disableBorderStyle: true,
+              disableBorderRadius: true
+            }
+          }
+        ],
+        target: `.ant-checkbox-checked:hover .ant-checkbox-inner:after`,
+        domTarget: `.ant-checkbox-indeterminate .ant-checkbox-inner`
+      },
+      {
+        title: '全选勾选符号',
+        catelog: 'Hover',
+        ifVisible({ data }: EditorResult<Data>) {
+          return data.useRowSelection;
+        },
+        options: [{ type: 'background', config: { disableBackgroundImage: true } }],
+        target: `.ant-checkbox-indeterminate:hover .ant-checkbox-inner:after`,
+        domTarget: `.ant-checkbox-indeterminate .ant-checkbox-inner`
+      },
+      {
         title: '勾选框',
         catelog: '选中',
         ifVisible({ data }: EditorResult<Data>) {
