93af89b85d87cc09053045a42ecd1c3bac8f8633
jiakeshen <416883813@qq.com>
2023-10-07 17:04:10 +0800

    fix(date): use canvaselement as popcontainer

--- src/form-coms/date-picker/runtime.tsx
+++ src/form-coms/date-picker/runtime.tsx
@@ -263,8 +263,8 @@ export default function Runtime(props: RuntimeParams<Data>) {
           onChange={onChange}
           disabledDate={data.disabledDate || disabledDateConfig}
           getPopupContainer={(triggerNode: HTMLElement) => {
-            return ref.current || document.body;
-            // return edit || debug ? env?.canvasElement : document.body;
+            // return ref.current || document.body;
+            return edit || debug ? env?.canvasElement : document.body;
           }}
           dropdownClassName={`${id} ${css.datePicker}`}
           open={(edit && data.useCustomDateCell) || env.design}
