9b7496e01d4e55115c55f7965b90b93b0c1da0ac
zhuba-Ahhh <3477826311@qq.com>
2023-10-09 15:11:02 +0800

    fix(globalLoading): debug close

--- src/_global-loading/runtime.ts
+++ src/_global-loading/runtime.ts
@@ -3,22 +3,30 @@ import { InputIds, Data } from './constants';
 
 export default function ({ env, data, inputs }: RuntimeParams<Data>) {
   const debugRuntime = !!(env?.runtime && env?.runtime.debug);
+  const getGlobalLoading = () =>
+    debugRuntime
+      ? (document
+          .getElementById('_mybricks-geo-webview_')
+          ?.shadowRoot?.getElementById('global-loading') as HTMLElement)
+      : document.getElementById('global-loading');
+  let globalLoading = getGlobalLoading();
+
   if (env?.runtime && inputs) {
     inputs[InputIds.Trigger]((val) => {
       const { closeLoading, loadingText: defaultLoadingText, maskStyle, ...spinProps } = data;
       if (closeLoading) {
-        GlobalLoading.close(debugRuntime ? env?.canvasElement : document.body, debugRuntime);
+        GlobalLoading.close(debugRuntime ? env?.canvasElement : document.body, globalLoading);
         return;
       }
       const loadingText = val && typeof val === 'string' ? val : data.loadingText;
-
-      GlobalLoading.open(loadingText, spinProps, debugRuntime ? env?.canvasElement : document.body);
-      const globalLoading = debugRuntime
-        ? (document
-            .getElementById('_mybricks-geo-webview_')
-            ?.shadowRoot?.querySelector('#global-loading') as HTMLElement)
-        : document.getElementById('global-loading');
-      if (globalLoading) {
+      GlobalLoading.open(
+        loadingText,
+        spinProps,
+        debugRuntime ? env?.canvasElement : document.body,
+        globalLoading
+      );
+      globalLoading = getGlobalLoading();
+      if (!!globalLoading) {
         Object.assign(globalLoading.style, maskStyle);
         const dotItems = globalLoading.querySelectorAll('.ant-spin-dot-item');
         Array.from(dotItems).forEach((item: any) => {
@@ -28,7 +36,8 @@ export default function ({ env, data, inputs }: RuntimeParams<Data>) {
     });
     if (inputs[InputIds.Close]) {
       inputs[InputIds.Close](() => {
-        GlobalLoading.close(debugRuntime ? env?.canvasElement : document.body, debugRuntime);
+        globalLoading = getGlobalLoading();
+        GlobalLoading.close(debugRuntime ? env?.canvasElement : document.body, globalLoading);
       });
     }
   }
