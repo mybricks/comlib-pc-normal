f68c0a8203d5b1dc9b2399617b739c6a8feed346
z35635 <linzhujlu@foxmail.com>
2023-10-10 14:56:42 +0800

    feat(input-textarea): support customRule

--- src/form-coms/input-textarea/editors.tsx
+++ src/form-coms/input-textarea/editors.tsx
@@ -1,6 +1,7 @@
 import { RuleKeys, defaultValidatorExample, defaultRules } from '../utils/validator';
 import { Data } from './runtime';
 import { createrCatelogEditor } from '../utils';
+import { outputIds } from '../form-container/constants';
 
 export default {
   '@resize': {
@@ -230,6 +231,19 @@ export default {
             set({ data }, value: any) {
               data.rules = value;
             }
+          }
+        },
+        {
+          title: '校验触发事件',
+          type: '_event',
+          ifVisible({ data }: EditorResult<Data>) {
+            const cutomRule = (data.rules || defaultRules).find(
+              (i) => i.key === RuleKeys.CUSTOM_EVENT
+            );
+            return !!cutomRule?.status;
+          },
+          options: {
+            outputId: outputIds.ON_VALIDATE
           }
         },
         {
