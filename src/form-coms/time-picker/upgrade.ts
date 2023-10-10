6bef70273c67e2196d0f941ebe165d7e778721d5
z35635 <linzhujlu@foxmail.com>
2023-10-10 17:33:07 +0800

    feat(time-picker): support customRule

--- src/form-coms/time-picker/upgrade.ts
+++ src/form-coms/time-picker/upgrade.ts
@@ -1,9 +1,15 @@
+import { InputIds, OutputIds } from '../types';
 import { Data } from './types';
 
 export default function ({ data, input, output }: UpgradeParams<Data>): boolean {
-  const valueSchema = {
-    type: 'number'
+  let valueSchema = {
+    type: 'string'
   };
+  if (data.format === 'timeStamp') {
+    valueSchema = {
+      type: 'number'
+    }
+  }
 
   if (!input.get('setInitialValue')) {
     input.add('setInitialValue', '设置初始值', valueSchema);
@@ -13,6 +19,40 @@ export default function ({ data, input, output }: UpgradeParams<Data>): boolean 
     output.add('onInitial', '值初始化', valueSchema);
   }
   output.get('onInitial').setTitle('值初始化');
+
+  /**
+   * @description v1.1.0 新增自定义校验事件
+   */
+
+  if (!input.get(InputIds.SetValidateInfo)) {
+    input.add(InputIds.SetValidateInfo, '设置校验状态', {
+      type: 'object',
+      properties: {
+        validateStatus: {
+          type: 'enum',
+          items: [
+            {
+              type: 'string',
+              value: 'success',
+            },
+            {
+              type: 'string',
+              value: 'error',
+            },
+          ],
+        },
+        help: {
+          type: 'string',
+        },
+      },
+    });
+  }
+
+  if (!output.get(OutputIds.OnValidate)) {
+    output.add(OutputIds.OnValidate, '校验触发', valueSchema);
+  }
+
+  //=========== v1.1.0 end ===============
 
   return true;
 }
