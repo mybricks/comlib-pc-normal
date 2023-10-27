3b878e7caa898e0322cc0a2dfe3e72aea39195bd
Bigbig-Huang <hqy09092020@163.com>
2023-10-27 11:33:37 +0800

    feat(table):style up

--- src/table/runtime.tsx
+++ src/table/runtime.tsx
@@ -56,6 +56,8 @@ export default function (props: RuntimeParams<Data>) {
 
   const rowKey = data.rowKey || DefaultRowKey;
 
+  const ref = useRef<HTMLDivElement>(null);
+
   const initFilterMap = () => {
     let res = {};
     data.columns.forEach((cItem) => {
@@ -185,6 +187,13 @@ export default function (props: RuntimeParams<Data>) {
       }
     }
   }, []);
+
+  useEffect(() => {
+    const target = ref.current?.querySelector?.('.ant-table-placeholder') as HTMLSpanElement;
+    if (target && data.fixedHeader) {
+      target.style.height = typeof data.scroll.y === 'string' ? data.scroll.y : '';
+    }
+  }, [data.scroll.y, data.fixedHeader]);
 
   const getSelectedRows = useCallback((dataSource, selectedRowKeys) => {
     let rowObj = {};
@@ -788,113 +797,120 @@ export default function (props: RuntimeParams<Data>) {
 
   const customizeRenderEmpty = () => (
     <div className={`emptyNormal ${css.emptyNormal}`}>
-      <Image src={data.image} className={`emptyImage ${css.emptyImage}`} />
+      <Image src={data.image} className={`emptyImage ${css.emptyImage}`} preview={false} />
       <p className={`emptyDescription ${css.emptyDescription}`}>{data.description}</p>
     </div>
   );
 
   return (
-    <ConfigProvider locale={zhCN} renderEmpty={data.isEmpty ? customizeRenderEmpty : void 0}>
-      <TableContext.Provider value={contextValue}>
-        <div className={css.table}>
-          <TableHeader
-            env={env}
-            data={data}
-            dataSource={dataSource}
-            slots={slots}
-            outputs={outputs}
-            selectedRows={selectedRows}
-            selectedRowKeys={selectedRowKeys}
-          />
-          {data.columns.length ? (
-            <Table
-              style={{
-                width: data.tableLayout === TableLayoutEnum.FixedWidth ? getUseWidth() : '100%',
-                height: data.fixedHeight
-              }}
-              dataSource={edit ? defaultDataSource : realShowDataSource}
-              loading={{
-                tip: data.loadingTip,
-                spinning: loading
-              }}
-              onRow={onRow}
-              rowKey={rowKey}
-              size={data.size as any}
-              bordered={data.bordered}
-              pagination={false}
-              rowSelection={data.useRowSelection ? rowSelection : undefined}
-              showHeader={data.showHeader === false && env.runtime ? false : true}
-              rowClassName={(_, index) => {
-                if (data.enableStripe) {
-                  return (index + 1) % 2 === 0
-                    ? 'mybricks-table-row-double'
-                    : 'mybricks-table-row-single';
+    <div ref={ref}>
+      <ConfigProvider locale={zhCN} renderEmpty={data.isEmpty ? customizeRenderEmpty : void 0}>
+        <TableContext.Provider value={contextValue}>
+          <div className={css.table}>
+            <TableHeader
+              env={env}
+              data={data}
+              dataSource={dataSource}
+              slots={slots}
+              outputs={outputs}
+              selectedRows={selectedRows}
+              selectedRowKeys={selectedRowKeys}
+            />
+            {data.columns.length ? (
+              <Table
+                style={{
+                  width: data.tableLayout === TableLayoutEnum.FixedWidth ? getUseWidth() : '100%',
+                  height: data.fixedHeader ? data.fixedHeight : ''
+                }}
+                dataSource={edit ? defaultDataSource : realShowDataSource}
+                loading={{
+                  tip: data.loadingTip,
+                  spinning: loading
+                }}
+                onRow={onRow}
+                rowKey={rowKey}
+                size={data.size as any}
+                bordered={data.bordered}
+                pagination={false}
+                rowSelection={data.useRowSelection ? rowSelection : undefined}
+                showHeader={data.showHeader === false && env.runtime ? false : true}
+                rowClassName={(_, index) => {
+                  if (data.enableStripe) {
+                    return (index + 1) % 2 === 0
+                      ? 'mybricks-table-row-double'
+                      : 'mybricks-table-row-single';
+                  }
+                  return '';
+                }}
+                scroll={{
+                  x: '100%',
+                  y: data.scroll.y ? data.scroll.y : void 0
+                }}
+                summary={
+                  data.useSummaryColumn
+                    ? () => SummaryColumn(slots, data, summaryColumnData)
+                    : void 0
                 }
-                return '';
-              }}
-              scroll={{
-                x: '100%',
-                y: data.scroll.y ? data.scroll.y : void 0
-              }}
-              summary={
-                data.useSummaryColumn ? () => SummaryColumn(slots, data, summaryColumnData) : void 0
-              }
-              expandable={
-                data.useExpand && slots[SlotIds.EXPAND_CONTENT]
-                  ? {
-                      expandedRowRender: (record, index) => {
-                        const inputValues = {
-                          [InputIds.EXP_COL_VALUES]: {
-                            ...record
-                          },
-                          [InputIds.INDEX]: index
-                        };
-                        if (data.useExpand && data.expandDataIndex) {
-                          inputValues[InputIds.EXP_ROW_VALUES] = get(record, data.expandDataIndex);
-                        }
-                        return slots[SlotIds.EXPAND_CONTENT].render({
-                          inputValues,
-                          key: `${InputIds.EXP_COL_VALUES}-${record[rowKey]}`
-                        });
-                      },
-                      expandedRowKeys: edit ? [defaultDataSource[0][rowKey]] : expandedRowKeys, //增加动态设置
-                      onExpand: (expanded, record) => {
-                        if (!env.runtime) return;
-                        const key = record[rowKey];
-                        if (expanded && !expandedRowKeys.includes(key)) {
-                          setExpandedRowKeys([...expandedRowKeys, key]);
-                        } else if (!expanded && expandedRowKeys.includes(key)) {
-                          expandedRowKeys.splice(expandedRowKeys.indexOf(key), 1);
-                          setExpandedRowKeys([...expandedRowKeys]);
+                expandable={
+                  data.useExpand && slots[SlotIds.EXPAND_CONTENT]
+                    ? {
+                        expandedRowRender: (record, index) => {
+                          const inputValues = {
+                            [InputIds.EXP_COL_VALUES]: {
+                              ...record
+                            },
+                            [InputIds.INDEX]: index
+                          };
+                          if (data.useExpand && data.expandDataIndex) {
+                            inputValues[InputIds.EXP_ROW_VALUES] = get(
+                              record,
+                              data.expandDataIndex
+                            );
+                          }
+                          return slots[SlotIds.EXPAND_CONTENT].render({
+                            inputValues,
+                            key: `${InputIds.EXP_COL_VALUES}-${record[rowKey]}`
+                          });
+                        },
+                        expandedRowKeys: edit ? [defaultDataSource[0][rowKey]] : expandedRowKeys, //增加动态设置
+                        onExpand: (expanded, record) => {
+                          if (!env.runtime) return;
+                          const key = record[rowKey];
+                          if (expanded && !expandedRowKeys.includes(key)) {
+                            setExpandedRowKeys([...expandedRowKeys, key]);
+                          } else if (!expanded && expandedRowKeys.includes(key)) {
+                            expandedRowKeys.splice(expandedRowKeys.indexOf(key), 1);
+                            setExpandedRowKeys([...expandedRowKeys]);
+                          }
                         }
                       }
-                    }
-                  : undefined
-              }
-              onChange={onChange}
-              tableLayout={
-                (data.tableLayout === TableLayoutEnum.FixedWidth
-                  ? TableLayoutEnum.Fixed
-                  : data.tableLayout) || TableLayoutEnum.Fixed
-              }
-            >
-              {env.runtime ? renderColumns() : renderColumnsWhenEdit()}
-            </Table>
-          ) : (
-            <Empty description="请添加列或连接数据源" className={css.emptyWrap} />
-          )}
-          <TableFooter
-            env={env}
-            parentSlot={props.parentSlot}
-            data={data}
-            slots={slots}
-            inputs={inputs}
-            outputs={outputs}
-            selectedRows={selectedRows}
-            selectedRowKeys={selectedRowKeys}
-          />
-        </div>
-      </TableContext.Provider>
-    </ConfigProvider>
+                    : undefined
+                }
+                onChange={onChange}
+                tableLayout={
+                  (data.tableLayout === TableLayoutEnum.FixedWidth
+                    ? TableLayoutEnum.Fixed
+                    : data.tableLayout) || TableLayoutEnum.Fixed
+                }
+              >
+                {env.runtime ? renderColumns() : renderColumnsWhenEdit()}
+              </Table>
+            ) : (
+              <Empty description="请添加列或连接数据源" className={css.emptyWrap} />
+            )}
+            <TableFooter
+              env={env}
+              parentSlot={props.parentSlot}
+              data={data}
+              slots={slots}
+              inputs={inputs}
+              outputs={outputs}
+              selectedRows={selectedRows}
+              selectedRowKeys={selectedRowKeys}
+            />
+          </div>
+        </TableContext.Provider>
+      </ConfigProvider>
+    </div>
   );
 }
