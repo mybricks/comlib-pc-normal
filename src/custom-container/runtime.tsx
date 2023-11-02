8f0394582bd47922c3eb1e96e5ce9f32043ba8a5
tangxiaoxin <1302947749@qq.com>
2023-11-02 14:48:56 +0800

    useResizeObserver

--- src/custom-container/runtime.tsx
+++ src/custom-container/runtime.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect, useRef, useMemo } from 'react';
 import { Data, SlotIds, InputIds, OutputIds, OverflowEnum } from './constants';
+import { useResizeObserver } from '../hooks/useResizeObserver';
 import css from './style.less';
 
 export default function (props: RuntimeParams<Data>) {
@@ -80,6 +81,14 @@ export default function (props: RuntimeParams<Data>) {
       maxWidth
     };
   }, [dynamicStyle, legacyStyle, data.slotStyle]);
+
+  useResizeObserver(ref, (entries) => {
+    if (!ref.current) return;
+    if (env.edit || env.runtime?.debug) {
+      const { contentRect } = entries[0];
+      ref.current.style.height = `${contentRect.height}px`;
+    }
+  });
 
   return (
     <div
