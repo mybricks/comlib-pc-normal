a55bf233503bd9ac14f362a8306cc214f1514e9e
jidan.wd <wudi27@kuaishou.com>
2023-11-08 19:30:41 +0800

    feat: 卡片新增双击事件

--- src/card/runtime.tsx
+++ src/card/runtime.tsx
@@ -38,6 +38,14 @@ export default (props: RuntimeParams<Data>) => {
     }
   }, []);
 
+  const onDoubleClick = useCallback(() => {
+    if (env.runtime && useClick && outputs[OutputIds.DoubleClick]) {
+      const outVal: string | number =
+        data.dataType === 'external' ? data.inVal : data.outputContent;
+      outputs[OutputIds.DoubleClick](outVal);
+    }
+  }, []);
+
   const RenderAction = () => {
     let itemsArr = data.items.map((item) => {
       return slots[item.key]?.render();
@@ -63,6 +71,7 @@ export default (props: RuntimeParams<Data>) => {
         extra={useExtra ? slots[SlotIds.Extra]?.render() : undefined}
         hoverable={hoverable}
         onClick={onClick}
+        onDoubleClick={onDoubleClick}
         actions={isAction ? RenderAction() : void 0}
       >
         <div
