09632d2d65740d9d3dd6adc6293a68b0da7520de
zhuba-Ahhh <3477826311@qq.com>
2023-12-04 13:31:33 +0800

    fix(_global-loading): Multiple runs

--- src/_global-loading/runtime.ts
+++ src/_global-loading/runtime.ts
@@ -1,31 +1,33 @@
 import { GlobalLoading } from './globalLoading';
 import { InputIds, Data } from './constants';
 
+/**
+ * @description 实时获取是否有global-loading
+ * @param debugRuntime 
+ * @returns 
+ */
+export const getGlobalLoading = (debugRuntime?: boolean) => {
+  return !!debugRuntime
+    ? (document
+        .getElementById('_mybricks-geo-webview_')
+        ?.shadowRoot?.getElementById('global-loading') as HTMLElement)
+    : document.getElementById('global-loading');
+};
+
 export default function ({ env, data, inputs }: RuntimeParams<Data>) {
   const debugRuntime = !!(env?.runtime && env?.runtime.debug);
-  const getGlobalLoading = () =>
-    debugRuntime
-      ? (document
-          .getElementById('_mybricks-geo-webview_')
-          ?.shadowRoot?.getElementById('global-loading') as HTMLElement)
-      : document.getElementById('global-loading');
-  let globalLoading = getGlobalLoading();
+  let globalLoading = getGlobalLoading(debugRuntime);
 
   if (env?.runtime && inputs) {
     inputs[InputIds.Trigger]((val) => {
       const { closeLoading, loadingText: defaultLoadingText, maskStyle, ...spinProps } = data;
       if (closeLoading) {
-        GlobalLoading.close(debugRuntime ? env?.canvasElement : document.body, globalLoading);
+        GlobalLoading.close(debugRuntime);
         return;
       }
       const loadingText = val && typeof val === 'string' ? val : env.i18n(data.loadingText);
-      GlobalLoading.open(
-        loadingText,
-        spinProps,
-        debugRuntime ? env?.canvasElement : document.body,
-        globalLoading
-      );
-      globalLoading = getGlobalLoading();
+      GlobalLoading.open(loadingText, spinProps, debugRuntime ? env?.canvasElement : document.body, debugRuntime);
+      globalLoading = getGlobalLoading(debugRuntime);
       if (!!globalLoading) {
         Object.assign(globalLoading.style, maskStyle);
         const dotItems = globalLoading.querySelectorAll('.ant-spin-dot-item');
@@ -36,8 +38,7 @@ export default function ({ env, data, inputs }: RuntimeParams<Data>) {
     });
     if (inputs[InputIds.Close]) {
       inputs[InputIds.Close](() => {
-        globalLoading = getGlobalLoading();
-        GlobalLoading.close(debugRuntime ? env?.canvasElement : document.body, globalLoading);
+        GlobalLoading.close(debugRuntime);
       });
     }
   }
