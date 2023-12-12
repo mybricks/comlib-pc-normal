9a649e39eadcd72bdfa61ff0f1227552791e9d79
jidan.wd <wudi27@kuaishou.com>
2023-12-12 17:43:03 +0800

    feat: 调整截图比对

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -18,7 +18,7 @@ declare global {
   }
 }
 compareSnapshotCommand({
-  failureThreshold: 0.03, // threshold for entire image
+  failureThreshold: 0.00, // threshold for entire image
   failureThresholdType: 'percent', // percent of image or number of pixels
   customDiffConfig: { threshold: 0.1 }, // threshold for each pixel
   capture: 'viewport' // capture viewport in screenshot
