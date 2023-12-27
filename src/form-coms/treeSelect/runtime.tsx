58764d69ce1613be0b429c67bcdf7fd8f8faf524
z35635 <linzhujlu@foxmail.com>
2023-12-27 11:01:34 +0800

    ref(treeSelect): onChange

--- src/form-coms/treeSelect/runtime.tsx
+++ src/form-coms/treeSelect/runtime.tsx
@@ -208,7 +208,8 @@ export default function Runtime({
     onChangeForFc(parentSlot, { id, value, name });
   }, []);
 
-  const onChange = useCallback((value) => {
+  const onChange: TreeSelectProps['onChange'] = useCallback((_, label, extra) => {
+    const value = extra.triggerValue;
     changeValue(value);
     outputs[OutputIds.OnChange](value);
     onValidateTrigger();
