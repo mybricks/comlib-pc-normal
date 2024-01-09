2d719c989a188f43817be28154dd44a194cf66a3
yingpengsha <yingpengsha@kuaishou.com>
2024-01-09 10:22:58 +0800

    feat: complete radio aidataset define

---
+++ src/form-coms/radio/aiDataset.ts
@@ -0,0 +1,44 @@
+import { DataSetItem, defineDataSet } from 'ai-dataset';
+
+export default defineDataSet((utils) => {
+  const result: DataSetItem[] = [];
+
+  // ======================== 禁用状态 ========================
+  result.push(
+    ...utils.options.switch().map((item) => ({
+      Q: `${item.label}禁用状态`,
+      A: {
+        data: {
+          config: {
+            disabled: item.value
+          }
+        }
+      }
+    }))
+  );
+
+  // ======================== 选项配置 ========================
+  result.push(
+    ...utils.repeat(() => {
+      const options = utils.repeat(() => utils.word.noun(), 5);
+      return {
+        Q: `选项配置为${options.map((option) => `“${option}”`).join('、')}`,
+        A: {
+          data: {
+            options: options.map((option) => ({
+              label: option,
+              value: option,
+              type: 'default',
+              key: utils.datatype.uuid()
+            }))
+          }
+        }
+      };
+    })
+  );
+
+  // ======================== 校验规则 ========================
+  // TODO: src/form-coms/radio/editors.ts:294
+
+  return result;
+});
