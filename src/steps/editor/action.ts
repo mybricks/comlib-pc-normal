ea4099d83212744e60ee50dda266d93bf2d85c7e
tangxiaoxin <1302947749@qq.com>
2024-01-16 19:32:56 +0800

    steps style

--- src/steps/editor/action.ts
+++ src/steps/editor/action.ts
@@ -2,83 +2,94 @@ import { Data, Btn } from '../constants';
 export default {
   '[data-item-type="stepActions"]': {
     title: '操作项',
-    items: [
-      {
-        title: '按钮组',
-        type: 'select',
-        options: {
+    items({ focusArea }: EditorResult<Data>, cate1) {
+      if(!focusArea) return
+      cate1.title = "操作栏"
+      cate1.items = [
+        {
+          title: '按钮组',
+          type: 'select',
+          options: {
+            options: [
+              {
+                label: "上一步",
+                value: "previous"
+              },
+              {
+                label: "下一步",
+                value: "next"
+              },
+              {
+                label: "提交",
+                value: "submit"
+              },
+            ],
+            mode: "multiple"
+          },
+          value: {
+            get({ data }: EditorResult<Data>) {
+              return data.toolbar.btns || [];
+            },
+            set({ data }: EditorResult<Data>, val: Array<Btn>) {
+              data.toolbar.btns = val
+            }
+          }
+        },
+        {
+          title: '对齐方式',
+          type: 'Select',
           options: [
-            {
-              label: "上一步",
-              value: "previous"
-            },
-            {
-              label: "下一步",
-              value: "next"
-            },
-            {
-              label: "提交",
-              value: "submit"
-            },
+            { label: '左对齐', value: 'flex-start' },
+            { label: '居中', value: 'center' },
+            { label: '右对齐', value: 'flex-end' }
           ],
-          mode: "multiple"
-        },
-        value: {
-          get({ data }: EditorResult<Data>) {
-            return data.toolbar.btns || [];
-          },
-          set({ data }: EditorResult<Data>, val: Array<Btn>) {
-            data.toolbar.btns = val
+          value: {
+            set({ data }: EditorResult<Data>, value: string) {
+              data.toolbar.actionAlign = value;
+            },
+            get({ data }: EditorResult<Data>) {
+              return data.toolbar.actionAlign;
+            }
           }
-        }
-      },
-      {
-        title: '对齐方式',
-        type: 'Select',
-        options: [
-          { label: '左对齐', value: 'flex-start' },
-          { label: '居中', value: 'center' },
-          { label: '右对齐', value: 'flex-end' }
-        ],
-        value: {
-          set({ data }: EditorResult<Data>, value: string) {
-            data.toolbar.actionAlign = value;
-          },
-          get({ data }: EditorResult<Data>) {
-            return data.toolbar.actionAlign;
-          }
-        }
-      },
-      {
-        title: '置底',
-        type: 'switch',
-        ifVisible({ data }: EditorResult<Data>) {
-          return !!data.toolbar.showActions;
         },
-        value: {
-          get({ data }: EditorResult<Data>) {
+        {
+          title: '置底',
+          type: 'switch',
+          ifVisible({ data }: EditorResult<Data>) {
+            return !!data.toolbar.showActions;
+          },
+          value: {
+            get({ data }: EditorResult<Data>) {
+              return !!data.toolbar.fixed;
+            },
+            set({ data }: EditorResult<Data>, val: boolean) {
+              data.toolbar.fixed = val
+            }
+          }
+        },
+        {
+          title: '底部距离',
+          type: 'inputNumber',
+          options: [{ min: 0, max: 100, width: 120 }],
+          ifVisible({ data }: EditorResult<Data>) {
             return !!data.toolbar.fixed;
           },
-          set({ data }: EditorResult<Data>, val: boolean) {
-            data.toolbar.fixed = val
+          value: {
+            get({ data }: EditorResult<Data>) {
+              return [data.toolbar.bottom];
+            },
+            set({ data }: EditorResult<Data>, val: number[]) {
+              data.toolbar.bottom = val[0]
+            }
           }
         }
-      },
+      ]
+    },
+    style: [
       {
-        title: '底部距离',
-        type: 'inputNumber',
-        options: [{ min: 0, max: 100, width: 120 }],
-        ifVisible({ data }: EditorResult<Data>) {
-          return !!data.toolbar.fixed;
-        },
-        value: {
-          get({ data }: EditorResult<Data>) {
-            return [data.toolbar.bottom];
-          },
-          set({ data }: EditorResult<Data>, val: number[]) {
-            data.toolbar.bottom = val[0]
-          }
-        }
+        title: '样式',
+        options: ['padding', { type: 'background', config: { disableBackgroundImage: true } }, 'boxshadow'],
+        target: () => '.step-toolbar'
       }
     ]
   },
