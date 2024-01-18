19854952e293c14f46ba0fbb44c392c54041d7f5
yingpengsha <yingpengsha@kuaishou.com>
2024-01-18 14:07:37 +0800

    fix: card dataset issue

--- src/card/aiDataset.ts
+++ src/card/aiDataset.ts
@@ -4,37 +4,45 @@ export default defineDataSet((utils) => {
   const result = {};
 
   // 显示标题
-  result['显示标题'] = {
-    Q: '如何显示标题？',
-    A: {
-      showTitle: true
+  result['显示标题'] = [
+    {
+      Q: '如何显示标题？',
+      A: {
+        showTitle: true
+      }
     }
-  };
+  ];
   // 标题内容
   const title = utils.lorem.word();
-  result['标题内容'] = {
-    Q: `修改标题内容为${title}`,
-    A: {
-      showTitle: true,
-      title: title
+  result['标题内容'] = [
+    {
+      Q: `修改标题内容为${title}`,
+      A: {
+        showTitle: true,
+        title: title
+      }
     }
-  };
+  ];
   // 开启卡片右上角操作
-  result['开启卡片右上角操作'] = {
-    Q: `如何开启右上角操作？`,
-    A: {
-      useExtra: true
+  result['开启卡片右上角操作'] = [
+    {
+      Q: `如何开启右上角操作？`,
+      A: {
+        useExtra: true
+      }
     }
-  };
+  ];
 
   // 操作组
-  result['操作组'] = {
-    Q: `如何开启操作组？`,
-    A: {
-      isAction: true,
-      items: []
+  result['操作组'] = [
+    {
+      Q: `如何开启操作组？`,
+      A: {
+        isAction: true,
+        items: []
+      }
     }
-  };
+  ];
 
   // 操作项配置
   result['操作项配置'] = [
