7e5a1ed01c13171b4e46fadd6a0dde5efacb60ca
yingpengsha <yingpengsha@kuaishou.com>
2024-01-18 11:57:07 +0800

    feat: add card dataset

--- src/card/aiDataset.ts
+++ src/card/aiDataset.ts
@@ -1,36 +1,41 @@
-({Util}) => {
-  const result = {}
+import { defineDataSet, DataSetItem } from 'ai-dataset';
+
+export default defineDataSet((utils) => {
+  const result = {};
+
   // 显示标题
   result['显示标题'] = {
     Q: '如何显示标题？',
     A: {
-      showTitle: true,
-    },
-  }
+      showTitle: true
+    }
+  };
   // 标题内容
-  const title = Util.random.word()
+  const title = utils.lorem.word();
   result['标题内容'] = {
     Q: `修改标题内容为${title}`,
     A: {
       showTitle: true,
       title: title
-    },
-  }
+    }
+  };
   // 开启卡片右上角操作
   result['开启卡片右上角操作'] = {
     Q: `如何开启右上角操作？`,
     A: {
       useExtra: true
-    },
-  }
+    }
+  };
+
   // 操作组
   result['操作组'] = {
     Q: `如何开启操作组？`,
     A: {
       isAction: true,
-      items: [],
-    },
-  }
+      items: []
+    }
+  };
+
   // 操作项配置
   result['操作项配置'] = [
     {
@@ -40,12 +45,12 @@
         items: [
           {
             name: `操作项1`,
-            key: Util.uuid()
+            key: utils.string.uuid()
           }
-        ],
-      },
+        ]
+      }
     }
-  ]
+  ];
 
-  return result
-}
+  return result;
+});
