4191adc2b8e4dee2f79102e48b1b19c61afbbae5
z35635 <linzhujlu@foxmail.com>
2024-01-30 15:11:58 +0800

    chore

--- src/form-coms/form-container/upgrade.ts
+++ src/form-coms/form-container/upgrade.ts
@@ -89,7 +89,8 @@ export default function ({ data, input, output, slot, children, setDeclaredStyle
     fontSize: '14px',
     fontWeight: 400,
     color: 'rgba(0, 0, 0, 0.85)',
-    fontStyle: 'normal'
+    fontStyle: 'normal',
+    textDecoration: void 0
   };
   const defaultDescriptionStyle = {
     whiteSpace: 'pre-wrap',
