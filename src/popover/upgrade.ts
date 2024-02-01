b52e51eb18c828dafe5595e4d8e7c8385beeea71
tangxiaoxin <1302947749@qq.com>
2024-02-01 11:43:07 +0800

    popover

--- src/popover/upgrade.ts
+++ src/popover/upgrade.ts
@@ -6,5 +6,18 @@ export default function ({ input, output, data, id, setDeclaredStyle }: UpgradeP
     setDeclaredStyle(`.{id} .ant-popover-arrow-content, .{id} .ant-popover-inner .ant-popover-inner-content`, { ...data.style });
     data.style = {};
   }
+
+  const dynamicTitle = input.get('_title')
+  if(dynamicTitle) {
+    output.add('_titleComplete', '完成', {type: 'any'})
+    dynamicTitle.setRels(['_titleComplete'])
+  }
+  
+  const dynamicContent = input.get('content')
+  if(dynamicContent) {
+    output.add('contentComplete', '完成', {type: 'any'})
+    dynamicContent.setRels(['contentComplete'])
+  }
+
   return true;
 }
