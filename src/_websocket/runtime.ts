f8f35253c4a540fde6cec3f121daae48752c4e4c
liuzhigang06 <liuzhigang06@kuaishou.com>
2024-02-04 19:26:20 +0800

    fix(websocket): fix error

--- src/_websocket/runtime.ts
+++ src/_websocket/runtime.ts
@@ -1,38 +1,41 @@
 function callCon({ env, data, outputs, logger }) {
   const { wsUrl } = data;
-  let connected = false
 
-  if (wsUrl) {
-    // 创建一个新的WebSocket对象
-    var websocket = new WebSocket(wsUrl);
+  try {
+    if (wsUrl) {
+      // 创建一个新的WebSocket对象
+      var websocket = new WebSocket(wsUrl);
 
-    // 连接成功建立时触发
-    websocket.onopen = function (evt) {
-      connected = true
-      outputs['onconnect'](`连接成功`);
-      if (typeof env?.runtime?.onComplete === 'function') {
-        env.runtime.onComplete(() => {
-          websocket?.close()
-        })
-      }
-    };
+      // 连接成功建立时触发
+      websocket.onopen = function (evt) {
+        outputs['onconnect'](`连接成功`);
+        if (typeof env?.runtime?.onComplete === 'function') {
+          env.runtime.onComplete(() => {
+            websocket?.close()
+          })
+        }
+      };
 
-    // 接收到消息时触发
-    websocket.onmessage = function (evt) {
-      outputs['onmessage'](evt.data);
-    };
+      // 接收到消息时触发
+      websocket.onmessage = function (evt) {
+        outputs['onmessage'](evt.data);
+      };
 
-    // 连接关闭时触发
-    websocket.onclose = function (evt) {
-      outputs['onClose'](evt);
-    };
+      // 连接关闭时触发
+      websocket.onclose = function (evt) {
+        outputs['onClose'](evt);
+      };
 
-    // 发生错误时触发
-    websocket.onerror = function (evt) {
-      outputs['onerror'](`发生错误`, evt?.data);
-    };
-  } else {
-    outputs['onerror'](`没有设置websocket地址`);
+      // 发生错误时触发
+      websocket.onerror = function (evt) {
+        outputs['onerror'](`发生错误`, evt?.data);
+      };
+    } else {
+      outputs['onerror'](`没有设置websocket地址`);
+    }
+  } catch (e) {
+    console.error(`websocket组件运行错误.`, e);
+    outputs['onerror'](`websocket创建失败，请检查地址是否正确`);
   }
 }
 
