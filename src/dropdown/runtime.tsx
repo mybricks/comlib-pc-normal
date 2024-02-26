152d2a5cac5c38a2892d76f2e9b5cd7d3af836aa
Bigbig-Huang <hqy09092020@163.com>
2024-02-26 18:05:06 +0800

    feat(dropdown):add dynamic options

--- src/dropdown/runtime.tsx
+++ src/dropdown/runtime.tsx
@@ -1,13 +1,87 @@
-import React, { useMemo } from 'react';
+import React, { useEffect, useMemo } from 'react';
 import { Menu, Dropdown, Space } from 'antd';
 import { DownOutlined } from '@ant-design/icons';
 import { Data } from './types';
 import * as Icons from '@ant-design/icons';
+import { uuid } from '../utils';
 
 export default function ({ data, env, style, inputs, outputs, slots, id }: RuntimeParams<Data>) {
   const { edit, runtime } = env;
   const debug = !!(runtime && runtime.debug);
+  const rowKey = '_itemKey';
+
+
+  //设置数据源输入
+  useEffect(() => {
+    if (env.runtime) {
+      inputs['setDynamicOptions']((v, relOutputs) => {
+        if (Array.isArray(v)) {
+          const ds = v.map((item,index)=>{
+            if(item.value){
+              return {
+                value: item.value,
+                disabled: item.disabled ? true: false,
+                [rowKey]: uuid(),
+                index: index
+              }
+            }else{
+              return {
+                value: item,
+                [rowKey]: uuid(),
+                index: index
+              }
+            }
+          })
+          data.dynamicOptions = ds;
+          relOutputs['setDynamicOptionsDone'](v);
+        }
+      });
+    }
+  }, []);
+
   /*menu数据的渲染*/
+  function dynamicMenuRender({ data }: { data: Data }){
+    if(env.edit){
+      return (
+        <Menu style={{ width: data.width }}>
+          <Menu.Item>
+            {
+              slots['item'].render()
+            }
+          </Menu.Item>          
+        </Menu>
+      )
+    }else if(env.runtime){
+      return (
+      <Menu style={{ width: data.width }}>
+        {data.dynamicOptions &&
+          data.dynamicOptions.map((option, index) => {
+            return (
+              <Menu.Item
+                data-menu-item={option[rowKey]}
+                disabled={option.disabled}
+                //style={{ color: option.disabled ? void 0 : option.iconColor }}
+                key={index}
+                onClick={() => onClick(option)}
+              >
+                {
+                  slots['item'].render(
+                    {
+                      inputValues: {
+                        itemData: option.value,
+                        index: index
+                      },
+                      key: option[rowKey]
+                    }
+                  )
+                }
+              </Menu.Item>
+            );
+          })}
+      </Menu>
+    );
+    }
+  }
   function menuRender({ data }: { data: Data }) {
     return (
       <Menu style={{ width: data.width }}>
@@ -42,21 +116,27 @@ export default function ({ data, env, style, inputs, outputs, slots, id }: Runti
 
   // 选项改变
   const onClick = (option) => {
-    outputs['onChange']({
-      label: option.label,
-      link: option.link,
-      key: option.key
-    });
+    if(data.isDynamic){
+      outputs['onChange'](
+        option
+      )
+    }else{
+      outputs['onChange']({
+        label: option.label,
+        link: option.link,
+        key: option.key
+      });
+    }
   };
 
   return (
     <div>
       <Dropdown
         overlayClassName={id}
-        overlay={menuRender({ data })}
+        overlay={data.isDynamic ? dynamicMenuRender({data}) : menuRender({ data })}
         placement={data.placement}
         arrow
-        visible={(edit && data.isChildCustom) || env.design ? true : edit ? false : void 0}
+        visible={(edit && data.isChildCustom) || env.design || data.isDynamic ? true : edit ? false : void 0}
         getPopupContainer={(triggerNode: HTMLElement) => env?.canvasElement || document.body}
         overlayStyle={{ minWidth: data.width }}
         trigger={[data.trigger || 'hover']}
