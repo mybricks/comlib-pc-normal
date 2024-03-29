faf9be8be087bb60e0ec81ca7677c23c3e287bc3
wudi27 <wudi27@kuaishou.com>
2024-03-29 15:08:03 +0800

    feat: 统一版本升级

--- src/tabs/test/tab.cy.ts
+++ src/tabs/test/tab.cy.ts
@@ -3,7 +3,13 @@ import dynamicTabs from './case/dynamic-tabs.json';
 
 describe('Tabs', () => {
   enhancedIt('动态tabs', () => {
-    dumpPreview(dynamicTabs, []);
+    dumpPreview(dynamicTabs, [{
+      type: 'contains',
+      selector: 'div',
+      text: '标签页1'
+    }]);
+    // 等待动画执行完成再截图
+    cy.wait(300)
     cy.compareSnapshot('动态tabs');
   });
 });
