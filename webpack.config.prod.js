4668a163eb18700c9d7ce8a70d2ba2c2fd246433
GodStream <zpqwangyiyouxiang@163.com>
2024-04-29 19:05:59 +0800

    build: update

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -66,35 +66,34 @@ module.exports = {
       }
     ]
   },
-  // externals: [
-  //   {
-  //     react: {
-  //       commonjs: 'react',
-  //       commonjs2: 'react',
-  //       amd: 'react',
-  //       root: 'React'
-  //     },
-  //     'react-dom': {
-  //       commonjs: 'react-dom',
-  //       commonjs2: 'react-dom',
-  //       amd: 'react-dom',
-  //       root: 'ReactDOM'
-  //     },
-  //     moment: 'moment',
-  //     antd: 'antd',
-  //     '@ant-design/icons': 'icons',
-  //     '@ant-design/charts': 'charts'
-  //   }
-  // ],
   externals: [
     {
-      react: 'react',
-      'react-dom': 'react-dom',
-      antd: 'antd',
+      react: {
+        commonjs: 'react',
+        commonjs2: 'react',
+        amd: 'react',
+        root: 'React'
+      },
+      'react-dom': {
+        commonjs: 'react-dom',
+        commonjs2: 'react-dom',
+        amd: 'react-dom',
+        root: 'ReactDOM'
+      },
       moment: 'moment',
-      '@ant-design/icons': '@ant-design/icons'
+      antd: 'antd',
+      '@ant-design/icons': 'icons'
     }
   ],
+  // externals: [
+  //   {
+  //     react: 'react',
+  //     'react-dom': 'react-dom',
+  //     antd: 'antd',
+  //     moment: 'moment',
+  //     '@ant-design/icons': '@ant-design/icons'
+  //   }
+  // ],
   plugins: [
     // new BundleAnalyzerPlugin({
     //   analyzerMode: 'static'
