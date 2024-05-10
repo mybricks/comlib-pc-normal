8cf5fdd905de247aaa8f686d20035994162a992b
zhanlin <zhanlin@kuaishou.com>
2024-05-10 15:10:36 +0800

    feat: excel导出新增动态文件名&列宽

--- src/_excel/types.ts
+++ src/_excel/types.ts
@@ -1,10 +1,19 @@
 export type Sheet = {
   name: string;
-  header: string[];
+  header?: string[];
+  columns?: {
+    width: number;
+    hidden: boolean;
+  }[];
   data: Array<Record<string, any>>;
 };
 
 export interface Data {
-  filename: string
+  filename: string;
+  useDynamicFilename: boolean;
   dataSource: Array<Partial<Sheet>>;
+}
+
+export enum InputIds {
+  Filename = 'input.filename'
 }
--- src/_excel/upgrade.ts
+++ src/_excel/upgrade.ts
@@ -14,5 +14,13 @@ export default function ({
     output.add('exportComplete', '完成', { type: 'any' });
     input.get('dataSource').setRels(['exportComplete']);
   }
+
+  /**
+   * @description v1.0.3 -> v1.0.4 新增动态设置导出文件名
+  */
+  if(data.useDynamicFilename === undefined) {
+    data.useDynamicFilename = false;
+  }
+
   return true;
 }
