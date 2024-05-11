ebbdc43975e339fb1eefff0b562d189f3776beba
zhanlin <zhanlin@kuaishou.com>
2024-05-11 15:29:54 +0800

    fix: _excel input schema

--- src/_excel/upgrade.ts
+++ src/_excel/upgrade.ts
@@ -16,10 +16,53 @@ export default function ({
   }
 
   /**
-   * @description v1.0.3 -> v1.0.4 新增动态设置导出文件名
-  */
-  if(data.useDynamicFilename === undefined) {
+   * @description v1.0.3 -> v1.0.4 新增动态设置导出文件名、列宽配置
+   */
+  if (data.useDynamicFilename === undefined) {
     data.useDynamicFilename = false;
+  }
+
+  if (input.get('dataSource')) {
+    const dataSourceSchema = {
+      type: 'array',
+      items: {
+        type: 'object',
+        properties: {
+          name: {
+            type: 'string'
+          },
+          header: {
+            type: 'array',
+            items: {
+              type: 'string'
+            }
+          },
+          columns: {
+            type: 'array',
+            items: {
+              type: 'object',
+              properties: {
+                width: {
+                  type: 'number'
+                },
+                hidden: {
+                  type: 'boolean'
+                }
+              }
+            }
+          },
+          data: {
+            type: 'array',
+            items: {
+              type: 'object'
+            }
+          }
+        }
+      }
+    };
+    input.get('dataSource').setSchema(dataSourceSchema);
+    input.get('dataSource').setId('input.dataSource');
+    input.get('input.dataSource').setRels([]);
   }
 
   return true;
