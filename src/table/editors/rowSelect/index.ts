881e834a59c588afdc6edbcf22f5a323c9eb08fc
liuzhigang06 <liuzhigang06@kuaishou.com>
2024-05-23 20:21:02 +0800

    fix: fix table select style

--- src/table/editors/rowSelect/index.ts
+++ src/table/editors/rowSelect/index.ts
@@ -1,3 +1,4 @@
+import { getFilterSelector } from '../../../utils/cssSelector';
 import { Data } from '../../types';
 
 const rowSelectEditor = {
@@ -16,20 +17,20 @@ const rowSelectEditor = {
       {
         title: '表头',
         catelog: '默认',
-        options: ['border', { type: 'background', config: { disableBackgroundImage: true } }],
+        options: ['font', 'border', { type: 'background', config: { disableBackgroundImage: true } }],
         ifVisible({ data }: EditorResult<Data>) {
           return data.useRowSelection;
         },
-        target: `.ant-table-thead .ant-table-cell.ant-table-selection-column`,
+        target: ({ id }) => `table thead.ant-table-thead tr th.ant-table-cell.ant-table-selection-column${getFilterSelector(id)}`,
       },
       {
         title: '内容',
         catelog: '默认',
-        options: ['border', { type: 'background', config: { disableBackgroundImage: true } }],
+        options: ['font', 'border', { type: 'background', config: { disableBackgroundImage: true } }],
         ifVisible({ data }: EditorResult<Data>) {
           return data.useRowSelection;
         },
-        target: `.ant-table-tbody .ant-table-cell.ant-table-selection-column`,
+        target: ({ id }) => `table .ant-table-tbody tr .ant-table-cell.ant-table-selection-column${getFilterSelector(id)}`,
       },
       {
         title: '勾选框',
