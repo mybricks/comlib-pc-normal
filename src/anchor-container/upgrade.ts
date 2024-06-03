0d640496d2131b063fffa56e10b0dc8b87ff4478
zhanlin <zhanlin@kuaishou.com>
2024-06-03 19:34:07 +0800

    feat: 锚点容器新增锚点链接位置配置&修复固定显示锚点链接不生效

--- src/anchor-container/upgrade.ts
+++ src/anchor-container/upgrade.ts
@@ -13,5 +13,12 @@ export default function ({ data, input, output }: UpgradeParams<Data>): boolean 
     data.useDynamicData = true;
   };
 
+  /**
+    * @description v1.0.7 增加锚点位置配置
+  */
+  if (typeof data.anchorPosition === "undefined") {
+    data.anchorPosition = 'right';
+  };
+
   return true;
 }
