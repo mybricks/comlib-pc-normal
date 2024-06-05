9db86280a744cca8015822b34d6e0814efda2787
wufan12 <wufan12@kuaishou.com>
2024-06-05 19:25:50 +0800

    feat: 提交

--- src/switch-container/type.ts
+++ src/switch-container/type.ts
@@ -11,6 +11,4 @@ export interface Data {
     value: boolean | number | string;
     /** 编辑态右侧选中的状态 */
     _editSelectId_?: string
-    /** 编辑态，默认数组选择器，active的类名，类名来自editor内部，编译后是会变化的，这里记录一下 */
-    _activeArrayClass?: string
 }
