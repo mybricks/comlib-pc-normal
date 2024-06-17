1c9dbf7118a5eb2d96449fa2c0c1ca61b96e0515
wudi27 <wudi27@kuaishou.com>
2024-06-17 16:27:21 +0800

    fix: 修复富文本批量上传图片失败的问题

--- src/form-coms/input-richtext/hooks/use-upload.tsx
+++ src/form-coms/input-richtext/hooks/use-upload.tsx
@@ -11,16 +11,43 @@ export interface UploadFn {
 }
 
 export default function useUpload(inputs: any, outputs: any) {
+  const uploadQueueRef = useRef<
+    {
+      params: UploadParams;
+      res: (response: { url: string }) => void;
+      rej: (errMsg: string) => void;
+    }[]
+  >([]);
+  const runRef = useRef(false);
+
   const resolvedRef = useRef<any>(null);
   const rejectedRef = useRef<any>(null);
 
   const upload: UploadFn = async (params) => {
-    const responsePromise = new Promise<{ url: string }>((res, rej) => {
-      resolvedRef.current = res;
-      rejectedRef.current = rej;
+    return new Promise((res, rej) => {
+      uploadQueueRef.current.push({ params, res, rej });
+      if (!runRef.current) resolvingUploadQueue();
     });
-    outputs['upload'](params);
-    return await responsePromise;
+  };
+
+  const resolvingUploadQueue = async () => {
+    runRef.current = true;
+    while (uploadQueueRef.current.length > 0) {
+      const { params, rej: reject, res: resolve } = uploadQueueRef.current.shift()!;
+      const responsePromise = new Promise<{ url: string }>((res, rej) => {
+        resolvedRef.current = (response: { url: string }) => {
+          res(response);
+          resolve(response);
+        };
+        rejectedRef.current = (errMsg: string) => {
+          rej(errMsg);
+          reject(errMsg);
+        };
+      });
+      outputs['upload'](params);
+      await responsePromise;
+    }
+    runRef.current = false;
   };
 
   useEffect(() => {
