dd6196d31f05b42b04953dc9ff05f75a517ac837
Bigbig-Huang <hqy09092020@163.com>
2024-06-18 10:36:38 +0800

    fix:(config-toolbar):remove scenceId

--- src/_template-compute/_config-toolbar/upgrade.ts
+++ src/_template-compute/_config-toolbar/upgrade.ts
@@ -3,7 +3,7 @@ export default function ({ input, output, data }): boolean {
    * @description v1.0.2->1.0.3 , remove sceneId场景ID
    */
   if (input.get('creator.sceneId')) {
-    input.remove("creator.sceneId");
+    input.remove('creator.sceneId');
   }
   return true;
 }
