3933f49d1b9463cdfc23b3d3469b74da7f9adfa2
wufan12 <wufan12@kuaishou.com>
2024-07-05 11:10:57 +0800

    feat: 动态表单项禁用测试用例

--- src/form-coms/form-list/test/form-list.cy.ts
+++ src/form-coms/form-list/test/form-list.cy.ts
@@ -1,4 +1,6 @@
 import dump_表单项标题配置检查 from './case-表单项标题配置/dump.json';
+import dump_设置动态表单项禁用启用检查 from './case-disable-form-list/dump.json';
+
 import { dumpPreview, enhancedIt } from '@/../cypress/tools';
 
 describe('动态表单项', () => {
@@ -11,4 +13,21 @@ describe('动态表单项', () => {
     // 截图对比
     cy.compareSnapshot('动态表单项_表单项标题配置');
   });
+
+  enhancedIt('动态表单项添加多项后，删除再禁用', () => {
+    // 加载测试页面
+    dumpPreview(dump_设置动态表单项禁用启用检查);
+    // 添加一项
+    cy.contains('button', '添加一项').click();
+    cy.contains('button', '添加一项').click();
+    cy.contains('button', '添加一项').click();
+
+
+    cy.get('div#u_Y4IsW').children('div').eq(2).contains('button', '删除').click()
+
+    cy.contains('button', '禁用').click()
+
+    // 截图对比
+    cy.compareSnapshot('动态表单项_设置禁用');
+});
 });
