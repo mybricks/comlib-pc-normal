c62774bd7274be0846afb9ab0ae2792785ff5441
zhuba-Ahhh <3477826311@qq.com>
2024-07-30 19:12:24 +0800

    chore(time-range-picker): description

--- src/form-coms/time-range-picker/editors.ts
+++ src/form-coms/time-range-picker/editors.ts
@@ -322,6 +322,7 @@ export default {
             {
               title: '时间格式模版',
               type: 'select',
+              description: '将输出(值变化事件和表单提交)的数据设置成所需要的格式',
               options: {
                 options: [
                   { label: '时:分:秒', value: 'HH:mm:ss' },
@@ -344,6 +345,7 @@ export default {
             {
               title: '自定义模版',
               type: 'text',
+              description: '自定义输出数据处理模版',
               ifVisible({ data }: EditorResult<Data>) {
                 return data.format === 'custom';
               },
@@ -379,6 +381,7 @@ export default {
             {
               title: '分隔符',
               type: 'text',
+              description: '配置时间之间的分隔符',
               ifVisible({ data }: EditorResult<Data>) {
                 return data.outFormat === 'string';
               },
@@ -398,7 +401,7 @@ export default {
           items: [
             {
               title: '时',
-              description: '展示可选的小时',
+              description: '设置小时的步长，展示可选的小时',
               type: 'inputNumber',
               options: [{ width: 100, min: 1, max: 24 }],
               value: {
@@ -412,7 +415,7 @@ export default {
             },
             {
               title: '分',
-              description: '展示可选的分钟',
+              description: '设置分的步长，展示可选的分钟',
               type: 'inputNumber',
               options: [{ width: 100, min: 0, max: 60 }],
               value: {
@@ -426,7 +429,7 @@ export default {
             },
             {
               title: '秒',
-              description: '展示可选的秒',
+              description: '设置秒的步长，展示可选的秒',
               type: 'inputNumber',
               options: [{ width: 100, min: 0, max: 60 }],
               value: {
@@ -501,6 +504,8 @@ export default {
             {
               title: '校验触发事件',
               type: '_event',
+              description:
+                '自定义校验的触发事件，开启自定义校验后校验时会触发【触发校验】输出项事件',
               ifVisible({ data }: EditorResult<Data>) {
                 const customRule = (data.rules || defaultRules).find(
                   (i) => i.key === RuleKeys.CUSTOM_EVENT
@@ -519,6 +524,8 @@ export default {
             {
               title: '值初始化',
               type: '_event',
+              description:
+                '设置时间范围选择框的初始值时触发，可以通过逻辑连线连接时间范围选择框的输入项【设置初始值】触发【值初始化】输出项事件',
               options: {
                 outputId: 'onInitial'
               }
@@ -526,6 +533,8 @@ export default {
             {
               title: '值更新',
               type: '_Event',
+              description:
+                '时间范围选择框的值发生变化时触发，可以通过逻辑连线连接时间范围选择框的输入项【设置值】或用户选择日期触发【值更新】输出项事件',
               options: {
                 outputId: 'onChange'
               }
