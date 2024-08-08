cce6aba3015d77b9046d814466d06374c6a0fdcf
wudi27 <wudi27@kuaishou.com>
2024-08-08 14:37:32 +0800

    fix: 修复数据表格组件传入数字0不展示的问题

--- src/table/components/ColumnRender.tsx
+++ src/table/components/ColumnRender.tsx
@@ -89,7 +89,7 @@ function ColumnRender(props: ColumnRenderProps) {
           </Tooltip>
         );
       } else {
-        return value || null;
+        return value ?? null;
       }
     case ContentTypeEnum.SlotItem:
       return (
@@ -112,7 +112,7 @@ function ColumnRender(props: ColumnRenderProps) {
         </TableContext.Consumer>
       );
     default:
-      return value || null;
+      return value ?? null;
   }
 }
 
