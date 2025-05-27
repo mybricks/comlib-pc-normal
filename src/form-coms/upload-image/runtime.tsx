638292409281067e6a5f73ba6f9c1034840d79f3
MrMa <mtf986252757@gmail.com>
2025-05-27 16:09:34 +0800

    feat: checkbox修改；直接内容修改；新增图片上传组件

---
+++ src/form-coms/upload-image/runtime.tsx
@@ -0,0 +1,561 @@
+import { Upload, message, Image, UploadFile } from 'antd';
+import { render, unmountComponentAtNode } from 'react-dom';
+import { PlusOutlined } from '@ant-design/icons';
+import React, { useCallback, useEffect, useLayoutEffect, useMemo, useRef, useState } from 'react';
+import { RuleKeys, validateFormItem } from '../utils/validator';
+import { debounceValidateTrigger } from '../form-container/models/validate';
+import cls from 'classnames';
+import { onChange as onChangeForFc } from '../form-container/models/onChange';
+import { slotInputIds } from '../form-container/constants';
+
+import css from './runtime.less';
+import { OutputIds, ValidateInfo } from '../types';
+
+interface UploadConfig {
+  buttonText: string;
+  name: string;
+  fileType: string[];
+  fileSize: number;
+  fileCount: number;
+  fileKey: string;
+  usePreview?: boolean;
+  multiple?: boolean;
+  uploadIcon?: string;
+  disabled: boolean;
+  useCustomRemove: boolean;
+}
+interface UploadFileList {
+  uid?: string;
+  name: string;
+  status?: string;
+  url?: string;
+  percent?: number;
+  thumbUrl?: string;
+}
+export interface Data {
+  rules: any[];
+  config: UploadConfig;
+  isCustom: boolean;
+  imageSize: number[];
+  customUpload: boolean;
+  fileClick: boolean;
+  isEditable: boolean;
+}
+
+export default function ({
+  env,
+  data,
+  inputs,
+  outputs,
+  slots,
+  parentSlot,
+  id,
+  name
+}: RuntimeParams<Data>) {
+  const [fileList, setFileList] = useState<UploadFile[]>([]);
+  const fileListRef = useRef<UploadFile[]>([]);
+  const removeFileRef = useRef<UploadFile>();
+
+  const uploadInputRef = useRef(null);
+  // fileListRef.current = fileList;
+  const uploadRef = useRef(null);
+  const {
+    fileKey,
+    fileCount,
+    buttonText,
+    fileSize,
+    fileType,
+    usePreview,
+    multiple
+  } = data.config;
+
+  const validateRelOutputRef = useRef<any>(null);
+
+  const checkIsValidFileListVal = (val) => {
+    if (!Array.isArray(val)) {
+      return false;
+    } else {
+      return val.every((item) => item?.name !== undefined);
+    }
+  };
+
+  useLayoutEffect(() => {
+    // ≥ v1.0.34 设置上传结果
+    slots['customUpload']?.outputs['setFileInfo']?.((file) => {
+      onUploadComplete(file);
+      parentSlot?._inputs[slotInputIds.VALIDATE_TRIGGER]?.({ id: id, name: name });
+    });
+
+    inputs['setValue']?.((val: UploadFile[], relOutputs) => {
+      if (!checkIsValidFileListVal(val)) {
+        return;
+      }
+      changeFileList(val);
+      if (relOutputs['setValueDone']) {
+        relOutputs['setValueDone'](val);
+      }
+    });
+    inputs['setInitialValue'] &&
+      inputs['setInitialValue']((val, relOutputs) => {
+        if (!checkIsValidFileListVal(val)) {
+          return;
+        }
+        changeFileList(val);
+        if (relOutputs['setInitialValueDone']) {
+          relOutputs['setInitialValueDone'](val);
+        }
+        outputs[OutputIds.OnInitial](val);
+      });
+    inputs['validate']?.((model, outputRels) => {
+      validateFormItem({
+        value: fileListRef.current,
+        model,
+        env,
+        rules: data.rules || []
+      })
+        .then((r) => {
+          const customRule = (data.rules || []).find((i) => i.key === RuleKeys.CUSTOM_EVENT);
+          if (customRule?.status) {
+            validateRelOutputRef.current = outputRels['returnValidate'];
+            outputs['onValidate'](fileListRef.current);
+          } else {
+            outputRels['returnValidate'](r);
+            debounceValidateTrigger(parentSlot, { id, name, validateInfo: r });
+          }
+        })
+        .catch((e) => {
+          outputRels['returnValidate'](e);
+          debounceValidateTrigger(parentSlot, { id, name, validateInfo: e });
+        });
+    });
+    inputs['setValidateInfo']?.((info: ValidateInfo, outputRels) => {
+      if (validateRelOutputRef.current) {
+        validateRelOutputRef.current(info);
+        outputRels['setValidateInfoDone'](info);
+        debounceValidateTrigger(parentSlot, { id, name, validateInfo: info });
+      }
+    });
+    inputs['getValue']?.((val, outputRels) => {
+      outputRels['returnValue'](fileListRef.current);
+    });
+
+    inputs['resetValue']?.((_, relOutputs) => {
+      changeFileList([]);
+      if (relOutputs['resetValueDone']) {
+        relOutputs['resetValueDone']();
+      }
+    });
+
+    //设置禁用
+    inputs['setDisabled']?.((_, relOutputs) => {
+      data.config.disabled = true;
+      if (relOutputs['setDisabledDone']) {
+        relOutputs['setDisabledDone']();
+      }
+    });
+    //设置启用
+    inputs['setEnabled']?.((_, relOutputs) => {
+      data.config.disabled = false;
+      if (relOutputs['setEnabledDone']) {
+        relOutputs['setEnabledDone']();
+      }
+    });
+
+    //设置启用/禁用
+    inputs['isEnable']?.((val, relOutputs) => {
+      if (val === true) {
+        data.config.disabled = false;
+        if (relOutputs['isEnableDone']) {
+          relOutputs['isEnableDone'](val);
+        }
+      } else {
+        data.config.disabled = true;
+        if (relOutputs['isEnableDone']) {
+          relOutputs['isEnableDone'](val);
+        }
+      }
+    });
+
+    //设置编辑/只读
+    inputs['isEditable']?.((val, relOutputs) => {
+      data.isEditable = val;
+      if (relOutputs['isEditableDone']) {
+        relOutputs['isEditableDone'](val);
+      }
+    });
+
+    // ＜ v1.0.34
+    inputs['uploadDone']?.((file: any, relOutputs) => {
+      onUploadComplete(file);
+      relOutputs['uploadComplete'](file);
+    });
+
+    inputs['remove']?.((file: any, relOutputs) => {
+      onRemoveFile(file?.uid ? file : removeFileRef.current || {});
+      relOutputs['removeDone'](file);
+    });
+  }, []);
+
+  const changeFileList = useCallback((newFileList) => {
+    fileListRef.current = newFileList || [];
+    setFileList(newFileList);
+    onChangeForFc(parentSlot, { id, value: newFileList, name });
+  }, []);
+
+  const onRemoveFile = useCallback((file) => {
+    changeFileList(fileListRef.current.filter((item) => item.uid !== file.uid));
+  }, []);
+
+  const formatCompleteFile = (res: UploadFileList, tempList: UploadFileList[]) => {
+    let isUpload = false;
+    let newVal = tempList || [];
+    newVal.forEach((item) => {
+      if ((item.uid === res.uid || item.name === res.name) && item.status === 'uploading') {
+        isUpload = true;
+        Object.assign(item, {
+          status: 'done',
+          percent: 100,
+          ...res
+        });
+      }
+    });
+    if (!isUpload) {
+      newVal.forEach((item) => {
+        if (!isUpload && item.status === 'uploading') {
+          isUpload = true;
+          Object.assign(item, {
+            status: 'done',
+            percent: 100,
+            ...res
+          });
+        }
+      });
+    }
+    if (!isUpload) {
+      newVal.push({
+        status: 'done',
+        percent: 100,
+        name: res.url,
+        ...res
+      });
+    }
+
+    return newVal;
+  };
+  const onUploadComplete = (res: UploadFileList | UploadFileList[]) => {
+    const list = Array.isArray(res) ? res : [res];
+    list.forEach((item) => {
+      formatCompleteFile(item, fileListRef.current);
+    });
+    changeFileList(onCheckFileCount([...fileListRef.current]));
+    outputs['uploadComplete'](res);
+  };
+
+  // 上传文件数字检查
+  const onCheckFileCount = (uploadedList) => {
+    if (fileCount && uploadedList.length > fileCount) {
+      uploadedList.shift();
+    }
+    return uploadedList;
+  };
+
+  // 文件数据格式化
+  const onFormatFileList = (fileList: UploadFile[]) => {
+    let uploadedList = [...(fileListRef.current || [])];
+    fileList.forEach((file) => {
+      const { name } = file;
+      const formItemResValue = {
+        name,
+        status: 'uploading',
+        percent: 0
+      };
+      uploadedList.push(formItemResValue);
+      if (fileCount && uploadedList.length > fileCount) {
+        uploadedList.shift();
+      }
+    });
+    return uploadedList;
+  };
+  // 文件上传输出
+  const onCustomRequest = (fileList: UploadFile[]) => {
+    if (!data.customUpload) {
+      console.log(env);
+      if (typeof env.uploadFile !== 'function') {
+        message.error(`应用的env中没有uploadFile方法`);
+        return;
+      }
+      env
+        .uploadFile(fileList)
+        .then((res) => {
+          onUploadComplete(res);
+        })
+        .catch((e) => {
+          message.error(String(e));
+          changeFileList([]);
+        });
+    } else {
+      const formData = new FormData();
+      fileList.forEach((file) => {
+        formData.append(fileKey, file);
+      });
+      changeFileList(onFormatFileList(fileList));
+      // ≥ v1.0.34
+      slots['customUpload']?.inputs['fileData'](formData);
+      // ＜ v1.0.34
+      outputs?.upload?.(formData);
+    }
+  };
+
+  //上传图片尺寸限制
+  //参数分别是上传的file，想要限制的宽，想要限制的高
+  const checkWH = (file, width, height) => {
+    return new Promise(function (resolve, reject) {
+      let filereader = new FileReader();
+      filereader.onload = (e: any) => {
+        let src: any = e.target.result;
+        const image = new window.Image();
+        image.onload = function () {
+          if (width && image.width != width) {
+            message.error(`请上传宽为${width}(px)的图片`);
+            //reject('error');
+          } else if (height && image.height != width) {
+            message.error(`请上传宽高为${height}}(px)的图片`);
+            // reject('error');
+          } else {
+            resolve('success');
+          }
+        };
+        image.onerror = reject;
+        image.src = src;
+      };
+      filereader.readAsDataURL(file);
+    });
+  };
+
+  // 文件合法校验
+  const beforeUpload = useCallback((file: File, fileList: File[]) => {
+    const acceptTypesList = fileType || [];
+    // 保证只执行最后一次
+    if (fileList[fileList.length - 1] !== file) return;
+    const isNotAccept = fileList.some((file) => {
+      let isImage = file.type.slice(0, 5) === 'image';
+      let isAcceptFileType = true;
+      if (acceptTypesList.length) {
+        isAcceptFileType = acceptTypesList.some((element) => {
+          return element.split('.').some((extname) => extname && file.name.includes);
+        });
+      } else {
+        isAcceptFileType = true;
+      }
+
+      if (!isAcceptFileType) {
+        message.error('上传文件格式不正确!');
+      }
+
+      const acceptFileSize = +fileSize;
+
+      let isAcceptFileSize = true;
+      if (acceptFileSize) {
+        isAcceptFileSize = file.size / 1024 / 1024 < acceptFileSize;
+      }
+
+      if (!isAcceptFileSize) {
+        message.error(`上传文件大小不能超过${acceptFileSize}MB!`);
+      }
+      return !(
+        isAcceptFileType &&
+        isAcceptFileSize &&
+        (JSON.stringify(data.imageSize) === JSON.stringify([0, 0]) || !isImage
+          ? true
+          : !checkWH(file, data.imageSize[0], data.imageSize[1]))
+      );
+    });
+
+    if (!isNotAccept) {
+      onCustomRequest(fileList);
+    }
+    return !isNotAccept;
+  }, []);
+
+  const onRemove = (file) => {
+    if (data.isEditable === false) {
+      return false;
+    } else {
+      if (!data.config.useCustomRemove) {
+        changeFileList(fileListRef.current.filter(({ uid }) => file.uid !== uid));
+        return true;
+      }
+      removeFileRef.current = file;
+      outputs.remove(file);
+      return false;
+    }
+  };
+
+  const onPreview = (file) => {
+    if (
+      usePreview &&
+      (/\.(jpg|jpeg|png|GIF|JPG|PNG)$/.test(file.name) ||
+        /\.(jpg|jpeg|png|GIF|JPG|PNG)$/.test(file.url))
+    ) {
+      onpenImgPreview(file.url);
+      if (data.fileClick && env.runtime) {
+        outputs['fileClick'](file);
+      }
+    } else if (data.fileClick && env.runtime) {
+      outputs['fileClick'](file);
+    } else {
+      window.open(file.url);
+    }
+  };
+
+  const ImgPreview = (props: any) => {
+    const [visible, setVisible] = useState(true);
+    const { src, onClose } = props;
+    const onVisibleChange = (visible) => {
+      if (!visible) {
+        setVisible(visible);
+        onClose();
+      }
+    };
+
+    return (
+      <Image
+        width={0}
+        height={0}
+        src={src}
+        preview={{
+          src,
+          visible,
+          onVisibleChange
+        }}
+      />
+    );
+  };
+  // 打开图片预览
+  const onpenImgPreview = (src) => {
+    let divEle = document.getElementById('img-preview');
+    if (!divEle) {
+      divEle = document.createElement('div');
+      divEle.setAttribute('id', 'img-preview');
+      document.body.appendChild(divEle);
+    }
+    const onClose = () => {
+      unmountComponentAtNode(divEle as HTMLElement);
+    };
+
+    render(<ImgPreview src={src} onClose={onClose} />, divEle);
+  };
+
+  const handleLabelClick = useCallback(
+    (e) => {
+      if (env.edit) {
+        e.stopPropagation();
+        return;
+      }
+    },
+    [env.edit]
+  );
+
+  const hideUploadButton = useMemo(() => {
+    if (env.edit) return false;
+    // 接收类型全是图片，且文件个数为1且已经上传了一个时，隐藏
+    const imageTypes = ['.jpg,.jpeg', '.png', '.svg', '.gif', '.tiff'];
+    const isAllAcceptImage =
+      fileType?.length && fileType.every((fType) => imageTypes.includes(fType));
+    return (
+      isAllAcceptImage &&
+      fileCount === 1 &&
+      (fileList || []).length === 1
+    );
+  }, [fileCount, fileList, fileType, env.edit]);
+
+  const outerSlotRender = useMemo(() => {
+    const imageTypes = ['.jpg,.jpeg', '.png', '.svg', '.gif', '.tiff'];
+    const isAllAcceptImage =
+      fileType?.length && fileType.every((fType) => imageTypes.includes(fType));
+    return fileCount === 1 && isAllAcceptImage;
+  }, [fileCount, fileList, fileType]);
+
+  // 上传按钮渲染
+  const renderUploadText = () => {
+    // 上传个数为1，且 当前文件列表为一个时，隐藏图片卡片的上传按钮
+    const pictureButton = (
+      <div>
+        <PlusOutlined />
+        <div style={{ marginTop: 8 }} className="upload-btn-text" onClick={handleLabelClick}>
+          {env.i18n(buttonText)}
+        </div>
+      </div>
+    );
+
+    return pictureButton;
+  };
+
+  const classnames = [css.uploadWrap];
+
+  // hideUploadButton ? css.uploadPictureCardHideWrap : ''
+  return (
+    <div ref={uploadRef} className={cls(classnames.join(' '))}>
+      <Upload
+        name={fileKey}
+        listType="picture-card"
+        fileList={Array.isArray(fileList) ? fileList : void 0}
+        accept={fileType.join()}
+        ref={uploadInputRef}
+        customRequest={() => {}}
+        beforeUpload={beforeUpload}
+        onRemove={onRemove}
+        onPreview={onPreview}
+        multiple={multiple}
+        maxCount={fileCount}
+        progress={{ strokeWidth: 5, showInfo: false }}
+        showUploadList={
+          hideUploadButton
+            ? false
+            : {
+                showPreviewIcon: usePreview
+              }
+        }
+        //iconRender={Icons && Icons[uploadIcon]?.render()}
+      >
+        {!outerSlotRender &&
+          slots['customUpload']?.render({
+            style: {
+              display: 'none'
+            }
+          })}
+        {data.isEditable ? (
+          hideUploadButton ? null : (
+            renderUploadText()
+          )
+        ) : (
+          ''
+        )}
+        {hideUploadButton ? (
+          <div className={css['custom-upload-image']}>
+            {fileList[0]?.url ? (
+              <img
+                src={fileList[0]?.url}
+                alt="uploaded"
+                onClick={(e) => e.stopPropagation()}
+                style={{ width: '100%', height: '100%', objectFit: 'cover' }}
+              />
+            ) : (
+              <span style={{ marginTop: '20%', display: 'inline-block' }}>Uploading </span>
+            )}
+            <div className={css['overlay-text']}>重新上传</div>
+          </div>
+        ) : (
+          <div></div>
+        )}
+      </Upload>
+      {outerSlotRender
+        ? slots['customUpload']?.render({
+            style: {
+              display: 'none'
+            }
+          })
+        : null}
+    </div>
+  );
+}
---
+++ src/form-coms/upload-image/upgrade.ts
@@ -0,0 +1,487 @@
+import { inputIds, outputIds, commonFormItemUpdateSchema } from '../form-container/constants';
+import { descriptionUp } from '../utils/descriptionUp';
+import { descriptionUpList } from './constants';
+import { getFilterSelector } from '../../utils/cssSelector';
+
+export default function ({
+  id,
+  input,
+  output,
+  slot,
+  data,
+  getDeclaredStyle,
+  removeDeclaredStyle,
+  setDeclaredStyle
+}): boolean {
+  //1.0.0 -> 1.0.1
+  input.get('setValue')?.setSchema({
+    type: 'array',
+    items: {
+      type: 'object',
+      properties: {
+        name: {
+          type: 'string'
+        },
+        url: {
+          type: 'string'
+        }
+      }
+    }
+  });
+  input.get('uploadDone')?.setSchema({
+    type: 'object',
+    properties: {
+      name: {
+        type: 'string'
+      },
+      url: {
+        type: 'string'
+      }
+    }
+  });
+
+  // 1.0.1 -> 1.0.2
+  output.get('upload')?.setSchema({
+    type: 'object'
+  });
+
+  //1.0.2 -> 1.0.3
+  const valueSchema = {
+    type: 'array',
+    items: {
+      type: 'object',
+      properties: {
+        name: {
+          type: 'string'
+        },
+        url: {
+          type: 'string'
+        },
+        status: {
+          type: 'string'
+        },
+        percent: {
+          type: 'number'
+        },
+        response: {
+          type: 'string'
+        }
+      }
+    }
+  };
+  if (!input.get('setInitialValue')) {
+    input.add('setInitialValue', '设置初始值', valueSchema);
+  }
+  if (!output.get('onInitial')) {
+    output.add('onInitial', '值初始化', valueSchema);
+  }
+  output.get('onInitial').setTitle('值初始化');
+
+  input.get('setValue').setSchema({
+    type: 'array',
+    items: {
+      type: 'object',
+      properties: {
+        name: {
+          type: 'string'
+        },
+        url: {
+          type: 'string'
+        },
+        status: {
+          type: 'string'
+        },
+        percent: {
+          type: 'number'
+        },
+        response: {
+          type: 'string'
+        }
+      }
+    }
+  });
+  input.get('uploadDone')?.setSchema({
+    type: 'object',
+    properties: {
+      name: {
+        type: 'string'
+      },
+      url: {
+        type: 'string'
+      },
+      status: {
+        type: 'string'
+      },
+      percent: {
+        type: 'number'
+      },
+      response: {
+        type: 'string'
+      }
+    }
+  });
+
+  if (!input.get('remove')) {
+    input.add('remove', '删除文件', {
+      type: 'object',
+      properties: {
+        name: {
+          type: 'string'
+        },
+        uid: {
+          type: 'string'
+        }
+      }
+    });
+  }
+
+  if (!output.get('remove')) {
+    output.add('remove', '删除文件', {
+      type: 'object',
+      properties: {
+        name: {
+          type: 'string'
+        },
+        uid: {
+          type: 'string'
+        },
+        url: {
+          type: 'string'
+        },
+        status: {
+          type: 'string'
+        },
+        percent: {
+          type: 'number'
+        },
+        response: {
+          type: 'string'
+        }
+      }
+    });
+  }
+
+  /**
+   * @description v1.0.7 , 新增自定义插槽, 展示文件列表
+   */
+  if (!slot?.get('carrier')) {
+    slot.add('carrier', '添加组件');
+  }
+  if (typeof data.isShowUploadList === 'undefined') {
+    data.isShowUploadList = true;
+  }
+  if (typeof data.isCustom === 'undefined') {
+    data.isCustom = false;
+  }
+
+  /**
+   * @description v1.0.8 , 新增尺寸校验
+   */
+  if (typeof data.imageSize === 'undefined') {
+    data.imageSize = [0, 0];
+  }
+
+  // 早期版本。默认都使用自定义上传
+  if (typeof data.customUpload === 'undefined') {
+    data.customUpload = true;
+  }
+
+  /**
+   * @description v1.0.22->1.0.23 , 新增上传点击事件
+   */
+  if (typeof data.fileClick === 'undefined') {
+    data.fileClick = false;
+  }
+
+  /**
+   * @description v1.0.24 新增启用/禁用 输入项
+   */
+  if (!input.get(inputIds.IsEnable)) {
+    input.add(inputIds.IsEnable, '启用/禁用', {
+      type: 'boolean'
+    });
+  }
+  //=========== v1.1.1 end ===============
+
+  /**
+   * @description v1.0.27 新增关联输出项
+   */
+  if (input.get('setDisabled').schema.type === 'boolean') {
+    input.get('setDisabled').setSchema({
+      type: 'any'
+    });
+  }
+  if (input.get('setEnabled').schema.type === 'boolean') {
+    input.get('setEnabled').setSchema({
+      type: 'any'
+    });
+  }
+  //1、设置值
+  const initValueSchema = {
+    type: 'array',
+    items: {
+      type: 'object',
+      properties: {
+        name: {
+          type: 'string'
+        },
+        url: {
+          type: 'string'
+        },
+        status: {
+          type: 'string'
+        },
+        percent: {
+          type: 'number'
+        },
+        response: {
+          type: 'string'
+        }
+      }
+    }
+  };
+  if (!output.get(outputIds.setValueDone)) {
+    output.add(outputIds.setValueDone, '设置值完成', initValueSchema);
+  }
+  if (
+    output.get(outputIds.setValueDone) &&
+    input.get(inputIds.setValue) &&
+    !input.get(inputIds.setValue)?.rels?.includes(outputIds.setValueDone)
+  ) {
+    input.get(inputIds.setValue).setRels([outputIds.setValueDone]);
+  }
+  //2、设置初始值
+  if (!output.get(outputIds.setInitialValueDone)) {
+    output.add(outputIds.setInitialValueDone, '设置初始值完成', initValueSchema);
+  }
+  if (
+    output.get(outputIds.setInitialValueDone) &&
+    input.get(inputIds.setInitialValue) &&
+    !input.get(inputIds.setInitialValue)?.rels?.includes(outputIds.setInitialValueDone)
+  ) {
+    input.get(inputIds.setInitialValue).setRels([outputIds.setInitialValueDone]);
+  }
+  //3、重置值
+  if (!output.get(outputIds.resetValueDone)) {
+    output.add(outputIds.resetValueDone, '重置完成', { type: 'any' });
+  }
+  if (
+    output.get(outputIds.resetValueDone) &&
+    input.get(inputIds.resetValue) &&
+    !input.get(inputIds.resetValue)?.rels?.includes(outputIds.resetValueDone)
+  ) {
+    input.get(inputIds.resetValue).setRels([outputIds.resetValueDone]);
+  }
+
+  //4、设置禁用
+  if (!output.get(outputIds.setDisabledDone)) {
+    output.add(outputIds.setDisabledDone, '禁用完成', { type: 'any' });
+  }
+  if (
+    output.get(outputIds.setDisabledDone) &&
+    input.get(inputIds.SET_DISABLED) &&
+    !input.get(inputIds.SET_DISABLED)?.rels?.includes(outputIds.setDisabledDone)
+  ) {
+    input.get(inputIds.SET_DISABLED).setRels([outputIds.setDisabledDone]);
+  }
+  //5、设置启用
+  if (!output.get(outputIds.setEnabledDone)) {
+    output.add(outputIds.setEnabledDone, '启用完成', { type: 'any' });
+  }
+  if (
+    output.get(outputIds.setEnabledDone) &&
+    input.get(inputIds.SET_ENABLED) &&
+    !input.get(inputIds.SET_ENABLED)?.rels?.includes(outputIds.setEnabledDone)
+  ) {
+    input.get(inputIds.SET_ENABLED).setRels([outputIds.setEnabledDone]);
+  }
+  //6、启用/禁用isEnable
+  if (!output.get(outputIds.isEnableDone)) {
+    output.add(outputIds.isEnableDone, '启用/禁用完成', { type: 'boolean' });
+  }
+  if (
+    output.get(outputIds.isEnableDone) &&
+    input.get(inputIds.IsEnable) &&
+    !input.get(inputIds.IsEnable)?.rels?.includes(outputIds.isEnableDone)
+  ) {
+    input.get(inputIds.IsEnable).setRels([outputIds.isEnableDone]);
+  }
+
+  //7、上传完成 uploadComplete
+  if (!output.get('uploadComplete')) {
+    output.add('uploadComplete', '设置值完成', initValueSchema);
+  }
+  if (
+    output.get('uploadComplete') &&
+    input.get('uploadDone') &&
+    !input.get('uploadDone')?.rels?.includes('uploadComplete')
+  ) {
+    input.get('uploadDone').setRels(['uploadComplete']);
+  }
+
+  //8、删除文件
+  const removeSchema = {
+    type: 'object',
+    properties: {
+      name: {
+        type: 'string'
+      },
+      uid: {
+        type: 'string'
+      }
+    }
+  };
+  if (!output.get('removeDone')) {
+    output.add('removeDone', '删除文件完成', removeSchema);
+  }
+  if (
+    output.get('removeDone') &&
+    input.get('remove') &&
+    !input.get('remove')?.rels?.includes('removeDone')
+  ) {
+    input.get('remove').setRels(['removeDone']);
+  }
+  //=========== v1.0.27 end ===============
+
+  /**
+   * @description v1.0.30 新增hideIcon
+   */
+  if (typeof data.hideIcon === 'undefined') {
+    data.hideIcon = false;
+  }
+  //=========== v1.0.30 end ===============
+
+  /**
+   * @description v1.0.32 新增 编辑/可读输入
+   */
+  if (!output.get(outputIds.isEditableDone)) {
+    output.add(outputIds.isEditableDone, '设置编辑/只读完成', { type: 'boolean' });
+  }
+  if (
+    output.get(outputIds.isEditableDone) &&
+    input.get(inputIds.isEditable) &&
+    !input.get(inputIds.isEditable)?.rels?.includes(outputIds.isEditableDone)
+  ) {
+    input.get(inputIds.isEditable).setRels([outputIds.isEditableDone]);
+  }
+
+  if (typeof data.isEditable === 'undefined') {
+    data.isEditable = true;
+  }
+  //=========== v1.0.32 end ===============
+
+  /**
+   * @description v1.0.33 新增 自定义图标配置
+   */
+
+  //=========== v1.0.33 end ===============
+
+  /**
+   * @description v1.0.40 change上传文案target
+   */
+
+  //=========== v1.0.40 end ===============
+
+  /**
+   * @description v1.0.45 新增description
+   */
+  descriptionUp(descriptionUpList, input, output);
+  //=========== 1.0.45 end ===============
+
+  /**
+   * @description v1.0.48 背景色，target 变化
+   */
+  const preStyle = getDeclaredStyle(['.ant-upload', '.ant-btn']);
+
+  if (preStyle) {
+    let contentCss = { ...preStyle.css };
+    removeDeclaredStyle(['.ant-upload', '.ant-btn']);
+    setDeclaredStyle(
+      [
+        `.ant-upload.ant-upload-select-picture-card${getFilterSelector(id)}`,
+        `.ant-upload .ant-btn${getFilterSelector(id)}`
+      ],
+      contentCss,
+      true
+    );
+  }
+  //=========== 1.0.45 end ===============
+
+  if (!data.rules) {
+    data.rules = [];
+  }
+
+  if (!input.get('setValidateInfo')) {
+    input.add('setValidateInfo', '设置校验状态', {
+      type: 'object',
+      properties: {
+        validateStatus: {
+          type: 'enum',
+          items: [
+            {
+              type: 'string',
+              value: 'success'
+            },
+            {
+              type: 'string',
+              value: 'error'
+            }
+          ]
+        },
+        help: {
+          type: 'string'
+        }
+      }
+    });
+  }
+
+  if (!output.get('setValidateInfoDone')) {
+    output.add('setValidateInfoDone', '设置校验状态完成', {
+      type: 'object',
+      properties: {
+        validateStatus: {
+          type: 'string'
+        },
+        help: {
+          type: 'string'
+        }
+      }
+    });
+  }
+
+  if (
+    input.get('setValidateInfo') &&
+    !input.get('setValidateInfo').rels?.includes('setValidateInfoDone')
+  ) {
+    input.get('setValidateInfo').setRels(['setValidateInfoDone']);
+  }
+
+  if (!output.get('onValidate')) {
+    output.add('onValidate', '校验触发', {
+      type: 'array'
+    });
+  }
+
+  //=========== v1.0.54 end ===============
+  
+  const transferStyleBetweenSelectors = (prevSelector, currentSelector) => {
+    const preStyle = getDeclaredStyle(prevSelector);
+    let currentCss: React.CSSProperties = {};
+
+    if (preStyle) {
+      currentCss = { ...preStyle.css };
+      removeDeclaredStyle(prevSelector);
+      setDeclaredStyle(currentSelector, currentCss);
+    }
+  };
+
+  transferStyleBetweenSelectors(
+    [
+      `.ant-upload.ant-upload-select-picture-card${getFilterSelector(id)}`,
+      `.ant-upload .ant-btn${getFilterSelector(id)}`
+    ],
+    [`.ant-upload.ant-upload-select-picture-card`, `.ant-upload .ant-btn`]
+  );
+
+  return true;
+}
