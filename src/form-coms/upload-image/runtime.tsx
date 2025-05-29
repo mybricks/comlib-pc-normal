dd7f70d9ce80bf583d77029a0dc08961dfeea9fe
MrMa <mtf986252757@gmail.com>
2025-05-29 10:52:58 +0800

    feat: 删除测试代码

--- src/form-coms/upload-image/runtime.tsx
+++ src/form-coms/upload-image/runtime.tsx
@@ -53,22 +53,8 @@ export default function ({
   id,
   name
 }: RuntimeParams<Data>) {
-  const [fileList, setFileList] = useState<UploadFile[]>([
-    {
-      uid: '-1',
-      name: 'image.png',
-      status: 'done',
-      url: 'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png'
-    }
-  ]);
-  const fileListRef = useRef<UploadFile[]>([
-    {
-      uid: '-1',
-      name: 'image.png',
-      status: 'done',
-      url: 'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png'
-    }
-  ]);
+  const [fileList, setFileList] = useState<UploadFile[]>([]);
+  const fileListRef = useRef<UploadFile[]>([]);
   const removeFileRef = useRef<UploadFile>();
 
   const uploadInputRef = useRef(null);
