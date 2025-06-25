b3e1a77296c854014c8168b9e96459e1662ba833
leon-js <42924541+leon-js@users.noreply.github.com>
2025-06-25 21:14:15 +0800

    chore:发布antd5版本

--- src/custom-container/ai.ts
+++ src/custom-container/ai.ts
@@ -1,29 +1,30 @@
 export default {
-  ':root' ({ data }) {
-    return {}
-  },
-  prompts: {
-    summary: '自定义容器',
-    usage: `data声明
-style: React.CSSProperties = {}
-slotStyle?: React.CSSProperties = {
-  "position": "smart"
-},
-legacyConfigStyle: React.CSSProperties = {}
-legacyStyle: React.CSSProperties = {}
-isAutoScroll: boolean = false
-direction: 'horizontal'|'vertical' "vertical"
-scrollTime: number = 2000
-eventBubble: boolean = false
+  ignore: true,
+//   ':root' ({ data }) {
+//     return {}
+//   },
+//   prompts: {
+//     summary: '自定义容器',
+//     usage: `data声明
+// style: React.CSSProperties = {}
+// slotStyle?: React.CSSProperties = {
+//   "position": "smart"
+// },
+// legacyConfigStyle: React.CSSProperties = {}
+// legacyStyle: React.CSSProperties = {}
+// isAutoScroll: boolean = false
+// direction: 'horizontal'|'vertical' "vertical"
+// scrollTime: number = 2000
+// eventBubble: boolean = false
 
-slots插槽
-content: 内容区内容
+// slots插槽
+// content: 内容区内容
 
-styleAry声明
-容器: > .root
-  可编辑样式: padding、border、background、overflow、BoxShadow
-容器hover: > .root:hover
-  可编辑样式: padding、border、background、BoxShadow
-`
-  }
+// styleAry声明
+// 容器: > .root
+//   可编辑样式: padding、border、background、overflow、BoxShadow
+// 容器hover: > .root:hover
+//   可编辑样式: padding、border、background、BoxShadow
+// `
+//   }
 }
