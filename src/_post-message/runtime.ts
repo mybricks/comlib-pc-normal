89c6a254b987cb054607dd6a1ef22d5169817b60
chemingjun <chemingjun@126.com>
2025-07-03 12:06:59 +0800

    post message组件的bug‘

--- src/_post-message/runtime.ts
+++ src/_post-message/runtime.ts
@@ -9,19 +9,24 @@ export interface Item {
 }
 
 export default function (props: RuntimeParams<Data>) {
-  const { env, inputs, data } = props;
-  const { runtime } = env;
+  const {env, inputs, data} = props;
+  const {runtime} = env;
+  const envCanvasElement = env.canvasElement || document
 
   if (runtime) {
     inputs['post']((val, relOutputs) => {
       //父页面向子页面传递数据
       if (data.goal === 'subPage' && data.iframeId !== '') {
-        let iFramexDom = document.getElementById(data.iframeId);
-        let iFramex = document.getElementById(data.iframeId).contentWindow;
-        iFramexDom.onload = function () {
-          iFramex.postMessage(val, '*');
-        };
-        relOutputs["postDone"](val);
+        const iFramexDom = envCanvasElement.querySelector(`#${data.iframeId}`)
+        if (iFramexDom) {
+          const iFramex = iFramexDom.contentWindow;
+          iFramexDom.onload = function () {
+            iFramex.postMessage(val, '*')
+          };
+          relOutputs["postDone"](val)
+        } else {
+          relOutputs["postDone"](`iframe with id ${data.iframeId} not found`)
+        }
       }
 
       //子页面向父页面传递数据
