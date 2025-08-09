ae9f097527a3f9328957c2514ea35745034f3a4c
MrMa <mtf986252757@gmail.com>
2025-08-09 17:46:58 +0800

    feat: 增加ios系统兼容

--- src/form-coms/raw-text/runtime.tsx
+++ src/form-coms/raw-text/runtime.tsx
@@ -9,6 +9,15 @@ export interface Data {
   expandRows: number;
 }
 
+function isIOS() {
+  const u = navigator.userAgent;
+  return (
+    !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/) ||
+    u.includes('iPhone') ||
+    u.includes('iPad')
+  );
+}
+
 export default function (props: RuntimeParams<Data>) {
   const { env, data, inputs, outputs, parentSlot } = props;
   const textRef = useRef<HTMLDivElement | null>(null);
@@ -52,18 +61,49 @@ export default function (props: RuntimeParams<Data>) {
   );
 
   useEffect(() => {
-    if (!!value && textRef.current && textRef.current?.getBoundingClientRect().height > data.expandRows * 22) {
+    if (
+      !!value &&
+      textRef.current &&
+      textRef.current?.getBoundingClientRect().height > data.expandRows * 22
+    ) {
       setWithHiddenStyle(true);
       setToggleHiddenStyle(true);
     }
   }, [value, data.expandRows]);
+
+  if (isIOS()) {
+    return (
+      <div className={css.textOverflowWrapperIOS}>
+        <div
+          className={css.textOverflowIOS + ' raw-text-content'}
+          style={{
+            WebkitLineClamp: !toggleHiddenStyle ? 999 : data.expandRows
+          }}
+          ref={textRef}
+        >
+          {value}
+        </div>
+        {withHiddenStyle && (
+          <Button
+            className={css.toggleHiddenBtnIOS}
+            onClick={() => {
+              setToggleHiddenStyle((v) => !v);
+            }}
+            type="link"
+          >
+            {toggleHiddenStyle ? env.i18n('展开') : env.i18n('收起')}
+          </Button>
+        )}
+      </div>
+    );
+  }
 
   return (
     <div className={css.textOverflowWrapper}>
       <div
         className={css.textOverflow + ' raw-text-content'}
         style={{
-          WebkitLineClamp: !toggleHiddenStyle ? 999 : data.expandRows,
+          WebkitLineClamp: !toggleHiddenStyle ? 999 : data.expandRows
         }}
         ref={textRef}
       >
