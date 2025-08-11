352c24aeb9f4310d238002b781b3c8713a6a484d
MrMa <mtf986252757@gmail.com>
2025-08-11 09:08:36 +0800

    feat: 增加苹果端兼容

--- src/form-coms/raw-text/runtime.tsx
+++ src/form-coms/raw-text/runtime.tsx
@@ -9,6 +9,15 @@ export interface Data {
   expandRows: number;
 }
 
+function isIOS() {
+  const u = navigator.userAgent;
+  return (
+    !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/) ||
+    u.includes('iPhone') ||
+    u.includes('iPad')
+  );
+}
+
 export default function (props: RuntimeParams<Data>) {
   const { env, data, inputs, outputs, parentSlot } = props;
   const textRef = useRef<HTMLDivElement | null>(null);
@@ -57,6 +66,33 @@ export default function (props: RuntimeParams<Data>) {
       setToggleHiddenStyle(true);
     }
   }, [value, data.expandRows]);
+
+  if (isIOS()) {
+    return (
+      <div className={css.textOverflowWrapperIOS}>
+        <div
+          className={css.textOverflowIOS + ' raw-text-content'}
+          style={{
+            WebkitLineClamp: !toggleHiddenStyle ? 999 : data.expandRows
+          }}
+          ref={textRef}
+        >
+          {value}
+        </div>
+        {withHiddenStyle && (
+          <Button
+            className={css.toggleHiddenBtnIOS}
+            onClick={() => {
+              setToggleHiddenStyle((v) => !v);
+            }}
+            type="link"
+          >
+            {toggleHiddenStyle ? env.i18n('展开') : env.i18n('收起')}
+          </Button>
+        )}
+      </div>
+    );
+  }
 
   return (
     <div className={css.textOverflowWrapper}>
