e36eb5a9551354ff1983e8ad00ca8a4092436a38
MrMa <mtf986252757@gmail.com>
2025-08-15 18:26:44 +0800

    feat: 直接内容增加换行空格转义符支持

--- src/form-coms/raw-text/runtime.tsx
+++ src/form-coms/raw-text/runtime.tsx
@@ -81,7 +81,7 @@ export default function (props: RuntimeParams<Data>) {
           }}
           ref={textRef}
         >
-          {value}
+          <span style={{wordBreak: 'break-word', whiteSpace: 'pre-wrap', letterSpacing: '0'}}>{value}</span>
         </div>
         {withHiddenStyle && (
           <Button
@@ -118,7 +118,7 @@ export default function (props: RuntimeParams<Data>) {
             {toggleHiddenStyle ? env.i18n('展开') : env.i18n('收起')}
           </Button>
         )}
-        {value}
+        <span style={{wordBreak: 'break-word', whiteSpace: 'pre-wrap', letterSpacing: '0'}}>{value}</span>
       </div>
     </div>
   );
