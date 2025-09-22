d600b75896850eb1a976953ccdb6582d4866f740
liubiao03 <liubiao03@kuaishou.com>
2025-09-22 15:31:23 +0800

    feat: 组件提示词更新

--- src/breadcrumb/ai.ts
+++ src/breadcrumb/ai.ts
@@ -25,5 +25,36 @@ slots插槽
 styleAry声明
 文本: .\${children[0].key}
   - 可编辑样式: font`
+  },
+  editors: [
+    '面包屑/分割符',
+    {
+      title: '面包屑/数据源',
+      description: `通过数组来配置面包屑数据
+[
+  {
+    key: string = "menu1"
+    label: string = '面包屑1'
   }
+]
+`,
+      type: 'array',
+      value: {
+        set: ({ data, slot, output }, value) => {
+          if (Array.isArray(value)) {
+            data.children = value.map(item => {
+              if (!output.get(item.key)) {
+                output.add(item.key, '面包屑点击', { type: 'string' })
+              }
+              return {
+                ...item,
+                event: { value: item.key }
+              }
+            })
+          }
+        }
+      }
+    },
+    '样式/文本',
+  ],
 };
---
+++ src/tooltip/ai.ts
@@ -0,0 +1,16 @@
+export default {
+  ':root' ({ data }) {
+    return {}
+  },
+  prompts: {
+    summary: '文字提示，对标antd的tooltip。',
+    usage: `文字提示，对标antd的tooltip。',
+slots插槽
+carrier: 触发器的内容
+
+layout声明
+width: 可配置，默认为fit-content
+height: 不可配置，默认为fit-content
+`
+  }
+}
