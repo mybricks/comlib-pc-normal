c07857451ff5a3a77f5798d02e18828f2fbc1b4a
liubiao03 <liubiao03@kuaishou.com>
2025-12-22 14:47:36 +0800

    feat: update

--- src/carousel/ai.ts
+++ src/carousel/ai.ts
@@ -3,14 +3,42 @@ export default {
     return {}
   },
   prompts: {
-    summary: '轮播图，可以配置图片和内容的轮播展示组件',
-    usage: `轮播图，可以配置图片和内容的轮播展示组件。
+    summary: '轮播图，可以同时配置图片和内容的轮播展示组件',
+    usage: `轮播图，可以同时配置图片和内容的轮播展示组件。
 
 slots插槽
-  [slotId]：每一项的插槽id对应着当前项的slotId
-
+  [slotId]：轮播插槽内容，跟着当前item的slotId走
 `
   },
+  editors: [
+    {
+      title: '常规/数据',
+      description: `通过数组来配置轮播数据
+[
+  {
+    url: string
+    slotId: string
+    bgSize: 'contain' | 'cover' | '100% 100%' = 'contain'
+  }
+]
+`,
+      type: 'array',
+      value: {
+        set: ({ data, slot }, value) => {
+          if (Array.isArray(value)) {
+            data.items = value.map(item => {
+              return item
+            })
+          }
+        }
+      }
+    },
+    '常规/自动切换',
+    '常规/时间间隔',
+    '常规/分页器配置/位置',
+    '常规/分页器配置/高度',
+    '常规/分页器配置/背景色'
+  ],
   modifyTptJson: (component) => {
     if (!component?.data) {
       component.data = {}
@@ -25,5 +53,5 @@ slots插槽
       item.slotId = `slot${index + 1}`
       item.bgSize = "cover"
     })
-  }
+  },
 }
